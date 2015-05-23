bcb7469842c65419d4f14d76436816debc4a9ffb
Caitlin Holman <caitlin.holman@gmail.com>
2015-05-22 21:32:11 -0400

    timeline updates

--- app/assets/javascripts/timeline_settings.js
+++ app/assets/javascripts/timeline_settings.js
@@ -19,15 +19,15 @@ $(document).ready(function(){
     var timeline_dates = source.timeline.date;
     var start_index = 1;
     var target_date = new Date();
-    console.log(target_date);
+    // console.log(target_date);
     for(x in timeline_dates) {
       var slide_date = new Date( timeline_dates[x].startDate );
-      console.log(timeline_dates[x]);
+      // console.log(timeline_dates[x]);
       if( slide_date < target_date ) {
         start_index++
       }
       else if (start_index > x) {
-        start_index = start_index - 1
+        start_index = start_index - 1;
       };
     }
 
