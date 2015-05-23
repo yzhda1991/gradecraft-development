54e36695d337e0d51fe941da4a005c4fa12842ab
Caitlin Holman <caitlin.holman@gmail.com>
2015-05-22 21:13:03 -0400

    Testing timeline for past courses

--- app/assets/javascripts/timeline_settings.js
+++ app/assets/javascripts/timeline_settings.js
@@ -18,13 +18,16 @@ $(document).ready(function(){
     //get the events.json format from https://github.com/VeriteCo/TimelineJS#file-formats
     var timeline_dates = source.timeline.date;
     var start_index = 1;
-    var target_date = new Date(); 
+    var target_date = new Date();
     console.log(target_date);
     for(x in timeline_dates) {
       var slide_date = new Date( timeline_dates[x].startDate );
       console.log(timeline_dates[x]);
       if( slide_date < target_date ) {
         start_index++
+      }
+      else if (start_index > x) {
+        start_index = start_index - 1
       };
     }
 
