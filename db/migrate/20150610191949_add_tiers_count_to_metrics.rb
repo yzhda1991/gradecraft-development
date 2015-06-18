bc751977382bafaae761871d50b4591003af7b8b
Caitlin Holman <caitlin.holman@gmail.com>
2015-06-18 11:22:40 -0400

    Updated tier count migration

--- db/migrate/20150610191949_add_tiers_count_to_metrics.rb
+++ db/migrate/20150610191949_add_tiers_count_to_metrics.rb
@@ -1,9 +1,5 @@
 class AddTiersCountToMetrics < ActiveRecord::Migration
   def change
     add_column :metrics, :tiers_count, :integer, :default => 0
-    Metric.reset_column_information
-    Metric.all.each do |m|
-      m.update_attribute :tiers_count, m.tiers.length
-    end
   end
 end
