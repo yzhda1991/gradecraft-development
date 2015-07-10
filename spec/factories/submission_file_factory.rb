44f500589cb5c357c01329eaf7126bd0c004da4b
chcholman <caitlin.holman@gmail.com>
2015-07-10 14:50:04 -0400

    passing assignments spec

--- spec/factories/submission_file_factory.rb
+++ spec/factories/submission_file_factory.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :submission_file do
-    submission
+    association :submission
     filename "test_file.rb"
+    filepath "uploads/submission_files/"
     file { fixture_file('test_image.jpg', 'img/jpg') }
   end
 end
