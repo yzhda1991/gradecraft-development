ce1c05b879419a89fd7dc1dd6c3595457b0788bd
Max Langensiepen <mlangens@emich.edu>
2015-07-13 14:47:10 -0400

    formly deps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,6 +30,9 @@
 //= require ./angular/main.js
 //= require ./angular/ui/sortable
 //= require ./angular/vendor/restangular.min
+//= require ./angular/vendor/api-check
+//= require ./angular/vendor/formly
+//= require ./angular/vendor/angular-formly-templates-foundation
 //= require_tree ./angular
 //= require gradecraft
 //= require preload_store
