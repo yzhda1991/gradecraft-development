103eadbaafcbc89f92b11fff34a8988d8d248c42
chcholman <caitlin.holman@gmail.com>
2015-07-16 16:56:27 -0400

    Migrations, models, and first form fields for unlocks

--- app/models/unlock_state.rb
+++ app/models/unlock_state.rb
@@ -3,11 +3,11 @@ class UnlockState < ActiveRecord::Base
 	attr_accessible :unlockable_id, :unlockable_type, :student_id, :instructor_unlocked, :unlocked
 
 	belongs_to :unlockable, :polymorphic => true
-	belongs_to :student, :class_name => 'User'
+  belongs_to :student, :class_name => 'User'
 
 
 	def is_unlocked?
 		unlocked? || instructor_unlocked?
 	end
-
+  
 end
