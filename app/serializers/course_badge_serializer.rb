9934c1f18aea08b47330b9382601cb2dfe3ae8df
Mike Zazaian <mike@zop.io>
2015-07-24 15:15:07 +0200

    repair some changes to CourseBadgeSerializer for rubrics

--- app/serializers/course_badge_serializer.rb
+++ app/serializers/course_badge_serializer.rb
@@ -1,18 +1,5 @@
 class CourseBadgeSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :point_total, :icon, :multiple, :student_id
-  has_many :student_earned_badges, serializer: EarnedBadgeSerializer
-
-  def student_earned_badges
-    if options[:student_id]
-      earned_badges.where(student_id: options[:student_id])
-    else
-      []
-    end
-  end
-
-  def student_id
-    @serialization_options[:student_id]
-  end
 
   def multiple
     object.can_earn_multiple_times
