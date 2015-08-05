2bbc51c13c29771b216198c4fba2baefe090e64e
Jamie Wright <jamie@brilliantfantastic.com>
2015-08-05 14:21:09 -0400

    Find user without regard to email case

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -5,7 +5,7 @@ class PasswordsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by_insensitive_email(params[:email])
     @user.deliver_reset_password_instructions! if @user
 
     redirect_to login_path, notice: "Password reset instructions have been sent to your email."
