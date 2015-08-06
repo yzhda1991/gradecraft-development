2c1480b5caad07437b2a118275a7b667948f1dcd
Jamie Wright <jamie@brilliantfantastic.com>
2015-08-06 17:18:48 -0400

    Add user_activation submodule for sorcery

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "mailer@gradecraft.com"
 
+  def activation_needed_email(user)
+  end
+
   def reset_password_email(user)
     @user = user
     mail(:to => user.email, :subject => "Your GradeCraft Password Reset Instructions")
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,7 +4,8 @@ FactoryGirl.define do
     last_name { Faker::Name.last_name }
     username { Faker::Internet.user_name }
     email { Faker::Internet.email }
-    salt "asdasdastr4325234324sdfds"
-    crypted_password { Sorcery::CryptoProviders::BCrypt.encrypt("secret", salt) }
+    password { "secret" }
+
+    after(:create) { |user| user.activate! }
   end
 end
