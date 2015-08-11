fac6b48aa3359947bb1723987d169b033dd65c65
Jamie Wright <jamie@brilliantfantastic.com>
2015-08-11 16:28:30 -0400

    Ensure that the announcement is delivered to the students

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -8,4 +8,17 @@ class AnnouncementsController < ApplicationController
     @title = "Create a New Announcement"
     @announcement = Announcement.new course_id: current_course.id
   end
+
+  def create
+    announcement_params = params[:announcement]
+      .merge({ course_id: current_course.id, author_id: current_user.id })
+    @announcement = Announcement.new announcement_params
+    if @announcement.save
+      @announcement.deliver!
+      redirect_to announcements_path, notice: "Announcement created and sent." and return
+    end
+
+    @title = "Create a New Announcement"
+    render :new
+  end
 end
--- spec/controllers/announcements_controller_spec.rb
+++ spec/controllers/announcements_controller_spec.rb
@@ -20,4 +20,33 @@ describe AnnouncementsController do
       expect(assigns(:announcements)).to eq [announcement]
     end
   end
+
+  describe "POST #create" do
+    context "with a successful announcement" do
+      let(:body) { Faker::Lorem.sentence(3) }
+
+      it "creates a new announcement" do
+        post :create, announcement: { title: "New Tour", body: body }
+        announcement = Announcement.unscoped.last
+        expect(announcement.title).to eq "New Tour"
+        expect(announcement.body).to eq body
+        expect(announcement.course).to eq course
+        expect(announcement.author).to eq professor
+      end
+
+      it "redirects back to the announcements page" do
+        post :create, announcement: { title: "New Tour", body: body }
+        expect(response).to redirect_to announcements_path
+      end
+
+      it "sends out the announcement to all the students in the course" do
+        student = create :user
+        CourseMembership.create! course_id: course.id,
+          user_id: student.id, role: "student"
+        expect {
+          post :create, announcement: { title: "New Tour", body: body }
+        }.to change  { ActionMailer::Base.deliveries.count }.by 1
+      end
+    end
+  end
 end
