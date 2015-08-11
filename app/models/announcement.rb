362398e160f6b6d5c8d6eb2118ee85559be03293
Jamie Wright <jamie@brilliantfantastic.com>
2015-08-11 15:03:29 -0400

    Announcement#abstract to retrieve a subset of the body

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,5 +2,11 @@ class Announcement < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :course
 
+  attr_accessible :body
+
   default_scope { order "created_at DESC" }
+
+  def abstract(words=25)
+    body.split(/\s+/)[0..words].join(" ").strip
+  end
 end
--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -8,4 +8,24 @@ describe Announcement do
       expect(Announcement.all).to eq [announcement2, announcement1]
     end
   end
+
+  describe "#abstract" do
+    let(:body) do <<-BODY
+        I am honored to be with you today at your commencement from one of the finest universities in the world. I never graduated from college. Truth be told, this is the closest I've ever gotten to a college graduation. Today I want to tell you three stories from my life. That's it. No big deal. Just three stories.
+
+        The first story is about connecting the dots.
+
+        I dropped out of Reed College after the first 6 months, but then stayed around as a drop-in for another 18 months or so before I really quit. So why did I drop out?
+      BODY
+    end
+    subject { Announcement.new body: body }
+
+    it "returns the first 25 words by default" do
+      expect(subject.abstract).to eq "I am honored to be with you today at your commencement from one of the finest universities in the world. I never graduated from college."
+    end
+
+    it "returns the specified number of words" do
+      expect(subject.abstract(3)).to eq "I am honored"
+    end
+  end
 end
