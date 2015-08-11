e89c5c964fdb65fb535b351574796f7f9bf8fa37
Jamie Wright <jamie@brilliantfantastic.com>
2015-08-11 15:39:55 -0400

    Announcements#new view

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,5 +1,11 @@
 class AnnouncementsController < ApplicationController
   def index
+    @title = "Announcements"
     @announcements = Announcement.where(course_id: current_course.id)
+  end
+
+  def new
+    @title = "Create a New Announcement"
+    @announcement = Announcement.new course_id: current_course.id
   end
 end
