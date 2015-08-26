dac535c87a48b5a99f2433295df3797f8cf858e7
Jonathan Gabel <hello@jonathangabel.com>
2015-08-26 17:41:47 -0400

    fixed pass fail switches on assignment and grade forms

--- app/assets/javascripts/pass_fail.js
+++ app/assets/javascripts/pass_fail.js
@@ -1,22 +1,22 @@
 !function($) {
 
+  // Toggle label on:
+  //   app/views/assignments/_form.haml
   $('.pass-fail-toggle :checkbox').click(function(){
-    $('.pass-fail-contingent').toggleClass("hidden")
+    $('.pass-fail-toggle :checkbox').prop("checked") ?
+      $('.pass-fail-contingent').addClass("hidden") :
+      $('.pass-fail-contingent').removeClass("hidden");
   });
 
 
   // Toggle label on:
   //   grades/_standard_edit.html.haml
-  //   students/predictor/_assignments.haml
-
-  $('.pass-fail-grade-toggle label').click(function(){
+  $('.pass-fail-grade-toggle :checkbox').click(function(){
     var on = $('.pass-fail-contingent').data("on");
     var off = $('.pass-fail-contingent').data("off");
-    if ($( ".pass-fail-grade-toggle input:checked" ).length > 0) {
-      $('.pass-fail-contingent').text(off);
-    } else {
-      $('.pass-fail-contingent').text(on);;
-    };
+    $('.pass-fail-grade-toggle :checkbox').prop("checked") ?
+      $('.pass-fail-contingent label').text(on) :
+      $('.pass-fail-contingent label').text(off);
   });
 
 }(jQuery);
