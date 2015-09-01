a2165700bb9a961505e3098847eb474c60e49b0a
Jamie Wright <jamie@brilliantfantastic.com>
2015-09-01 14:42:06 -0400

    Validator for file size of an uploader

--- app/validators/file_size_validator.rb
+++ app/validators/file_size_validator.rb
@@ -1,8 +1,45 @@
 class FileSizeValidator < ActiveModel::EachValidator
+  CHECKS = { is: :==, minimum: :>=, maximum: :<= }.freeze
+  MESSAGES  = { is: :wrong_size, minimum: :size_too_small, maximum: :size_too_big }.freeze
+  RESERVED_OPTIONS  = [:minimum, :maximum, :within, :is, :too_short, :too_long]
+
   def initialize(options)
+    if range = (options.delete(:in) || options.delete(:within))
+      raise ArgumentError, ":in and :within must be a Range" unless range.is_a?(Range)
+      options[:minimum], options[:maximum] = range.begin, range.end
+      options[:maximum] -= 1 if range.exclude_end?
+    end
     super
   end
 
   def validate_each(record, attribute, value)
+    raise(ArgumentError, "A CarrierWave::Uploader::Base object was expected") \
+      unless value.kind_of? CarrierWave::Uploader::Base
+
+    CHECKS.each do |key, validity_check|
+      next unless check_value = options[key]
+
+      value ||= [] if key == :maximum
+
+      value_size = value.size
+      next if value_size.send(validity_check, check_value)
+
+      errors_options = options.except(*RESERVED_OPTIONS)
+      errors_options[:file_size] = help.number_to_human_size check_value
+
+      default_message = options[MESSAGES[key]]
+      errors_options[:message] ||= default_message if default_message
+
+      record.errors.add(attribute, MESSAGES[key], errors_options)
+    end
+  end
+
+  def help
+    Helper.instance
+  end
+
+  class Helper
+    include Singleton
+    include ActionView::Helpers::NumberHelper
   end
 end
--- spec/validators/file_size_validator_spec.rb
+++ spec/validators/file_size_validator_spec.rb
@@ -6,15 +6,53 @@ describe FileSizeValidator do
     include ActiveModel::Validations
 
     attr_accessor :media
-    validates :media, file_size: true
   end
 
   let(:image) { fixture_file "test_image.jpg", "img/jpg" }
-  subject { Foo.new(media: image) }
+  let(:uploader) { AssignmentMediaUploader.new subject, :media }
+  subject { Foo.new }
+  before do
+    subject.class.clear_validators!
+    uploader.store! image
+  end
 
-  context "when file size is within an acceptable range" do
-    it "is valid" do
-      expect(subject).to be_valid
-    end
+  it "is valid if the size is within range" do
+    subject.class.validates :media, file_size: { maximum: 4.megabytes.to_i }
+    subject.media = uploader
+    expect(subject).to be_valid
+  end
+
+  it "must be an uploader" do
+    subject.class.validates :media, file_size: true
+    subject.media = image
+    expect { subject.valid? }.to raise_error ArgumentError
+  end
+
+  it "is invalid if the file size exceeds the maximum size" do
+    subject.class.validates :media, file_size: { maximum: 2.kilobytes.to_i }
+    subject.media = uploader
+    expect(subject).to_not be_valid
+    expect(subject.errors[:media]).to include "is too big (should be at most 2 KB)"
+  end
+
+  it "is invalid if the file size falls below the minimum size" do
+    subject.class.validates :media, file_size: { minimum: 4.megabytes.to_i }
+    subject.media = uploader
+    expect(subject).to_not be_valid
+    expect(subject.errors[:media]).to include "is too small (should be at least 4 MB)"
+  end
+
+  it "is invalid if the file size is not within a range" do
+    subject.class.validates :media, file_size: { in: 4.megabytes.to_i..6.megabytes.to_i }
+    subject.media = uploader
+    expect(subject).to_not be_valid
+    expect(subject.errors[:media]).to include "is too small (should be at least 4 MB)"
+  end
+
+  it "is invalid of the file size does not equal a specific size" do
+    subject.class.validates :media, file_size: { is: 4.megabytes.to_i }
+    subject.media = uploader
+    expect(subject).to_not be_valid
+    expect(subject.errors[:media]).to include "is the wrong size (should be 4 MB)"
   end
 end
