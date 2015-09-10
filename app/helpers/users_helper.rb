eb0bf9326e8a38ea8858f7943521ff3915463d1b
Jamie Wright <jamie@brilliantfantastic.com>
2015-09-10 15:13:47 -0400

    Render the javascript for exchanging the html within the flagged user icon button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,11 @@ module UsersHelper
   def generate_random_password
     Sorcery::Model::TemporaryToken.generate_random_token
   end
+
+  def flagged_user_icon(course, flagger, flagged_id)
+    flagged = FlaggedUser.flagged? course, flagger, flagged_id
+    style = flagged ? "fa-star" : "fa-star-o"
+    text = flagged ? "Unflag" : "Flag"
+    raw("<i class=\"fa #{style} fa-fw\"></i> #{text}")
+  end
 end
