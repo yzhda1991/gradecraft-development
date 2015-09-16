48128f706ad68f3d7240d3f09bfd01593e6d3ab5
Jamie Wright <jamie@brilliantfantastic.com>
2015-09-16 10:20:25 -0400

    Remove the stupid collection cache key idea

--- app/models/collection_cache_key.rb
+++
@@ -1,9 +0,0 @@
-class CollectionCacheKey
-  def self.cache_key(collection)
-    digest = Digest::MD5.new
-    collection.each do |item|
-      digest << "#{item.id}-#{item.updated_at}"
-    end
-    "#{collection.class.name.underscore}/#{digest.hexdigest}"
-  end
-end
