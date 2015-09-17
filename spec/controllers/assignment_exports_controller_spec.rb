2769e04f04d56a820805329c043e9f08f6c76ced
Mike Zazaian <mike@zop.io>
2015-09-16 20:46:51 -0400

    iron out bugs in assignment export grouping spec for controller

--- spec/controllers/assignment_exports_controller_spec.rb
+++ spec/controllers/assignment_exports_controller_spec.rb
@@ -19,19 +19,19 @@ RSpec.describe AssignmentExportsController, type: :controller do
         @controller = AssignmentExportsController.new
 
         @submission1 = {id: 1, student: 
-          {first_name: "Ben", last_name: "Bailey", id: 40}},
+          {first_name: "Ben", last_name: "Bailey", id: 40}}
         @submission2 = {id: 2, student:
-          {first_name: "Mike", last_name: "McCaffrey", id: 55}},
+          {first_name: "Mike", last_name: "McCaffrey", id: 55}}
         @submission3 = {id: 3, student:
           {first_name: "Dana", last_name: "Dafferty", id: 92}}
         @submission4 = {id: 4, student:
-          {first_name: "Mike", last_name: "McCaffrey", id: 55}},
+          {first_name: "Mike", last_name: "McCaffrey", id: 55}}
 
         @submissions = [@submission1, @submission2, @submission3, @submission4]
 
         @grouped_submission_expectation = {
           "bailey_ben-40" => [@submission1],
-          "mccaffrey_mike-50" => [@submission2, @submission4],
+          "mccaffrey_mike-55" => [@submission2, @submission4],
           "dafferty_dana-92" => [@submission3]
         }
 
@@ -40,7 +40,8 @@ RSpec.describe AssignmentExportsController, type: :controller do
 
       context "grouping students" do
         it "should group students by 'last_name_first_name-id'" do
-          @controller.instance_eval { group_submissions_by_student }
+          pp @submissions
+          expect(@controller.instance_eval { group_submissions_by_student }).to eq(@grouped_submission_expectation)
         end
       end
     end
