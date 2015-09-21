190787ce29c41c92ba9dbbb97abe88100563b867
Mike Zazaian <mike@zop.io>
2015-09-21 01:43:32 -0400

    add content_tag helper for building color palette. refactor color palette with helper. refactor sass color helper class definition using sass-maps.

---
+++ app/helpers/color_palette_helper.rb
@@ -0,0 +1,11 @@
+module ColorPaletteHelper 
+  def color_block_set(color, numbers)
+    numbers.collect do |number|
+      class_name = "color-#{color}-#{number}"
+      content_tag :div, class: "color-block-set" do
+        content_tag(:div, "", class: "color-block #{class_name}") +
+        content_tag(:div, ".#{class_name}", class: "color-name")
+      end
+    end.join("\n")
+  end
+end
