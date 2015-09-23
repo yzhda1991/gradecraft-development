39c2cdedfd71fb35140e0b175c4c9e7b9c1ae3b5
Prabode Weebadde <prabode@venturit.com>
2015-09-23 00:39:27 -0400

    VT - refactored to load saml cert files using files in the system to fix production server's env setting issues.

--- config/initializers/saml.rb
+++ config/initializers/saml.rb
@@ -11,7 +11,9 @@ saml_settings.issuer                         =  ENV["ISSUER"]
 saml_settings.idp_entity_id                  = ENV["IDP_ENTITY_ID"]
 saml_settings.idp_sso_target_url             = ENV["IDP_SSO_TARGET_URL"]
 saml_settings.idp_slo_target_url             = ENV["IDP_SLO_TARGET_URL"]
-saml_settings.idp_cert                       = ENV["IDP_CERT"]
+
+idp_cert_file = File.open(ENV["IDP_CERT"], "rb")
+saml_settings.idp_cert = idp_cert_file.read
 
 # or saml_settings.idp_cert_fingerprint           = "3B:05:BE:0A:EC:84:CC:D4:75:97:B3:A2:22:AC:56:21:44:EF:59:E6"
 #saml_settings.idp_cert_fingerprint_algorithm = XMLSecurity::Document::SHA1
@@ -27,9 +29,11 @@ saml_settings.security[:digest_method]    = XMLSecurity::Document::SHA1
 saml_settings.security[:signature_method] = XMLSecurity::Document::RSA_SHA1
 
 
+saml_cert_file = File.open(ENV["SAML_CERT"], "rb")
+saml_settings.certificate = saml_cert_file.read
 
-saml_settings.certificate = ENV["SAML_CERT"]
-saml_settings.private_key = ENV["SAML_PR_KEY"]
+saml_pr_key_file = File.open(ENV["SAML_PR_KEY"], "rb")
+saml_settings.private_key = saml_pr_key_file.read
 
 SAML_SETTINGS=saml_settings
 
