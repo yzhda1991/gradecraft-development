1ab6cebdef1f6530871e654952ca120a3e055383
Mike Zazaian <mike@zop.io>
2015-09-23 15:16:22 -0400

    strip out reference method from assignment export presenter

--- app/presenters/assignment_export_presenter.rb
+++ app/presenters/assignment_export_presenter.rb
@@ -20,23 +20,3 @@ class AssignmentExportPresenter < Presenter::Base
     "#{properties.assignment.name}"
   end
 end
-
-    files = []
-    sbs[1].each do |submission|
-      if submission.text_comment.present? or submission.link.present?
-        student = submission.student
-        filename = "#{student.last_name}_#{student.first_name}_#{@assignment.name.gsub(/\W+/, "_").downcase[0..20]}_submission_text.txt"
-        contents = "Submission items from #{student.last_name}, #{student.first_name}\n"
-        contents += "\ntext comment: #{submission.text_comment}\n" if submission.text_comment.present?
-        contents += "\nlink: #{submission.link }\n" if submission.link.present?
-        files << { content: contents, name: filename, content_type: "text" }
-      end
-      if submission.submission_files
-        submission.submission_files.each do |sf|
-          files << { path: sf.url, content_type: sf.content_type }
-        end
-      end
-    end
-
-  end
-end
