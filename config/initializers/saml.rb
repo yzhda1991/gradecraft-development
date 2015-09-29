90a37800889db4e8987230877391c331a0a02d27
Jamie Wright <jamie@brilliantfantastic.com>
2015-09-28 22:38:16 -0400

    Reset the saml initializer to the master copy

--- config/initializers/saml.rb
+++ config/initializers/saml.rb
@@ -12,9 +12,10 @@ saml_settings.idp_entity_id                  = ENV["IDP_ENTITY_ID"]
 saml_settings.idp_sso_target_url             = ENV["IDP_SSO_TARGET_URL"]
 saml_settings.idp_slo_target_url             = ENV["IDP_SLO_TARGET_URL"]
 
-# idp_cert_file = File.open(ENV["IDP_CERT"], "rb")
-# saml_settings.idp_cert = idp_cert_file.read
-
+if ENV["IDP_CERT"].present?
+  idp_cert_file = File.open(ENV["IDP_CERT"], "rb")
+  saml_settings.idp_cert = idp_cert_file.read if idp_cert_file.present?
+end
 # or saml_settings.idp_cert_fingerprint           = "3B:05:BE:0A:EC:84:CC:D4:75:97:B3:A2:22:AC:56:21:44:EF:59:E6"
 #saml_settings.idp_cert_fingerprint_algorithm = XMLSecurity::Document::SHA1
 
@@ -28,12 +29,12 @@ saml_settings.security[:metadata_signed]         = true     # Enable or not sign
 saml_settings.security[:digest_method]    = XMLSecurity::Document::SHA1
 saml_settings.security[:signature_method] = XMLSecurity::Document::RSA_SHA1
 
-
-# saml_cert_file = File.open(ENV["SAML_CERT"], "rb")
-# saml_settings.certificate = saml_cert_file.read
-
-# saml_pr_key_file = File.open(ENV["SAML_PR_KEY"], "rb")
-# saml_settings.private_key = saml_pr_key_file.read
-
+if ENV["SAML_CERT"].present?
+  saml_cert_file = File.open(ENV["SAML_CERT"], "rb")
+  saml_settings.certificate = saml_cert_file.read if saml_cert_file.present?
+end
+if ENV["SAML_PR_KEY"].present?
+  saml_pr_key_file = File.open(ENV["SAML_PR_KEY"], "rb")
+  saml_settings.private_key = saml_pr_key_file.read if saml_pr_key_file.present?
+end
 SAML_SETTINGS=saml_settings
-
