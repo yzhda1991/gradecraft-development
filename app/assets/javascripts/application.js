6236c6253a1c9a547b54552fb40fe90d020d5cfe
Max Langensiepen <mlangens@emich.edu>
2015-09-30 11:01:24 -0400

    WIP: custom sort

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery.sparkline.min
 //= require s3_direct_upload
 //= require underscore
+//= require alpha_num_sort
 //= require jquery.dynatable
 //= require dynatable
 //= require lodash.min
