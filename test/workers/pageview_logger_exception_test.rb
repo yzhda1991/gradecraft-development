03a2af42cec571f74ffe83b9fae653c5abea0ca0
Mike Zazaian <mike@zop.io>
2015-10-04 23:40:29 +0200

    building out pageview exception test, not triggering in Resque log

--- test/workers/pageview_logger_exception_test.rb
+++ test/workers/pageview_logger_exception_test.rb
@@ -1,21 +1,36 @@
 require_relative '../../app/workers/event_loggers/pageview_event_logger'
 
 class PageviewLoggerWithExceptions < PageviewEventLogger
-  @queue = :pageview_event_logger
+  @queue = :pageview_logger_with_exceptions
   @retry_limit = 3
   @retry_delay = 60
   @start_message = "Starting PageviewEventLogger with exceptions"
-
 
   def self.perform(event_type, data={})
     p @start_message
     p "event_type: #{event_type}"
     begin
-      raise "Some chaos happened"
+      Analytics::Event.create self.event_attrs(event_type, data)
     rescue Exception => e
       puts e.message
       puts e.backtrace.inspect
     end
+  end
+
+  def initialize(attrs={})
+    @attrs = attrs
+  end
+
+  def enqueue_in(time_until_start)
+    Resque.enqueue_in(time_until_start, self.class, 'pageview', @attrs)
+  end
+
+  def enqueue_at(scheduled_time)
+    Resque.enqueue_in(elapsed_time, self.class, 'pageview', @attrs)
+  end
+
+  def enqueue
+    Resque.enqueue(self.class, 'pageview', @attrs)
   end
 end
 
