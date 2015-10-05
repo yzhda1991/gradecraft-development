b83baccacb62f9f6657a2537986a1fbd60718ed3
Mike Zazaian <mike@zop.io>
2015-10-05 18:53:03 +0200

    pass some initial specs for event logger worker class.

--- spec/workers/event_logger_spec.rb
+++ spec/workers/event_logger_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 # PageviewEventLogger.new(pageview_logger_attrs).enqueue_in(ResqueManager.time_until_next_lull)
 RSpec.describe EventLogger, type: :background_job do
   describe "self.perform" do
+    before do
+      @now = Time.parse "Oct 20 1985"
+      allow(EventLogger).to receive(:notify_event_outcome).and_return double(:mongoid_event, valid?: true)
+    end
+
     it "should print the @start_message class instance variable to the log" do
       EventLogger.instance_variable_set(:@start_message, "some message")
       expect(EventLogger).to receive(:p).with("some message")
@@ -10,9 +15,16 @@ RSpec.describe EventLogger, type: :background_job do
     end
 
     it "should create a new analytics event using the event attrs" do
+      allow(EventLogger).to receive_messages(event_type: "event", data: {created_at: @now})
+      expect(Analytics::Event).to receive(:create).with EventLogger.event_attrs("event", {created_at: @now})
+      EventLogger.perform "event", created_at: @now
     end
 
     it "should notify the event outcome to the log" do
+      @mongoid_event = double(:mongoid_event)
+      allow(Analytics::Event).to receive_messages(create: @mongoid_event)
+      expect(EventLogger).to receive(:notify_event_outcome).with @mongoid_event
+      EventLogger.perform "event"
     end
   end
 end
