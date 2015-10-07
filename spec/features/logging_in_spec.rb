008fe9463e82e434edbe587763fae2b64cd13abf
Jamie Wright <jamie@brilliantfantastic.com>
2015-10-07 17:00:05 -0400

    Unsuccessful login spec

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -7,14 +7,21 @@ feature "logging in" do
     let!(:course_membership) { create :student_course_membership, user: user }
     let(:user) { create :user, password: password }
 
-    scenario "logging in with password successfully" do
-      visit root_path
-      LoginPage.new(user).submit({password: password})
+    before { visit root_path }
+
+    scenario "with a password successfully" do
+      LoginPage.new(user).submit({ password: password })
 
       expect(current_path).to eq syllabus_path
       within("header") do
         expect(page).to have_content user.display_name
       end
+    end
+
+    scenario "with an invalid email and password combination" do
+      LoginPage.new(user).submit({ password: "blah" })
+      expect(current_path).to eq user_sessions_path
+      expect(page).to have_error_message "Email or Password were invalid, login failed."
     end
   end
 end
