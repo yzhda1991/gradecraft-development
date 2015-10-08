e6e77bc71171f1a45d3b7858172b262bbbcf3ea2
Mike Zazaian <mike@zop.io>
2015-10-08 14:36:08 +0200

    stub out base specs for new performer implementations

---
+++ spec/workers/grade_export_performer_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe GradebookExportPerformer, type: :background_job do
+  # public methods
+
+  describe "setup" do
+  end
+
+  describe "do_the_work" do
+  end
+
+  describe "outcome_messages" do
+  end
+
+  # private methods
+end
