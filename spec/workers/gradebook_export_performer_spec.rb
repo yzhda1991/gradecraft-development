7845407dd1536418b4d3f75974c3763eb6610733
Mike Zazaian <mike@zop.io>
2015-10-09 14:59:22 +0200

    improve/pass gradebook export performer spec.

--- spec/workers/gradebook_export_performer_spec.rb
+++ spec/workers/gradebook_export_performer_spec.rb
@@ -134,18 +134,21 @@ RSpec.describe GradebookExportPerformer, type: :background_job do
     end
 
     describe "notify_gradebook_export" do
-      let(:csv_data_double) { double(:csv_data) }
       subject { performer.instance_eval { notify_gradebook_export } }
+      let(:csv_data) { performer.instance_variable_get(:@csv_data) }
+      let(:mock_csv) { double(:csv) }
       after(:each) { subject }
+      before(:each) { allow(NotificationMailer).to receive(:gradebook_export).and_return(mock_csv) }
 
       it "should create a new gradebook export notifier with @course, @user, and @csv_data" do
-        performer.instance_variable_set(:@csv_data, csv_data_double)
-        expect(NotificationMailer).to receive(:gradebook_export).with(course, user, csv_data_double)
+        performer.instance_eval { fetch_csv_data }
+        expect(NotificationMailer).to receive(:gradebook_export).with(course, user, csv_data)
+        expect(mock_csv).to receive(:deliver_now)
       end
 
       it "should deliver the mailer" do
-        allow(performer).to receive_messages(gradebook_export: csv_data_double)
-        expect(csv_data_double).to receive(:deliver_now)
+        allow(performer).to receive_messages(gradebook_export:  mock_csv)
+        expect(mock_csv).to receive(:deliver_now)
       end
     end
 
