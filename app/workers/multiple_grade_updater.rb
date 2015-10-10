98513d0ec1e749d81dbf4cc0c917bb92eb7eb5a3
Mike Zazaian <mike@zop.io>
2015-10-10 02:56:40 +0200

    refactor multiple_grade_updater with new syntax. begin building out full spec for multiple grade updater, need to convert all methods to using local variables.

--- app/workers/multiple_grade_updater.rb
+++ app/workers/multiple_grade_updater.rb
@@ -1,4 +1,4 @@
-class MultipleGradeUpdatePerformer < ResqueJob::Performer
+class GradeUpdatePerformer < ResqueJob::Performer
   def setup
     @grade_ids = @attrs[:grade_ids]
     @grades = fetch_grades_with_assignment
@@ -7,36 +7,49 @@ class MultipleGradeUpdatePerformer < ResqueJob::Performer
   # perform() attributes assigned to @attrs in the ResqueJob::Base class
   def do_the_work
     @grades.each do |grade|
-      require_success { grade.save_student_and_team_scores }
-
-      if grade.assignment.notify_released?
-        require_success { NotificationMailer.grade_released(grade.id).deliver_now }
-      end
-    end
-  end
-
-  def outcome_messages
-    if outcome_success?
-      puts "All grades saved and notified correctly."
-    elsif outcome_failure?
-      puts "All grades and notifications failed."
-    else
-      puts "Some grades and notifications succeeded but others failed."
+      require_saved_scores_success(grade)
+      require_notify_released_success(grade)
     end
   end
 
   protected
 
+  def require_saved_scores_success(grade)
+    require_success(save_scores_messages) do
+      grade.save_student_and_team_scores
+    end
+  end
+
+  def require_notify_released_success(grade)
+    if grade.assignment.notify_released?
+      require_success(notify_released_messages) { notify_grade_released(grade) } 
+    end
+  end
+
+  def save_scores_messages(grade)
+    {
+      success: "Student and team scores saved successfully for grade ##{grade.id}",
+      failure: "Student and team scores failed to save for grade ##{grade.id}"
+    }
+  end
+
+  def notify_released_messages(grade)
+    {
+      success: "Successfully sent notification for release of grade ##{grade.id}.",
+      failure: "Failed to send grade release notification for grade ##{grade.id}."
+    }
+  end
+
   def fetch_grades_with_assignment
-    Grade.where(id: grade_ids).includes(:assignment).load
+    Grade.where(id: @attrs[:grade_id]).includes(:assignment).load.first
   end
 
   def notify_grade_released(grade)
-    NotificationMailer.grade_released(grade.id).deliver
+    NotificationMailer.grade_released(grade.id).deliver_now
   end
 end
 
-class MultipleGradeUpdaterJob < ResqueJob::Base
-  @queue = :multiple_grade_updater
-  @performer_class = MultipleGradeUpdatePerformer
+class GradeUpdaterJob < ResqueJob::Base
+  @queue = :grade_updater
+  @performer_class = GradeUpdatePerformer
 end
