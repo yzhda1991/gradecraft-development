f2d91a6f0312f99dfb4dff6679b8b01fc30bd7ac
Mike Zazaian <mike@zop.io>
2015-10-11 01:28:09 +0200

    improve gradebook exporter console test syntax

--- test/performers/gradebook_export_performer_test.rb
+++ test/performers/gradebook_export_performer_test.rb
@@ -1,5 +1,6 @@
 require_relative '../test_helper'
 ENV['TEST_CYCLES'] ||= '3'
+cycles = ENV['TEST_CYCLES']
 
 class GradebookExporterTest
   def initialize
@@ -10,12 +11,22 @@ class GradebookExporterTest
     { user_id: 1, course_id: 3 }
   end
 
-  def run
+  def enqueue
     @job.enqueue
+  end
+
+  def enqueue_in(time)
+    @job.enqueue_in(10)
   end
 end
 
-Rails.logger.info "Starting #{ENV['TEST_CYCLES']} GradebookExporterTest cycles..."
+Rails.logger.info "Starting #{cycles} GradebookExporterTest cycles..."
+Rails.logger.info "Running normal enqueues, should happen right now:"
 ENV['TEST_CYCLES'].to_i.times do
-  GradebookExporterTest.new.run
+  GradebookExporterTest.new.enqueue
+end
+
+Rails.logger.info "Running delayed enqueue_in calls, should happen in ten seconds:"
+ENV['TEST_CYCLES'].to_i.times do
+  GradebookExporterTest.new.enqueue_in(10)
 end
