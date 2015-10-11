cdccf250ec1d27a87bd3260282d9dfd93e9b85f0
Jamie Wright <jamie@brilliantfantastic.com>
2015-10-10 21:28:13 -0400

    Add uploaders to the spec helper

--- spec/active_record_spec_helper.rb
+++ spec/active_record_spec_helper.rb
@@ -1,15 +1,32 @@
 require "base_spec_helper"
 require "active_record"
 require "active_support/core_ext"
+require "carrierwave"
+require "carrierwave/orm/activerecord"
 require "factory_girl"
 require "faker"
 require "protected_attributes"
 require "yaml"
 
-Dir['./app/validators/*.rb'].each { |f| require f }
+# stub out the process_in_background for carrierwave_backgrounder
+module CarrierWave
+  module Backgrounder
+    module Delay; end
+  end
+end
+class ActiveRecord::Base
+  def self.process_in_background(_); end
+end
+
+Dir["./app/uploaders/*.rb"].each { |f| require f }
+Dir["./app/validators/*.rb"].each { |f| require f }
 
 connection_info = YAML.load_file("config/database.yml")["test"]
 ActiveRecord::Base.establish_connection(connection_info)
+
+# supress the warning that is generated from CarrierWave because it uses
+# after_commit/after_save callbacks
+ActiveRecord::Base.raise_in_transactional_callbacks = true
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
