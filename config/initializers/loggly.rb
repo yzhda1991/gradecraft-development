b4bb3bd6f3b2e8aa1952ed8ec4940f3bdf892958
Mike Zazaian <mike@zop.io>
2015-10-16 00:07:29 +0200

    fix bug where Rails logger wouldn't output to loggly on staging

--- config/initializers/loggly.rb
+++ config/initializers/loggly.rb
@@ -1,7 +1,7 @@
 # Rails 4 loggly add to broadcast
 # loggly = Logglier.new("tcp://logs-01.loggly.com:514")
 
-if Rails.env.production? || Rails.env.development?
+unless Rails.env.test?
   loggly = Logglier.new("https://logs-01.loggly.com/inputs/#{ENV['LOGGLY_TOKEN']}/tag/rails-#{Rails.env}", threaded: false, format: :json)
   Rails.logger.extend(ActiveSupport::Logger.broadcast(loggly))
 end
