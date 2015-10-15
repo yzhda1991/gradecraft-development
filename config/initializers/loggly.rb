c4e6d42eea7487ec9b93f46e491906cf60c61ced
Mike Zazaian <mike@zop.io>
2015-10-15 15:29:24 +0200

    fix issue where non-threaded instance of loggly was holding up rspec output

--- config/initializers/loggly.rb
+++ config/initializers/loggly.rb
@@ -1,5 +1,7 @@
 # Rails 4 loggly add to broadcast
 # loggly = Logglier.new("tcp://logs-01.loggly.com:514")
 
-loggly = Logglier.new("https://logs-01.loggly.com/inputs/#{ENV['LOGGLY_TOKEN']}/tag/rails-#{Rails.env}", format: :json)
-Rails.logger.extend(ActiveSupport::Logger.broadcast(loggly))
+if Rails.env.production? || Rails.env.development?
+  loggly = Logglier.new("https://logs-01.loggly.com/inputs/#{ENV['LOGGLY_TOKEN']}/tag/rails-#{Rails.env}", threaded: false, format: :json)
+  Rails.logger.extend(ActiveSupport::Logger.broadcast(loggly))
+end
