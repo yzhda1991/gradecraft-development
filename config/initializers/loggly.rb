dbfe1836dcb1a27e434d903d06c9662e1e34f1b7
Mike Zazaian <mike@zop.io>
2015-10-15 14:30:58 +0200

    turn off threaded logging for primary rails loggly delivery

--- config/initializers/loggly.rb
+++ config/initializers/loggly.rb
@@ -1,5 +1,5 @@
 # Rails 4 loggly add to broadcast
 # loggly = Logglier.new("tcp://logs-01.loggly.com:514")
 
-loggly = Logglier.new("https://logs-01.loggly.com/inputs/#{ENV['LOGGLY_TOKEN']}/tag/rails-#{Rails.env}", threaded: true, format: :json)
+loggly = Logglier.new("https://logs-01.loggly.com/inputs/#{ENV['LOGGLY_TOKEN']}/tag/rails-#{Rails.env}", format: :json)
 Rails.logger.extend(ActiveSupport::Logger.broadcast(loggly))
