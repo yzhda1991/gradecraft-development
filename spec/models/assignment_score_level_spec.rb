f69fe62b4db2569220babef9059c2c25cce4d49a
Jamie Wright <jamie@brilliantfantastic.com>
2015-10-21 10:54:51 -0400

    Add AR spec helper to assignment score level and implement skipped test

--- spec/models/assignment_score_level_spec.rb
+++ spec/models/assignment_score_level_spec.rb
@@ -1,26 +1,29 @@
-require 'spec_helper'
+require "active_record_spec_helper"
 
 describe AssignmentScoreLevel do
+  describe "validations" do
+    subject { build(:assignment_score_level) }
 
-  it "is valid with a name, a value, and an assignment" do
-    assignment_score_level = AssignmentScoreLevel.new(
-      name: "Level 1", value: "1000", assignment_id: "1")
-    expect(assignment_score_level).to be_valid
-  end
+    it "is valid with a name, a value, and an assignment" do
+      expect(subject).to be_valid
+    end
 
-  it "is invalid without a name" do
-    assignment_score_level = AssignmentScoreLevel.new(name: nil)
-    expect(assignment_score_level).to_not be_valid
-    expect(assignment_score_level.errors[:name].count).to eq 1
-  end
+    it "requires a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors[:name]).to include "can't be blank"
+    end
 
-  it "is invalid without an assignment" do
-    skip
-  end
+    it "requires a valid assignment" do
+      subject.assignment.assignment_type_id = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors[:assignment]).to include "is invalid"
+    end
 
-  it "is invalid without a value" do
-    assignment_score_level = AssignmentScoreLevel.new(value: nil)
-    expect(assignment_score_level).to_not be_valid
-    expect(assignment_score_level.errors[:value].count).to eq 1
+    it "requires a value" do
+      subject.value = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors[:value]).to include "can't be blank"
+    end
   end
 end
