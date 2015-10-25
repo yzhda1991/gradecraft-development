88bff846238d1ea684d35fa81f4b5d5ca0643ea9
Jamie Wright <jamie@brilliantfantastic.com>
2015-10-25 09:28:35 -0400

    Removed some unnecessary instance variables

--- spec/controllers/current_courses_controller_spec.rb
+++ spec/controllers/current_courses_controller_spec.rb
@@ -1,25 +1,23 @@
-#spec/controllers/current_courses_controller_spec.rb
 require 'spec_helper'
 
 describe CurrentCoursesController do
-
-	before do
-    @course = create(:course)
+  before do
+    course = create(:course)
     @course_2 = create(:course)
-    @user = create(:user)
-    @user.courses << [@course, @course_2]
+    user = create(:user)
+    user.courses << [course, @course_2]
 
-    login_user(@user)
-    session[:course_id] = @course.id
+    login_user(user)
+    session[:course_id] = course.id
     allow(Resque).to receive(:enqueue).and_return(true)
   end
 
   describe "POST change" do
-  	it "switches the course context" do
-	  	post :change, :course_id => @course_2.id
-	  	expect(response).to redirect_to(root_url)
-	  	expect(session[:course_id]).to eq(@course_2.id)
-	  end
-	end
+    it "switches the course context" do
+      post :change, :course_id => @course_2.id
+      expect(response).to redirect_to(root_url)
+      expect(session[:course_id]).to eq(@course_2.id)
+    end
+  end
 
 end
