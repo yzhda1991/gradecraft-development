22c3635e4424b6f85730c670430b700af8062fa7
chcholman <caitlin.holman@gmail.com>
2015-10-24 23:32:30 -0400

    Testing submission process

--- spec/features/submitting_an_assignment_spec.rb
+++ spec/features/submitting_an_assignment_spec.rb
@@ -4,9 +4,11 @@ feature "submitting an assignment", focus: true do
   let(:password) { "p@ssword" }
 
   context "as a student" do
-    let!(:course_membership) { create :student_course_membership, user: user }
+    let(:course) { create :course }
+    let(:assignment_type) {create :assignment_type, course: course}
+    let(:assignment) {create :individual_assignment_with_submissions, assignment_type: assignment_type}
+    let!(:course_membership) { create :student_course_membership, user: user, course: course }
     let(:user) { create :user, password: password }
-    let(:assignment) {create :individual_assignment_with_submissions}
 
     before { visit root_path }
 
@@ -18,9 +20,7 @@ feature "submitting an assignment", focus: true do
 
       visit syllabus_path
 
-      click_link "#{assignment.name}"
-
-      expect(page).to have_content assignment.name
+      expect(page).to have_content assignment_type.name
     end
 
   end
