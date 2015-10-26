154b495627c21ccdb4849c82c060fc31bb01a73a
Jamie Wright <jamie@brilliantfantastic.com>
2015-10-25 22:18:56 -0400

    Sped up the metrics controller from 1 second to .2

--- spec/controllers/metrics_controller_spec.rb
+++ spec/controllers/metrics_controller_spec.rb
@@ -1,31 +1,19 @@
-#spec/controllers/metrics_controller_spec.rb
 require 'spec_helper'
 
 describe MetricsController do
-
-  context "as a professor" do
-
-    before do
-      @course = create(:course)
-      @professor = create(:user)
-      @professor.courses << @course
-      @membership = CourseMembership.where(user: @professor, course: @course).first.update(role: "professor")
-      @challenge = create(:challenge, course: @course)
-      @course.challenges << @challenge
-      @challenges = @course.challenges
-      @student = create(:user)
-      @student.courses << @course
-      @team = create(:team, course: @course)
-      @team.students << @student
-      @teams = @course.teams
-
-      login_user(@professor)
-      session[:course_id] = @course.id
-      allow(Resque).to receive(:enqueue).and_return(true)
-    end
+  before(:all) { @course = create(:course) }
+  before(:each) do
+    session[:course_id] = @course.id
+    allow(Resque).to receive(:enqueue).and_return(true)
   end
 
   context "as a student" do
+    before(:all) do
+      @student = create(:user)
+      @student.courses << @course
+    end
+    before(:each) { login_user(@student) }
+
     describe "protected routes" do
       [
         :new,
