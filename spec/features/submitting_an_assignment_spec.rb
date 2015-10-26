e1040a9ca6e1fd64817e7fb2bc694ccd9048d181
chcholman <caitlin.holman@gmail.com>
2015-10-26 10:45:09 -0400

    Submitting spec

--- spec/features/submitting_an_assignment_spec.rb
+++ spec/features/submitting_an_assignment_spec.rb
@@ -5,10 +5,11 @@ feature "submitting an assignment", focus: true do
 
   context "as a student" do
     let(:course) { create :course }
-    let(:assignment_type) {create :assignment_type, course: course}
-    let(:assignment) {create :individual_assignment_with_submissions, assignment_type: assignment_type}
     let!(:course_membership) { create :student_course_membership, user: user, course: course }
     let(:user) { create :user, password: password }
+
+    let(:assignment_type) {create :assignment_type, course: course, name: "Assignment Type Name"}
+    let(:assignment) {create :individual_assignment_with_submissions, assignment_type: assignment_type, course: course}
 
     before { visit root_path }
 
@@ -16,11 +17,18 @@ feature "submitting an assignment", focus: true do
       LoginPage.new(user).submit({ password: password })
     end
 
-    scenario "successfully" do
+    scenario "successfully from the assignment page" do
 
-      visit syllabus_path
+      visit assignment_path("#{assignment.id}")
 
-      expect(page).to have_content assignment_type.name
+      click_link "Submit"
+
+      within(".new_submission") do
+        fill_in "Link", with: "http://www.umich.edu"
+        click_button "Submit Assignment"
+      end
+
+      expect(page).to have_notification_message('notice', "#{assignment.name} was successfully submitted.")
     end
 
   end
