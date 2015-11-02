2efaa6aa1f4d2fad7c2ff80a025dcc997ab5aee1
Mike Zazaian <mike@zop.io>
2015-11-02 17:06:24 +0100

    move course student recalculations specs into dedicated spec file to reduce overhead in course_spec requirements

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -53,7 +53,7 @@ describe Course do
 
   describe "#instructors_of_record" do
     it "returns all the staff who are instructors of record for the course" do
-      membership = create :staff_course_membership, course: subject, instructor_of_record: true
+      membership = staff_membership
       expect(subject.instructors_of_record).to eq [membership.user]
     end
   end
@@ -76,98 +76,5 @@ describe Course do
     admin = create :user, admin: true
     course = create :course
     expect(CourseMembership.where(user_id: admin.id, course_id: course.id, role: "admin")).to be_exist
-  end
-
-  let(:course1) { create(:course) }
-  let(:course2) { create(:course) }
-  let(:staff_membership) { create :staff_course_membership, course: course1, instructor_of_record: true }
-  let(:student_membership1) { create :student_course_membership, course: course1 }
-  let(:student_membership2) { create :student_course_membership, course: course1 }
-  let(:student_membership3) { create :student_course_membership, course: course2 }
-
-  describe "recalculate_student_scores" do
-    before do
-      CourseMembership.where(course_id: course1.id).destroy_all
-      @course1 = course1
-      @student_membership1 = student_membership1
-      @student_membership2 = student_membership2
-    end
-
-    before(:each) { ResqueSpec.reset! }
-    subject { @course1.recalculate_student_scores }
-    let(:score_recalculator_queue) { queue(ScoreRecalculatorJob) }
-    let(:student1_job_attributes) {{ user_id: @student_membership1.user_id, course_id: @course1.id }}
-    let(:student2_job_attributes) {{ user_id: @student_membership2.user_id, course_id: @course1.id }}
-
-    context "student ids present" do
-      it "creates a score recalculator job for each student" do
-        expect(ScoreRecalculatorJob).to receive_message_chain(:new, :enqueue).exactly(2).times
-        subject
-      end
-    end
-
-    context "no student ids present" do
-      it "creates creates no score recalculator jobs" do
-        # stub out a no-students scenario
-        allow(@course1).to receive(:ordered_student_ids) { [] }
-        expect(ScoreRecalculatorJob).to receive_message_chain(:new, :enqueue).exactly(0).times
-        subject
-      end
-    end
-
-    it "increases the queue size by two" do
-      expect{ subject }.to change { queue(ScoreRecalculatorJob).size }.by(2)
-    end
-
-    it "queues the job with the correct arguments" do
-      subject
-      expect(score_recalculator_queue.first[:args]).to eq([student1_job_attributes])
-      expect(score_recalculator_queue.last[:args]).to eq([student2_job_attributes])
-    end
-
-    it "queues the job in the proper queue" do
-      subject
-      expect(score_recalculator_queue.first[:class]).to eq(ScoreRecalculatorJob.to_s)
-      expect(score_recalculator_queue.last[:class]).to eq(ScoreRecalculatorJob.to_s)
-    end
-  end
-
-  describe "ordered_student_ids" do
-    before do
-      @course1 = course1
-      @student_membership1 = student_membership1
-      @student_membership2 = student_membership2
-      @student_membership3 = student_membership3
-      @staff_membership = staff_membership
-    end
-
-    subject { @course1.ordered_student_ids }
-
-    it "should order the ids by users.id ASC" do
-      expect(subject).to eq([@student_membership1.user_id, @student_membership2.user_id])
-    end
-
-    it "should only return an array of ids" do
-      expect(subject.collect(&:class)).to eq([Fixnum, Fixnum])
-    end
-
-    context "user is a student not in the course" do
-      it "doesn't include the student's id" do
-        expect(subject).not_to include(@student_membership3.user_id)
-      end
-    end
-
-    context "user is a student in the course" do
-      it "includes the student's id" do
-        expect(subject).to include(@student_membership1.user_id)
-        expect(subject).to include(@student_membership2.user_id)
-      end
-    end
-
-    context "user is a professor in the course" do
-      it "doesn't include the student's id" do
-        expect(subject).not_to include(@staff_membership.user_id)
-      end
-    end
   end
 end
