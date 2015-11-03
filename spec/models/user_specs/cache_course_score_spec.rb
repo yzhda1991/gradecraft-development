c7744e357810f718f3bc8f56fb942ff7f15368d6
Mike Zazaian <mike@zop.io>
2015-11-03 17:32:18 +0100

    add/pass specs for improved_cache_course_score

--- spec/models/user_specs/cache_course_score_spec.rb
+++ spec/models/user_specs/cache_course_score_spec.rb
@@ -11,57 +11,88 @@ describe User do
   describe "#cache_course_score", focus: true do
     subject { student.cache_course_score(course.id) }
 
-    context "course membership is present" do
-      let(:stub_course_membership) { allow(student).to receive(:fetch_course_membership) { course_membership } }
+    it "fetches the proper course membership" do
+      expect(student).to receive(:fetch_course_membership) { course_membership }
+      subject
+    end
 
-      it "fetches the proper course membership" do
-        expect(student).to receive(:fetch_course_membership) { course_membership }
-        subject
-      end
+    context "course membership is present" do
+      before(:each) { allow(student).to receive(:fetch_course_membership) { course_membership } }
 
       it "checks whether the course_membership is nil" do
-        stub_course_membership
         expect(course_membership).to receive(:nil?)
         subject
       end
 
       it "recalculates and updates the student score" do
-        stub_course_membership
         expect(course_membership).to receive(:recalculate_and_update_student_score)
         subject
       end
 
       it "returns the the recalculated student score" do
-        stub_course_membership
         allow(course_membership).to receive(:recalculate_and_update_student_score) { 58000 }
         expect(subject).to eq(58000)
       end
     end
 
     context "course membership is nil" do
-      let(:stub_course_membership) { allow(student).to receive(:fetch_course_membership) { nil } }
+      before(:each) { allow(student).to receive(:fetch_course_membership) { nil } }
 
       it "doesn't recalculate the student score" do
-        stub_course_membership
         expect(course_membership).not_to receive(:recalculate_and_update_student_score)
         subject
       end
 
       it "returns nil" do
-        stub_course_membership
         expect(subject).to eq(nil)
       end
     end
   end
 
   describe "#improved_cache_course_score", focus: true do
+    subject { student.improved_cache_course_score(course.id) }
+
+    it "fetches the proper course membership" do
+      expect(student).to receive(:fetch_course_membership) { course_membership }
+      subject
+    end
+
     context "course membership is present" do
+      before(:each) { allow(student).to receive(:fetch_course_membership) { course_membership } }
+
+      it "checks whether the course_membership is nil" do
+        expect(course_membership).to receive(:nil?)
+        subject
+      end
+
       it "recalculates and updates the student score" do
+        allow(course_membership).to receive(:recalculated_student_score) { 79000 }
+        expect(course_membership).to receive(:recalculated_student_score)
+        subject
+      end
+
+      it "updates the course membership with the recalculated score" do
+        allow(course_membership).to receive(:recalculated_student_score) { 64000 }
+        expect(course_membership).to receive(:update_attribute).with(:score, 64000)
+        subject
+      end
+
+      it "returns the the recalculated student score" do
+        allow(course_membership).to receive(:recalculated_student_score) { 58000 }
+        expect(subject).to eq(58000)
       end
     end
 
     context "course membership is nil" do
+      before(:each) { allow(student).to receive(:fetch_course_membership) { nil } }
+
       it "doesn't recalculate the student score" do
+        expect(course_membership).not_to receive(:recalculate_and_update_student_score)
+        subject
+      end
+
+      it "returns zero" do
+        expect(subject).to eq(0)
       end
     end
   end
