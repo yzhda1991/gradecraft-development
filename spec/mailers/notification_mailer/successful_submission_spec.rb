00efb0053a53673bd63dc45069a5cc73f1639455
Mike Zazaian <mike@zop.io>
2015-11-05 16:34:56 +0100

    build out/pass spec for successful_submission notification mailer, text and html examples passed

---
+++ spec/mailers/notification_mailer/successful_submission_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_spec_helper'
+
+describe NotificationMailer do
+  let(:email) { ActionMailer::Base.deliveries.last }
+  let(:sender) { NotificationMailer::SENDER_EMAIL }
+  let(:admin_email) { NotificationMailer::ADMIN_EMAIL }
+  let(:text_part) { email.body.parts.detect {|part| part.content_type.match "text/plain" }}
+  let(:html_part) { email.body.parts.detect {|part| part.content_type.match "text/html" }}
+
+  describe "#successful_submission" do
+    let(:submission) { create(:submission, course: course, student: student, assignment: assignment) }
+    let(:student) { create(:user) }
+    let(:course) { create(:course, assignment_term: "whifflebox") }
+    let(:assignment) { create(:assignment) }
+
+    before(:each) do
+      NotificationMailer.successful_submission(submission.id).deliver_now
+    end
+
+    it "is sent from gradecraft's default mailer email" do
+      expect(email.from).to eq [sender]
+    end
+
+    it "is sent to the student's email" do
+      expect(email.to).to eq [student.email]
+    end
+
+    it "has the correct subject" do
+      expect(email.subject).to eq "#{course.courseno} - #{assignment.name} Submitted"
+    end
+
+    describe "text part body" do
+      subject { text_part.body }
+      it "includes the student's first name" do
+        should include student.first_name
+      end
+
+      it "includes the assignment name" do
+        should include assignment.name
+      end
+
+      it "includes the assignment term for the course" do
+        should include course.assignment_term.pluralize.downcase
+      end
+
+      it "includes the course name" do
+        should include course.name
+      end
+
+      it "includes the submission created_at timestamp" do
+        should include submission.created_at
+      end
+
+      it "doesn't include a template" do
+        should_not include "Regents of The University of Michigan"
+      end
+    end
+
+    describe "html part body" do
+      subject { html_part.body }
+      it "includes the student's first name" do
+        should include student.first_name
+      end
+
+      it "includes the assignment name" do
+        should include assignment.name
+      end
+
+      it "includes the assignment term for the course" do
+        should include course.assignment_term.pluralize.downcase
+      end
+
+      it "includes the course name" do
+        should include course.name
+      end
+
+      it "includes the submission created_at timestamp" do
+        should include submission.created_at
+      end
+
+      it "should use include a template" do
+        should include "Regents of The University of Michigan"
+        should include "DOCTYPE"
+      end
+    end
+  end
+end
