63c23cee5de42ba73d5f0b76196f90b9de38464c
Mike Zazaian <mike@zop.io>
2015-11-06 11:58:33 +0100

    clean up email submission templates. add/pass specs for initial submissions export specs.

--- app/mailers/exports_mailer.rb
+++ app/mailers/exports_mailer.rb
@@ -2,32 +2,32 @@ class ExportsMailer < ApplicationMailer
   layout "mailers/exports_layout"
 
   def submissions_export_success(professor, assignment, archive_data)
-    cache_submission_attrs(professor, assignment)
-    mail(default_attrs.merge(:subject => "Submissions export for assignment #{@assignment.name} is ready.")) do |format|
+    cache_submission_attrs(professor, assignment, archive_data)
+    mail(default_attrs.merge(:subject => "Submissions export for assignment #{@assignment.name} is ready")) do |format|
       format.text
       format.html
     end
   end
 
   def submissions_export_failure(professor, assignment, archive_data)
-    cache_submission_attrs(professor, assignment)
-    mail(default_attrs.merge(:subject => "Submissions export for assignment #{@assignment.name} failed.")) do |format|
+    cache_submission_attrs(professor, assignment, archive_data)
+    mail(default_attrs.merge(:subject => "Submissions export for assignment #{@assignment.name} failed to build")) do |format|
       format.text
       format.html
     end
   end
 
   def team_submissions_export_success(professor, assignment, team, archive_data)
-    cache_team_submission_attrs(professor, assignment, team)
-    mail(default_attrs.merge(:subject => "Submissions export for team #{@team.name} is ready.")) do |format|
+    cache_team_submission_attrs(professor, assignment, team, archive_data)
+    mail(default_attrs.merge(:subject => "Submissions export for team #{@team.name} is ready")) do |format|
       format.text
       format.html
     end
   end
 
   def team_submissions_export_failed(professor, assignment, team, archive_data)
-    cache_team_submission_attrs(professor, assignment, team)
-    mail(default_attrs.merge(:subject => "Submissions export for team #{@team.name} failed.")) do |format|
+    cache_team_submission_attrs(professor, assignment, team, archive_data)
+    mail(default_attrs.merge(:subject => "Submissions export for team #{@team.name} failed to build")) do |format|
       format.text
       format.html
     end
@@ -38,18 +38,22 @@ class ExportsMailer < ApplicationMailer
   def default_attrs
     {
       to: @professor.email,
-      bcc: ADMIN_EMAIL
+      bcc: ExportsMailer::ADMIN_EMAIL
     }
   end
 
-  def cache_submission_attrs(professor, assignment)
+  def cache_submission_attrs(professor, assignment, archive_data)
     @professor = professor
     @assignment = assignment
+    @course = assignment.course
+    @archive_data = archive_data
   end
 
-  def cache_team_submission_attrs(professor, assignment, team)
+  def cache_team_submission_attrs(professor, assignment, team, archive_data)
     @professor = professor
     @assignment = assignment
+    @course = assignment.course
     @team = team
+    @archive_data = archive_data
   end
 end
---
+++ spec/mailers/exports_mailer/submissions_export_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_spec_helper'
+
+RSpec.shared_examples "a complete submissions export email body" do 
+  it "includes the professor's first name" do
+    should include professor.first_name
+  end
+
+  it "includes the assignment name" do
+    should include assignment.name
+  end
+
+  it "includes the assignment term for the course" do
+    should include course.assignment_term.downcase
+  end
+
+  it "includes the course name" do
+    should include course.name
+  end
+end
+
+RSpec.shared_examples "a submissions export email with archive data" do 
+  it "includes the archive format" do
+    should include archive_data[:format].upcase
+  end
+
+  it "includes the archive url" do
+    should include archive_data[:url]
+  end
+end
+
+# specs for submission notifications that are sent to students
+describe NotificationMailer do
+  extend Toolkits::Mailers::EmailToolkit::Definitions # brings in helpers for default emails and parts
+  define_email_context # taken from the definitions toolkit
+
+  include Toolkits::Mailers::EmailToolkit::SharedExamples # brings in shared examples for emails and parts
+
+  let(:professor) { create(:user) }
+  let(:assignment) { create(:assignment, course: course) }
+  let(:course) { create(:course, assignment_term: "deception") }
+  let(:archive_data) {{ format: "zip", url: "http://aws.com/some-archive-hash" }}
+
+  before(:each) { deliver_email }
+
+  describe "#submissions_export_success" do
+    let(:deliver_email) { ExportsMailer.submissions_export_success(professor, assignment, archive_data).deliver_now }
+
+    it_behaves_like "a gradecraft email to a professor"
+
+    it "BCC's to the gradecraft admin" do
+      expect(email.bcc).to eq [admin_email]
+    end
+
+    it "has the correct subject" do
+      expect(email.subject).to eq "Submissions export for assignment #{assignment.name} is ready"
+    end
+
+    describe "text part body" do
+      subject { text_part.body }
+      it_behaves_like "a complete submissions export email body"
+      it_behaves_like "a submissions export email with archive data"
+      it_behaves_like "an email text part"
+    end
+
+    describe "html part body" do
+      subject { html_part.body }
+      it_behaves_like "a complete submissions export email body"
+      it_behaves_like "a submissions export email with archive data"
+      it_behaves_like "an email html part"
+    end
+  end
+
+  describe "#submissions_export_failure" do
+    let(:deliver_email) { ExportsMailer.submissions_export_failure(professor, assignment, archive_data).deliver_now }
+
+    it_behaves_like "a gradecraft email to a professor"
+
+    it "BCC's to the gradecraft admin" do
+      expect(email.bcc).to eq [admin_email]
+    end
+
+    it "has the correct subject" do
+      expect(email.subject).to eq "Submissions export for assignment #{assignment.name} failed to build"
+    end
+
+    describe "text part body" do
+      subject { text_part.body }
+      it_behaves_like "a complete submissions export email body"
+      it_behaves_like "an email text part"
+    end
+
+    describe "html part body" do
+      subject { html_part.body }
+      it_behaves_like "a complete submissions export email body"
+      it_behaves_like "an email html part"
+    end
+  end
+
+end
