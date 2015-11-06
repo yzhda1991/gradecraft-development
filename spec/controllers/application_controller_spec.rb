d77a2d2fdcbc9dcdcf780b34605c04a79762aaa5
Mike Zazaian <mike@zop.io>
2015-11-06 13:21:18 +0100

    clean a bunch of nonsense out the application controller spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,40 +3,4 @@ require 'spec_helper'
 
 describe ApplicationController do
 
-  describe "check_url"
-
-  describe "not_authenticated"
-
-  describe "get_course_scores"
-
-  describe "ensure_student?"
-
-  describe "ensure_staff?"
-
-  describe "ensure_prof?"
-
-  describe "ensure_admin?"
-
-  describe "export_zip"
-
-  describe "enforce_view_permission"
-
-  describe "increment_page_views"
-
-  describe "log_course_login_event"
-
-  describe "temp_view_context" do
-    it "should return a temporary view context" do
-      pending
-    end
-
-    it "should be inherited by subclasses" do
-      pending
-    end
-
-    it "should be protected but not private" do
-      # use this if there are methods available for expressly checking whether a method on a class is 'protected'
-      pending
-    end
-  end
 end
