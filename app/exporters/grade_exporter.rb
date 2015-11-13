3dd65335adf653074cec856b288ce39dc41abe52
Jamie Wright <jamie@brilliantfantastic.com>
2015-11-13 17:16:59 -0500

    Use NullGrade instead of try's for GradeExporter

--- app/exporters/grade_exporter.rb
+++ app/exporters/grade_exporter.rb
@@ -3,9 +3,9 @@ class GradeExporter
     CSV.generate(options) do |csv|
       csv << headers
       students.each do |student|
-        grade = student.grade_for_assignment(assignment)
+        grade = student.grade_for_assignment(assignment) || NullGrade.new
         csv << [student.first_name, student.last_name,
-                student.email, grade.try(:score) || "", grade.try(:feedback) || ""]
+                student.email, grade.score || "", grade.feedback || ""]
       end
     end
   end
