2a93f98810c954ce42d905cb385ae29552035366
Jamie Wright <jamie@brilliantfantastic.com>
2015-11-13 15:52:13 -0500

    Add csv details for each student

--- app/exporters/assignment_exporter.rb
+++ app/exporters/assignment_exporter.rb
@@ -2,6 +2,16 @@ class AssignmentExporter
   def export(assignment, students, options={})
     CSV.generate(options) do |csv|
       csv << headers
+      students.each do |student|
+        grade = student.grade_for_assignment(assignment)
+        submission = student.submission_for_assignment(assignment)
+        if grade and (grade.instructor_modified? || grade.graded_or_released?)
+          csv << [student.first_name, student.last_name,
+                  student.username, grade.try(:score) || "", grade.try(:raw_score) || "",
+                  submission.try(:text_comment) || "", grade.try(:feedback) || "",
+                  grade.try(:updated_at) || ""]
+        end
+      end
     end
   end
 
