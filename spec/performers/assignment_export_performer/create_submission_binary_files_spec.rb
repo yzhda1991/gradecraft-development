80d28962a286e59d9a5d1867695fdff9b7ef27e6
Mike Zazaian <mike@zop.io>
2015-11-23 17:30:54 -0500

    refactor spec for testing binary submission files, pass examples

--- spec/performers/assignment_export_performer/create_submission_binary_files_spec.rb
+++ spec/performers/assignment_export_performer/create_submission_binary_files_spec.rb
@@ -11,43 +11,24 @@ RSpec.describe AssignmentExportPerformer, type: :background_job do
   subject { performer }
 
   describe "creating submission binary files" do
-    let(:submissions) { [ submission_double1, submission_double2 ] }
+    let(:submissions) { [ submission_with_files, submission_without_files ] }
+    let(:submission_with_files) { double(:submission, submission_files: true) }
+    let(:submission_without_files) { double(:submission, submission_files: false) }
 
     describe "create_submission_binary_files" do
-      subject { performer.instance_eval { create_submission_binary_files }}
-      before(:each) do
-        performer.instance_variable_set(:@submissions, submissions)
-      end
+      subject { performer.instance_eval { create_submission_binary_files } }
+      before { performer.instance_variable_set(:@submissions, submissions) }
 
       describe "submission with files" do
-        let(:submission_with_files) { double(:submission, submission_files: true) }
-
-        before(:each) do
-          submissions.each do |submission|
-            allow(performer).to receive(:create_binary_files_for_submission).with(submission) { true }
-          end
-        end
-
-        it "creates binary files for each submission" do
-          submissions.each do |submission|
-            expect(performer).to receive(:create_binary_files_for_submissions).with(submission) { true }
-          end
+        it "creates a binary file for that submission" do
+          expect(performer).to receive(:create_binary_files_for_submission).with(submission_with_files)
           subject
         end
       end
 
-      context "submissions don't have submission files" do
-        let(:submission_double1) { double(:submission, submission_files: false) }
-        let(:submission_double2) { double(:submission, submission_files: false) }
-
-        before(:each) do
-          submissions.each {|submission| allow(submission).to receive(:submission_files) { false }}
-        end
-
-        it "doesn't create binary files for each submission" do
-          submissions.each do |submission|
-            expect(performer).not_to receive(:create_binary_files_for_submissions).with(submission)
-          end
+      describe "submission without files" do
+        it "doesn't create a binary file for that submission" do
+          expect(performer).not_to receive(:create_binary_files_for_submission).with(submission_without_files)
           subject
         end
       end
