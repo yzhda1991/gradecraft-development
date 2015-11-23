2d5c81fc8114c3880203bc303e0c5e12919e4a30
Mike Zazaian <mike@zop.io>
2015-11-23 15:45:51 -0500

    break assignment export performer context out into extensible context module, begin breaking out additional spec components into individual files

---
+++ spec/performers/assignment_export_performer/student_directories_spec.rb
@@ -0,0 +1,109 @@
+require 'rails_spec_helper'
+
+RSpec.describe AssignmentExportPerformer, type: :background_job do
+  include PerformerToolkit::SharedExamples
+  include Toolkits::Performers::AssignmentExport::SharedExamples
+  include ModelAddons::SharedExamples
+
+  extend Toolkits::Performers::AssignmentExport::Context
+  define_context
+
+  subject { performer }
+
+  describe "student directories" do
+    let(:tmp_dir_path) { "/tmp/123-456-abc-xyz" }
+    let(:student_doubles) { [ student_double1, student_double2 ] }
+    let(:stub_student_doubles) { performer.instance_variable_set(:@students, student_doubles) }
+    let(:student_double1) { double(:student, formatted_key_name: "stevens_anna-10") }
+    let(:student_double2) { double(:student, formatted_key_name: "rogers_tina-20") }
+    let(:student_dir_path1) { "#{tmp_dir_path}/stevens_anna-10" }
+    let(:student_dir_path2) { "#{tmp_dir_path}/rogers_tina-20" }
+    let(:student_dir_paths) { [ student_dir_path1, student_dir_path2 ] }
+    let(:remove_student_dirs) do
+      [ student_dir_path1, student_dir_path2 ].each do |dir_path|
+        Dir.rmdir(dir_path) if Dir.exist?(dir_path)
+      end
+    end
+
+    before(:each) do
+      allow(performer).to receive(:tmp_dir) { tmp_dir_path }
+    end
+
+    describe "missing_student_directories" do
+      subject { performer.instance_eval { missing_student_directories }}
+      before(:each) { stub_student_doubles }
+
+      context "student directories are missing" do
+        it "returns the names of the missing directories relative to the tmp_dir" do
+          expect(subject).to eq(["stevens_anna-10", "rogers_tina-20"])
+        end
+      end
+
+      context "student directories have been created" do
+        it "returns an empty array" do
+          performer.instance_eval { create_student_directories }
+          expect(subject).to be_empty
+        end
+        after { remove_student_dirs }
+      end
+    end
+
+    describe "student_directories_created_successfully?" do
+      subject { performer.instance_eval { student_directories_created_successfully? }}
+
+      context "missing_student_directories is empty" do
+        it "returns true" do
+          allow(performer).to receive(:missing_student_directories) { [] }
+          expect(subject).to be_truthy
+        end
+      end
+
+      context "missing_student_directories are present" do
+        it "returns false" do
+          allow(performer).to receive(:missing_student_directories) { ["stevens_anna-10", "rogers_tina-20"] }
+          expect(subject).to be_falsey
+        end
+      end
+    end
+
+    describe "create_student_directories" do
+      subject { performer.instance_eval { create_student_directories }}
+      before(:each) { stub_student_doubles }
+
+      it "calls Dir.mkdir once for each student in @students" do
+        expect(Dir).to receive(:mkdir).exactly(2).times
+        subject
+      end
+
+      it "creates the directories for each student" do
+        student_dir_paths.each {|dir_path| expect(Dir).to receive(:mkdir).with(dir_path) }
+        subject
+      end
+
+      it "actually creates the directories on disk for each student" do
+        subject
+        student_dir_paths.each {|dir_path| expect(Dir.exist?(dir_path)).to be_truthy }
+      end
+
+      after(:each) do
+        remove_student_dirs
+      end
+    end
+
+    describe "student_directory_path" do
+      subject { performer.instance_eval { student_directory_path( @some_student ) }}
+      before do
+        performer.instance_variable_set(:@some_student, student1)
+      end
+
+      it "returns the correct directory path" do
+        expect(subject).to eq("#{tmp_dir_path}/#{student1.formatted_key_name}")
+      end
+
+      it "expands the path relative to the tmp_dir" do
+        expect(File).to receive(:expand_path).with(student1.formatted_key_name, tmp_dir_path)
+        subject
+      end
+    end
+  end
+end
--- spec/toolkits/performers/assignment_export_toolkit.rb
+++ spec/toolkits/performers/assignment_export_toolkit.rb
@@ -2,34 +2,62 @@ module Toolkits
   module Performers
     module AssignmentExport
 
-      RSpec.shared_examples "an expandable messages hash" do
-        it "expands the base messages" do
-          expect(performer).to receive(:expand_messages)
-          subject
+      module Context
+        def define_context
+          # public methods
+          let(:course) { @course ||= create(:course) }
+          let(:professor_course_membership) { @professor_course_membership ||= create(:professor_course_membership, course: course) }
+          let(:professor) { @professor ||= professor_course_membership.user }
+          let(:assignment) { @assignment ||= create(:assignment, course: course) }
+          let(:team) { @team ||= create(:team) }
+          let(:student_course_membership1) { @student_course_membership1 ||= create(:student_course_membership, course: course) }
+          let(:student_course_membership2) { @student_course_membership2 ||= create(:student_course_membership, course: course) }
+          let(:students) { @students ||= [ student_course_membership1.user, student_course_membership2.user ] }
+          let(:student1) { student_course_membership1.user }
+          let(:student2) { student_course_membership2.user }
+          let(:submission1) { create(:submission, assignment: assignment, student: student_course_membership1.user) }
+          let(:submission2) { create(:submission, assignment: assignment, student: student_course_membership2.user) }
+          let(:submissions) { [ submission1, submission2 ] }
+
+          let(:job_attrs) {{ professor_id: professor.id, assignment_id: assignment.id }}
+          let(:job_attrs_with_team) { job_attrs.merge(team_id: team.try(:id)) }
+
+          let(:performer) { AssignmentExportPerformer.new(job_attrs) }
+          let(:performer_with_team) { AssignmentExportPerformer.new(job_attrs_with_team) }
         end
       end
 
-      RSpec.shared_examples "it has a success message" do |message|
-        it "has a success message" do
-          expect(subject[:success]).to match(message)
-        end
-      end
+      module SharedExamples
 
-      RSpec.shared_examples "it has a failure message" do |message|
-        it "has a failure message" do
-          expect(subject[:failure]).to match(message)
-        end
-      end
-
-      RSpec.shared_examples "a created student directory" do |dir_path|
-        it "actually creates the directory on disk" do
-          subject
-          expect(File.exist?(dir_path)).to be_truthy
+        RSpec.shared_examples "an expandable messages hash" do
+          it "expands the base messages" do
+            expect(performer).to receive(:expand_messages)
+            subject
+          end
         end
 
-        it "makes a directory for the student path" do
-          expect(Dir).to receive(:mkdir).with(dir_path)
-          subject
+        RSpec.shared_examples "it has a success message" do |message|
+          it "has a success message" do
+            expect(subject[:success]).to match(message)
+          end
+        end
+
+        RSpec.shared_examples "it has a failure message" do |message|
+          it "has a failure message" do
+            expect(subject[:failure]).to match(message)
+          end
+        end
+
+        RSpec.shared_examples "a created student directory" do |dir_path|
+          it "actually creates the directory on disk" do
+            subject
+            expect(File.exist?(dir_path)).to be_truthy
+          end
+
+          it "makes a directory for the student path" do
+            expect(Dir).to receive(:mkdir).with(dir_path)
+            subject
+          end
         end
       end
 
