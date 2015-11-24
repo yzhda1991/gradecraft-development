8b19ec0ea1100925eb5b7d9fd1de703bee462a3f
Jamie Wright <jamie@brilliantfantastic.com>
2015-11-24 10:30:50 -0500

    Wrap test around non-UM email address

--- spec/features/creating_a_new_user_spec.rb
+++ spec/features/creating_a_new_user_spec.rb
@@ -11,23 +11,33 @@ feature "creating a new user" do
     end
 
     context "for a UM student" do
-      let(:user) { build(:user, email: "#{Faker::Internet.user_name}@umich.edu") }
-
       scenario "successfully" do
+        user = build(:user, email: "#{Faker::Internet.user_name}@umich.edu")
         within(".pageContent") do
           NewUserPage.new(user)
             .submit(internal: true, courses: [course_membership.course])
-
-          expect(current_path).to eq students_path
-          expect(page).to have_notification_message "notice", "#{course_membership.course.user_term} #{user.name} was successfully created!"
-
-          result = User.unscoped.last
-          expect(result.first_name).to eq user.first_name
-          expect(result.last_name).to eq user.last_name
-          expect(result.email).to eq user.email
-          expect(result.username).to eq user.username
-          expect(result.crypted_password).to be_nil
         end
+
+        expect(current_path).to eq students_path
+        expect(page).to have_notification_message "notice", "#{course_membership.course.user_term} #{user.name} was successfully created!"
+
+        result = User.unscoped.last
+        expect(result.first_name).to eq user.first_name
+        expect(result.last_name).to eq user.last_name
+        expect(result.email).to eq user.email
+        expect(result.username).to eq user.username
+        expect(result.crypted_password).to be_nil
+      end
+
+      scenario "with a non-umich email address" do
+        user = build(:user)
+        within(".pageContent") do
+          NewUserPage.new(user)
+            .submit(internal: true, courses: [course_membership.course])
+        end
+
+        expect(current_path).to eq users_path
+        expect(page).to have_notification_message :alert, "Email must be a University of Michigan email"
       end
     end
   end
