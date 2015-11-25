39003b65fc81fc6480d06c71d66696d0c3247350
chcholman <caitlin.holman@gmail.com>
2015-11-25 17:18:25 -0500

    Added tests for team validation, removed unused methods

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -5,20 +5,12 @@ class TeamMembership < ActiveRecord::Base
   belongs_to :team
   belongs_to :student, class_name: 'User'
 
+  validates_presence_of  :team, :student
+
   scope :for_course, ->(course) do
     joins(:team).where(teams: {course_id: course.id})
   end
+
   scope :for_student, ->(student) { where(student_id: student.id) }
 
-  def team_score
-    Grade
-      .where(course_id: course.id)
-      .where(student_id: user.id)
-      .sum(&:score)
-  end
-
-  def challenge_grade_score
-    ChallengeGrade
-      .where(team_id: team_id)
-  end
 end
