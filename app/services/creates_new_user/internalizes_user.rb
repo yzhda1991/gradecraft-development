0ac42b68679d83622d48ac32bed49f12065cd9c8
Jamie Wright <jamie@brilliantfantastic.com>
2015-11-24 19:59:30 -0500

    Create the username and email from the cooresponding fields

--- app/services/creates_new_user/internalizes_user.rb
+++ app/services/creates_new_user/internalizes_user.rb
@@ -7,7 +7,22 @@ module Services
 
       executed do |context|
         user = context[:user]
-        user.kerberos_uid = user.username if user.internal?
+
+        if user.internal?
+          user.username = username_from_email(user.email) if user.username.blank?
+          user.email = email_from_username(user.username) if user.email.blank?
+          user.kerberos_uid = user.username
+        end
+      end
+
+      private
+
+      def self.email_from_username(username)
+        "#{username}@umich.edu"
+      end
+
+      def self.username_from_email(email)
+        email.split(/@/).first
       end
     end
   end
--- spec/services/creates_new_user_spec/internalizes_user_spec.rb
+++ spec/services/creates_new_user_spec/internalizes_user_spec.rb
@@ -3,7 +3,7 @@ require "active_record_spec_helper"
 require "./app/services/creates_new_user/internalizes_user"
 
 describe Services::Actions::InternalizesUser do
-  let(:user) { build :user, password: nil, internal: true }
+  let(:user) { build :user, email: "blah@example.com", password: nil, username: "bleh", internal: true }
 
   it "expects a user to internalize" do
     expect { described_class.execute }.to \
@@ -13,5 +13,17 @@ describe Services::Actions::InternalizesUser do
   it "sets the kerberos id to the username" do
     result = described_class.execute user: user
     expect(result[:user].kerberos_uid).to eq user.username
+  end
+
+  it "creates the username from the email address" do
+    user.username = nil
+    result = described_class.execute user: user
+    expect(result[:user].username).to eq "blah"
+  end
+
+  it "creates the email address from the username" do
+    user.email = nil
+    result = described_class.execute user: user
+    expect(result[:user].email).to eq "bleh@umich.edu"
   end
 end
