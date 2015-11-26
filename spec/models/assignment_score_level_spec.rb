89cbb1aeb638671791b340e72b30b3deec7e74fa
chcholman <caitlin.holman@gmail.com>
2015-11-26 10:25:50 -0500

    Test coverage for assignment score level

--- spec/models/assignment_score_level_spec.rb
+++ spec/models/assignment_score_level_spec.rb
@@ -1,8 +1,11 @@
 require "active_record_spec_helper"
 
 describe AssignmentScoreLevel do
+  
+  let(:assignment) {create :assignment}
+  subject { build(:assignment_score_level, assignment: assignment) }
+
   describe "validations" do
-    subject { build(:assignment_score_level) }
 
     it "is valid with a name, a value, and an assignment" do
       expect(subject).to be_valid
@@ -24,6 +27,24 @@ describe AssignmentScoreLevel do
       subject.value = nil
       expect(subject).to_not be_valid
       expect(subject.errors[:value]).to include "can't be blank"
+    end
+  end
+
+  describe "#formatted_name" do 
+    it "returns the level name followed by the point values in paraentheses" do 
+      subject.name = "Level Name"
+      subject.value = 10000
+      expect(subject.formatted_name).to eq("Level Name (10000 points)")
+    end
+  end
+
+  describe "#copy" do
+    it "creates an identical level" do 
+      subject.name = "Example Name"
+      subject.value = 10
+      new_subject = subject.copy
+      expect(new_subject.name).to eq("Example Name")
+      expect(new_subject.value).to eq(10)
     end
   end
 end
