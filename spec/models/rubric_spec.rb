8f49e692a233a00982227b4956c2a0e73fed7582
chcholman <caitlin.holman@gmail.com>
2015-11-26 16:47:22 -0500

    Test coverage on rubric

--- spec/models/rubric_spec.rb
+++ spec/models/rubric_spec.rb
@@ -1,6 +1,20 @@
 require "active_record_spec_helper"
 
 describe Rubric do
+
+  describe "validations" do 
+    let(:rubric) { build :rubric}
+
+    it "is valid with an assignment" do 
+      expect(rubric).to be_valid
+    end 
+
+    it "is invalid without an assignment" do 
+      rubric.assignment = nil
+      expect(rubric).to be_invalid
+    end
+  end 
+
   describe "#copy" do
     let(:rubric) { build :rubric }
     subject { rubric.copy }
@@ -12,4 +26,34 @@ describe Rubric do
       expect(subject.metrics.map(&:rubric_id)).to eq [subject.id]
     end
   end
+
+  describe "#max_tier_count" do 
+    let(:rubric) { build :rubric }
+
+    it "returns the maximum number of tiers present for any metric" do 
+      metric_1 = create(:metric, rubric: rubric) 
+      metric_2 = create(:metric, rubric: rubric)
+      tier_1 = create(:tier, metric: metric_1)
+      tier_2 = create(:tier, metric: metric_1)
+      tier_3 = create(:tier, metric: metric_1)
+
+      #count is the number created + the zero credit and the max credit
+      expect(rubric.max_tier_count).to eq(5)
+    end
+  end
+
+  describe "#designed?" do
+    let(:rubric) { build :rubric }
+
+    it "returns true if metrics are present" do 
+      metric = create(:metric, rubric: rubric)
+
+      expect(rubric.designed?).to eq(true)
+    end
+
+    it "returns false if metrics are not present" do 
+      expect(rubric.designed?).to eq(false)
+    end
+  end
+
 end
