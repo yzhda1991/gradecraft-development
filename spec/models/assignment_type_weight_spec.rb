eb323b4983b87869adfcf37dc79037141abef779
chcholman <caitlin.holman@gmail.com>
2015-11-26 16:18:49 -0500

    Add test coverage for validation method on assignment type weight struct

--- spec/models/assignment_type_weight_spec.rb
+++ spec/models/assignment_type_weight_spec.rb
@@ -2,6 +2,28 @@ require "active_record_spec_helper"
 
 describe AssignmentTypeWeight do
 
+  describe "validations" do 
+    it "is valid if the weight is less than or equal to the max weight" do 
+      course = create(:course, max_assignment_weight: 4)
+      assignment_type = create(:assignment_type, course: course)
+      student = create(:user)
+      assignment_weight = create(:assignment_weight, student: student, assignment_type: assignment_type, course: course, weight: 3)
+      subject = AssignmentTypeWeight.new(student, assignment_type)
+
+      expect(subject).to be_valid
+    end 
+
+    it "is invalid if the weight is more than the max weight" do 
+      course = create(:course, max_assignment_weight: 4)
+      assignment_type = create(:assignment_type, course: course)
+      student = create(:user)
+      assignment_weight = create(:assignment_weight, student: student, assignment_type: assignment_type, course: course, weight: 5)
+      subject = AssignmentTypeWeight.new(student, assignment_type)
+
+      expect(subject).to be_invalid
+    end 
+  end
+
   describe "#weight" do 
 
     it "returns the value of the first assignment weight in the type for the student if present" do 
