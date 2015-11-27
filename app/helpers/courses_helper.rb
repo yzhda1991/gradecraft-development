ea0f6308c93b8a1dc9ad736e81560501861cab8b
Jamie Wright <jamie@brilliantfantastic.com>
2015-11-27 15:13:46 -0500

    Helper to return the available roles for the specific current user roles

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -7,6 +7,16 @@ module CoursesHelper
     multi_cache_key :current_user_archived_courses, user
   end
 
+  def available_roles(course)
+    roles = [["Student", "student"]]
+    if current_user.is_professor?(course) || current_user.is_admin?(course)
+      roles << ["GSI", "gsi"]
+      roles << ["Professor", "professor"]
+    end
+    roles << ["Admin", "admin"] if current_user.is_admin?(course)
+    roles
+  end
+
   def bust_course_list_cache(user)
     expire_fragment current_courses_cache_key(user)
     expire_fragment archived_courses_cache_key(user)
---
+++ spec/helpers/course_helper_spec.rb
@@ -0,0 +1,51 @@
+require "spec_helper"
+require "./app/helpers/courses_helper"
+
+describe CoursesHelper do
+  let(:user) { double(:user) }
+
+  class Helper
+    include CoursesHelper
+
+    def current_user
+    end
+  end
+
+  subject(:helper) { Helper.new }
+  before { allow(helper).to receive(:current_user).and_return user }
+
+  describe "#available_roles" do
+    let(:course) { double(:couse) }
+
+    context "when current user is an administrator" do
+      before do
+        allow(user).to receive(:is_professor?).with(course).and_return true
+        allow(user).to receive(:is_admin?).with(course).and_return true
+      end
+
+      it "includes the admin role" do
+        expect(helper.available_roles(course)).to eq [
+          ["Student", "student"],
+          ["GSI", "gsi"],
+          ["Professor", "professor"],
+          ["Admin", "admin"]
+        ]
+      end
+    end
+
+    context "when the current user is a professor" do
+      before do
+        allow(user).to receive(:is_professor?).with(course).and_return true
+        allow(user).to receive(:is_admin?).with(course).and_return false
+      end
+
+      it "does not include the admin role" do
+        expect(helper.available_roles(course)).to eq [
+          ["Student", "student"],
+          ["GSI", "gsi"],
+          ["Professor", "professor"]
+        ]
+      end
+    end
+  end
+end
