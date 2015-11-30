17159375c53d6767e75cdf822d95099980b80b12
Jamie Wright <jamie@brilliantfantastic.com>
2015-11-30 16:55:16 -0500

    CourseMembershipBuilder that builds out the memberships for a user

---
+++ spec/models/course_membership_builder_spec.rb
@@ -0,0 +1,28 @@
+require "active_record_spec_helper"
+
+describe CourseMembershipBuilder do
+  let(:builder) { create :user }
+  let(:user) { create :user }
+  let!(:membership) { create :professor_course_membership, user: builder }
+
+  subject { described_class.new(builder) }
+
+  describe "#build_for" do
+    it "builds memberships for courses the builder has access to" do
+      subject.build_for(user)
+      expect(user.course_memberships.map(&:course)).to eq [membership.course]
+      expect(user.course_memberships.map(&:role)).to eq ["student"]
+    end
+
+    it "optionally builds a membership with a specific role" do
+      subject.build_for(user, "professor")
+      expect(user.course_memberships.map(&:role)).to eq ["professor"]
+    end
+
+    it "does not build duplicated memberships" do
+      user.course_memberships.create course_id: membership.course_id
+      subject.build_for(user)
+      expect(user.course_memberships.map(&:course)).to eq [membership.course]
+    end
+  end
+end
