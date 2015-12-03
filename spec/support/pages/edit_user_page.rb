14537835bbf636df867f5a49cd42b493e4ebc176
Jamie Wright <jamie@brilliantfantastic.com>
2015-12-03 00:17:13 -0500

    Split out the user page to allow for creating and editing

--- spec/support/pages/edit_user_page.rb
+++ spec/support/pages/edit_user_page.rb
@@ -1,10 +1,10 @@
 require_relative "user_page"
 
-class NewUserPage < UserPage
+class EditUserPage < UserPage
   include Capybara::DSL
 
   def submit(fields={})
     fill_out_form fields
-    click_button "Create User"
+    click_button "Update User"
   end
 end
---
+++ spec/support/pages/user_page.rb
@@ -0,0 +1,28 @@
+class UserPage
+  include Capybara::DSL
+
+  attr_accessor :user
+
+  def initialize(user)
+    @user = user
+  end
+
+  protected
+
+  def fill_out_form(fields={})
+    fill_in "First name", with: fields[:first_name] || user.first_name
+    fill_in "Last name", with: fields[:last_name] || user.last_name
+    fill_in "Email", with: fields[:email] || user.email
+    fill_in "Display name", with: fields[:display_name] || user.display_name
+
+    courses = fields[:courses]
+    courses.each_with_index do |course, index|
+      destroyed = find(:xpath, ".//input[@id='user_course_memberships_attributes_#{index}__destroy']")
+      if destroyed.value == "true"
+        destroyed.set false
+      else
+        destroyed.set true
+      end
+    end if courses
+  end
+end
