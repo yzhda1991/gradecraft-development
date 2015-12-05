e5b875797354d9cc7eebd64ff453eaf5ac9c4951
chcholman <caitlin.holman@gmail.com>
2015-12-05 12:32:26 -0500

    Test coverage for tiers controller

--- spec/controllers/analytics_events_controller_spec.rb
+++ spec/controllers/analytics_events_controller_spec.rb
@@ -2,8 +2,10 @@ require 'rails_spec_helper'
 
 RSpec.describe AnalyticsEventsController, type: :controller do
   describe "POST predictor_event" do
+    skip "implement"
   end
 
   describe "POST tab_select_event" do
+    skip "implement"
   end
 end
--- spec/controllers/tiers_controller_spec.rb
+++ spec/controllers/tiers_controller_spec.rb
@@ -1,11 +1,46 @@
 require 'rails_spec_helper'
 
 describe TiersController do
-  before(:all) { @course = create(:course) }
+  before(:all) do 
+    @course = create(:course)
+  end
   before(:each) do
     session[:course_id] = @course.id
     allow(Resque).to receive(:enqueue).and_return(true)
   end
+
+  context "as a professor" do 
+    before(:all) do
+      @professor = create(:user)
+      CourseMembership.create user: @professor, course: @course, role: "professor"
+    end
+
+    before(:each) { login_user(@professor) }
+
+    describe "POST create" do 
+      it "creates a new tier" do 
+        params = attributes_for(:tier)
+        expect{ post :create, :tier => params }.to change(Tier,:count).by(1)  
+      end
+    end
+
+    describe "GET destroy" do 
+      it "destroys a tier" do 
+        @tier = create(:tier)
+        expect{ get :destroy, { :id => @tier } }.to change(Tier,:count).by(-1)
+      end
+    end
+
+    describe "POST update" do 
+      it "updates a tier" do 
+        @tier = create(:tier)
+        params = { name: "new name" }
+        post :update, id: @tier.id, :tier=> params
+        expect(@tier.reload.name).to eq("new name")
+      end
+    end
+  end
+
 
   context "as a student" do
     before(:all) do
@@ -16,7 +51,6 @@ describe TiersController do
 
     describe "protected routes" do
       [
-        :new,
         :create
       ].each do |route|
           it "#{route} redirects to root" do
