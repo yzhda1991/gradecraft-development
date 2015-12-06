4e036e79fd014d33d4b93ba655785e17fd7b673a
chcholman <caitlin.holman@gmail.com>
2015-12-05 19:40:43 -0500

    coverage for assignment type export with no assignment types present

--- spec/controllers/assignment_types_controller_spec.rb
+++ spec/controllers/assignment_types_controller_spec.rb
@@ -113,8 +113,14 @@ describe AssignmentTypesController do
       context "with CSV format" do
         it "returns all scores in csv form" do
           grade = create(:grade, assignment: @assignment, student: @student, feedback: "good jorb!")
-          get :export_all_scores, :id => @assignment_type, :format => :csv
+          get :export_all_scores, :format => :csv
           expect(response.body).to include("First Name,Last Name,Email,Username,Team")
+        end
+
+        it "redirects to the dashboard if no assignment types exist" do 
+          @assignment_type.destroy
+          get :export_all_scores, :format => :csv
+          expect(response).to redirect_to dashboard_path
         end
       end
     end
