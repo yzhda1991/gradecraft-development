6c465ffa535da0de3a949f809cad13fdfbf987f0
Jamie Wright <jamie@brilliantfantastic.com>
2015-12-10 11:55:09 -0500

    Store the course id on the user

--- app/controllers/current_courses_controller.rb
+++ app/controllers/current_courses_controller.rb
@@ -1,12 +1,11 @@
 class CurrentCoursesController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  respond_to :json
 
   # Switch between enrolled courses
   def change
     if course = current_user.courses.where(:id => params[:course_id]).first
       unless session[:course_id] == course.id
-        session[:course_id] = course.id
+        session[:course_id] = CourseRouter.change!(current_user, course).id
         log_course_login_event
       end
     end
