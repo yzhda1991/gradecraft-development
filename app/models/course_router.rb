bb8b24fc242a71de4a7d563179ded14afa81103f
Jamie Wright <jamie@brilliantfantastic.com>
2015-12-10 12:35:51 -0500

    Allow the current course to be retrieved for a user

--- app/models/course_router.rb
+++ app/models/course_router.rb
@@ -1,7 +1,17 @@
 class CourseRouter
-  def self.change!(user, course)
-    user.default_course_id = course.id
-    user.save
-    course
+  class << self
+    def change!(user, course)
+      user.default_course_id = course.id
+      user.save
+      course
+    end
+
+    def current_course_for(user, current_course_id=nil)
+      return nil if user.nil?
+      course = user.courses.find_by(id: current_course_id) unless current_course_id.nil?
+      course ||= user.default_course
+      course ||= user.courses.first
+      course
+    end
   end
 end
--- spec/models/course_router_spec.rb
+++ spec/models/course_router_spec.rb
@@ -2,8 +2,9 @@ require "spec_helper"
 require "./app/models/course_router"
 
 describe CourseRouter do
+  let(:course) { double(:course, id: 456) }
+
   describe ".change!" do
-    let(:course) { double(:course, id: 456) }
     let(:user) { double(:user, :default_course_id= => nil, save: true) }
 
     it "updates the default course id for the user" do
@@ -15,6 +16,35 @@ describe CourseRouter do
     it "returns the new course" do
       result = described_class.change! user, course
       expect(result).to eq course
+    end
+  end
+
+  describe ".current_course_for" do
+    let(:courses) { double(:active_record_relation) }
+    let(:user) { double(:user, courses: courses) }
+
+    it "returns nil if the user is nil" do
+      expect(described_class.current_course_for(nil)).to be_nil
+    end
+
+    context "with a current course id" do
+      it "returns the course that the user has access to" do
+        allow(courses).to receive(:find_by).and_return course
+        expect(described_class.current_course_for(user, course.id)).to eq course
+      end
+
+      it "returns the default course if one is not specified" do
+        allow(courses).to receive(:find_by).and_return nil
+        allow(user).to receive(:default_course).and_return course
+        expect(described_class.current_course_for(user, course.id)).to eq course
+      end
+
+      it "returns the first course if there is not one set as the default" do
+        allow(courses).to receive(:find_by).and_return nil
+        allow(user).to receive(:default_course).and_return nil
+        allow(courses).to receive(:first).and_return course
+        expect(described_class.current_course_for(user, course.id)).to eq course
+      end
     end
   end
 end
