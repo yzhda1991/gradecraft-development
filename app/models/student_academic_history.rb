67928d7ed5bcd2840374dbe6e53021330f026a56
chcholman <caitlin.holman@gmail.com>
2015-12-12 15:22:54 -0500

    Improving student academic history controller

--- app/models/student_academic_history.rb
+++ app/models/student_academic_history.rb
@@ -6,5 +6,7 @@ class StudentAcademicHistory < ActiveRecord::Base
   attr_accessible :student_id, :major, :gpa, :current_term_credits, :accumulated_credits, :year_in_school,
   :state_of_residence, :high_school, :athlete, :act_score, :sat_score, :course_id
 
+  validates :gpa, :numericality => { :greater_than_or_equal_to => 0 }, :allow_blank => true
+  validates_presence_of :course_id, :student_id
 
 end
