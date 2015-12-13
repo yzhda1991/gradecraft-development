faf4926a88377a3f769f45874a4e9ce89786410c
chcholman <caitlin.holman@gmail.com>
2015-12-13 15:38:48 -0500

    Breaking gradebooks out into separate exporters

--- app/exporters/multiplied_gradebook_exporter.rb
+++ app/exporters/multiplied_gradebook_exporter.rb
@@ -1,12 +1,12 @@
-class GradebookExporter
+class MultipliedGradebookExporter
 
-  #gradebook spreadsheet export for course
-  def gradebook(course_id)
+  #gradebook spreadsheet export for course with multipliers
+  def multiplied_gradebook(course_id)
     course = fetch_course(course_id)
     CSV.generate do |csv|
       csv << gradebook_columns(course)
       course.students.each do |student|
-        csv << base_column_data_for(student, course) + standard_grade_data_for(student, course)
+        csv << base_column_data_for(student, course) + multiplied_grade_data_for(student, course)
       end
     end
   end
@@ -30,20 +30,17 @@ class GradebookExporter
   end
 
   def assignment_name_columns(course)
-    course.assignments.collect(&:name)
+    course.assignments.collect do |assignment|
+      [ assignment.name, assignment.name ]
+    end.flatten
   end
 
-  def base_column_data_for(student, course)
-    student_data << [student.first_name, student.last_name, student.email, student.username, student.team_for_course()]
-  end
-
-  def standard_grade_data_for(student, course)
+  def multiplied_grade_data_for(student, course)
+    # add the grades for the necessary assignments, todo: improve the performance here
     course.assignments.inject(student_data) do |memo, assignment|
       grade = assignment.grade_for_student(student)
       if grade and grade.is_student_visible?
-        memo << grade.try(:raw_score)
-      else
-        memo << ''
+        memo << grade.try(:score)
       end
       memo
     end
