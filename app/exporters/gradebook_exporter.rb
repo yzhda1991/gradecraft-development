eaef738dfc0c3b03d01c643f57c67183a2add8d8
chcholman <caitlin.holman@gmail.com>
2015-12-13 19:04:33 -0500

    passing specs for course gradebook exporter

--- app/exporters/gradebook_exporter.rb
+++ app/exporters/gradebook_exporter.rb
@@ -6,7 +6,7 @@ class GradebookExporter
     CSV.generate do |csv|
       csv << gradebook_columns(course)
       course.students.each do |student|
-        csv << base_column_data_for(student, course) + standard_grade_data_for(student, course)
+        csv << student_data_for(student, course)
       end
     end
   end
@@ -33,19 +33,27 @@ class GradebookExporter
     course.assignments.collect(&:name)
   end
 
-  def base_column_data_for(student, course)
-    student_data << [student.first_name, student.last_name, student.email, student.username, student.team_for_course()]
+  def base_column_methods
+     [:first_name, :last_name, :email, :username]
   end
 
-  def standard_grade_data_for(student, course)
-    course.assignments.inject(student_data) do |memo, assignment|
-      grade = assignment.grade_for_student(student)
-      if grade and grade.is_student_visible?
-        memo << grade.try(:raw_score)
-      else
-        memo << ''
+  def student_data_for(student, course)
+      # add the base column names
+      student_data = base_column_methods.inject([]) do |memo, method|
+        memo << student.send(method)
       end
-      memo
+      # todo: we need to pre-fetch the course teams for this
+      student_data << student.team_for_course(course).try(:name)
+
+      # add the grades for the necessary assignments, todo: improve the performance here
+      course.assignments.inject(student_data) do |memo, assignment|
+        grade = assignment.grade_for_student(student)
+        if grade and grade.is_student_visible?
+          memo << grade.try(:raw_score)
+        else
+          memo << ''
+        end
+        memo
+      end
     end
-  end
 end
--- spec/exporters/gradebook_exporter_spec.rb
+++ spec/exporters/gradebook_exporter_spec.rb
@@ -1,7 +1,7 @@
 require "active_record_spec_helper"
 require "./app/exporters/gradebook_exporter"
 
-describe GradebookExporter, focus: true do
+describe GradebookExporter do
   let(:course) { create :course }
   subject { GradebookExporter.new }
 
@@ -9,6 +9,33 @@ describe GradebookExporter, focus: true do
     it "generates an empty CSV if there are no students or assignments" do
       csv = subject.gradebook(course.id)
       expect(csv).to eq "First Name,Last Name,Email,Username,Team\n"
+    end
+
+    it "generates an empty CSV if there are no students" do
+      @assignment = create(:assignment, course: course, name: "The Flash!")
+      csv = subject.gradebook(course.id)
+      expect(csv).to eq "First Name,Last Name,Email,Username,Team,The Flash!\n"
+    end
+
+    it "generates an gradebook CSV if there are students and assignments present" do
+      @assignment_type_1 = create(:assignment_type, course: course, name: "Charms")
+      @assignment = create(:assignment, course: course, assignment_type: @assignment_type_1)
+      @assignment_2 = create(:assignment, course: course, assignment_type: @assignment_type_1)
+      @student = create(:user)
+      @student.courses << course
+      create(:grade, assignment: @assignment, student: @student, raw_score: 100, status: "Released" )
+      create(:grade, assignment: @assignment_2, student: @student, raw_score: 200, status: "Released")
+
+      csv = CSV.new(subject.gradebook(course.id)).read
+      expect(csv.length).to eq 2
+      expect(csv[1][0]).to eq @student.first_name
+      expect(csv[1][1]).to eq @student.last_name
+      expect(csv[1][2]).to eq @student.email
+      expect(csv[1][3]).to eq @student.username
+      expect(csv[1][4]).to eq @student.team_for_course(course)
+      expect(csv[1][5]).to eq "100"
+      expect(csv[1][6]).to eq "200"
+
     end
   end
 end
