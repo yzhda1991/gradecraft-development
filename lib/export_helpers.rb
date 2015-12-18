0e745620a11d0f4b51cc31a9235ddbaa4d744aca
Mike Zazaian <mike@zop.io>
2015-12-18 11:25:40 -0500

    refactor S3 export helpers module for KMS client management and better encryption, add some test methods for testing S3 interactions

--- lib/export_helpers.rb
+++ lib/export_helpers.rb
@@ -3,12 +3,12 @@ module ExportHelpers
   module S3
     extend ActiveSupport::Concern
 
-    self.included do
-      before_create :set_s3_symmetric_key
-    end
-
-    def build_s3_client
-      @s3_client ||= Aws::S3::Client.new
+    def s3_client
+      @s3_client ||= Aws::S3::Client.new({
+        region: ENV['AWS_S3_REGION'],
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      })
     end
 
     def s3_resource
@@ -16,32 +16,54 @@ module ExportHelpers
     end
 
     def s3_bucket
-      build_s3_client
+      s3_client
       @s3_bucket ||= s3_resource.bucket(s3_bucket_name)
     end
 
     def s3_bucket_name
-      "gradecraft-#{Rails.env}"
+      @s3_bucket_name ||= "gradecraft-#{Rails.env}"
+    end
+
+    def kms_client
+      @kms_client ||= Aws::KMS::Client.new
+    end
+
+    def kms_key_id
+      @kms_key_id ||= kms_client.create_key.key_metadata.key_id
+    end
+
+    def encrypted_s3_client
+      encrypted_s3_client ||= Aws::S3::Encryption::Client.new(
+        kms_key_id: kms_key_id,
+        kms_client: kms_client,
+        client: s3_client
+      )
+    end
+
+    def push_test_object
+      encrypted_s3_client.put_object s3_object_attrs.merge(server_side_encryption: "AES256", key: "some-rando-key", body: "some rando content.")
+    end
+
+    def push_test_object_pair(key, body)
+      encrypted_s3_client.put_object s3_object_attrs.merge(server_side_encryption: "AES256", key: key, body: body)
+    end
+
+    def get_test_object(object_key)
+      encrypted_s3_client.get_object s3_object_attrs.merge(key: object_key)
+    end
+
+    def s3_object_attrs
+      {
+        bucket: s3_bucket_name
+      }
     end
 
     def s3_object_key
       "#{s3_object_key_path}/#{export_filename}"
     end
 
-    def export_filename
-      @export_filename ||= random_cipher_key
-    end
-
-    def random_cipher_key
-      OpenSSL::Cipher.new("AES-256-ECB").random_key
-    end
-
     def s3_object_key_path
       "/exports"
-    end
-
-    def set_s3_symmetric_key
-      # self[:s3_symmetric_key] = random_cipher_key
     end
 
     def s3_object
