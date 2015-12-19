4efd3a477c39564b84dc0d9d2d32a811e352de05
Mike Zazaian <mike@zop.io>
2015-12-19 09:58:16 -0500

    add push/get helper methods for client-encrypted objects, building out additional spec examples for client-encrypted object handling

--- lib/s3_manager/encryption.rb
+++ lib/s3_manager/encryption.rb
@@ -2,18 +2,20 @@ module S3Manager
   module Encryption
     # requires S3Manager::Kms
     # requires S3Manager::Basic
-    def encrypted_s3_client
-      @encrypted_s3_client ||= Aws::S3::Encryption::Client.new(
+    def encrypted_client
+      @encrypted_client ||= Aws::S3::Encryption::Client.new(
         kms_key_id: kms_key_id,
         kms_client: kms_client,
-        client: s3_client
+        client: client
       )
     end
 
-    def get_client_encrypted_object
-    end
+    def get_client_encrypted_object(object_key)
+      encrypted_client
+        .get_object object_attrs.merge(key: object_key)
 
-    def put_client_encrypted_object
+    def put_client_encrypted_object(object_key, body)
+      encrypted_client.put_object object_attrs.merge(server_side_encryption: "AES256", key: object_key, body: body)
     end
   end
 end
---
+++ spec/lib/s3_manager/encryption_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_spec_helper'
+
+include Toolkits::S3Manager::EncryptionToolkit
+
+RSpec.describe S3Manager::Encryption do
+  let(:s3_manager) { S3Manager::Manager.new }
+
+  describe "#encrypted_client" do
+    subject { s3_manager.encrypted_client }
+
+    it "builds a new encrypted client" do
+      expect(subject.class).to eq(Aws::S3::Encryption::Client)
+    end
+
+    it "uses a standard s3 client" do
+      expect(subject.instance_variable_get(:@client)).to eq(s3_manager.client)
+    end
+
+    describe "cipher provider" do
+      subject { s3_manager.encrypted_client.instance_variable_get(:@cipher_provider) }
+
+      it "sets the kms_key_id" do
+        expect(subject.instance_variable_get(:@kms_key_id)).to eq(s3_manager.kms_key_id)
+      end
+
+      it "sets the kms_client" do
+        expect(subject.instance_variable_get(:@kms_client)).to eq(s3_manager.kms_client)
+      end
+    end
+
+    it "caches the encrypted client" do
+      subject
+      expect(Aws::S3::Encryption::Client).not_to receive(:new)
+      subject
+    end
+  end
+
+  describe "managing client-encrypted objects" do
+    describe "#get_client_encrypted_object" do
+    end
+
+    describe "#put_client_encrypted_object" do
+    end
+  end
+end
