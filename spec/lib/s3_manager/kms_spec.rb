70f6165bbc0f3305954fe0f5f7c575ae9a9d71a0
Mike Zazaian <mike@zop.io>
2015-12-19 10:57:41 -0500

    write/pass spec examples for the S3Manager's AWS Key Management Service (KMS) module

---
+++ spec/lib/s3_manager/kms_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_spec_helper'
+
+RSpec.describe S3Manager::Kms do
+  let(:s3_manager) { S3Manager::Manager.new }
+
+  describe "#kms_client" do
+    subject { s3_manager.kms_client }
+
+    it "builds a new kms client" do
+      expect(subject.class).to eq(Aws::KMS::Client)
+    end
+
+    it "uses the Seahorse::Model API" do
+      expect(subject.config.api.class).to eq(Seahorse::Model::Api)
+    end
+
+    it "caches the kms client" do
+      subject
+      expect(Aws::KMS::Client).not_to receive(:new)
+      subject
+    end
+  end
+
+  describe "#kms_key_id" do
+    let(:kms_client) { s3_manager.kms_client }
+    subject { s3_manager.kms_key_id }
+
+    it "creates a new kms key and gets the key id" do
+      expect(kms_client).to receive_message_chain(:create_key, :key_metadata, :key_id)
+      subject
+    end
+
+    it "should have 36 characters" do
+      expect(subject.size).to eq(36)
+    end
+    
+    it "should be a string of alphanum characters and hyphens" do
+      expect(subject).to match(/[a-z0-9\-]{36}/)
+    end
+
+    it "caches the kms_key_id" do
+      subject
+      expect(kms_client).not_to receive(:create_key)
+      subject
+    end
+  end
+end
