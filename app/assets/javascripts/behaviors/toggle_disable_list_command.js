69e7c35ed30ebeda6a5d701dad3abc10542d4088
Jamie Wright <jamie@brilliantfantastic.com>
2015-12-21 15:02:25 -0500

    Implement toggle behavior from checkbox change

--- app/assets/javascripts/behaviors/toggle_disable_list_command.js
+++ app/assets/javascripts/behaviors/toggle_disable_list_command.js
@@ -1,7 +1,12 @@
 $(function() {
   $(document).on("change", "[data-behavior~=toggle-disable-list-command]", function(e) {
     var commandButtonsSelector = $(e.target).data("commands");
-    var commandButtons = $(e.target).closest(commandButtonsSelector);
-    console.log(commandButtons.length);
+    var commandButtons = $(document).find(commandButtonsSelector);
+    var list = $(document).find("[name='" + $(e.target).attr("name") + "']:checked");
+    if (list.length > 0) {
+      commandButtons.removeClass("disabled").attr("disabled", false);
+    } else {
+      commandButtons.addClass("disabled").attr("disabled", true);
+    }
   });
 });
