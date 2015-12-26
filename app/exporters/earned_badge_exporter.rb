2c6fbbf171b87972e69f5131bb8ed56a6b489a1c
chcholman <caitlin.holman@gmail.com>
2015-12-26 00:01:50 -0500

    Refactored earned badge exporter slightly

--- app/exporters/earned_badge_exporter.rb
+++ app/exporters/earned_badge_exporter.rb
@@ -4,16 +4,7 @@ class EarnedBadgeExporter
     CSV.generate do |csv|
       csv.add_row baseline_headers
       earned_badges.each do |earned_badge|
-        csv << [
-          earned_badge.student.first_name,
-          earned_badge.student.last_name,
-          earned_badge.student.username,
-          earned_badge.student.email,
-          earned_badge.badge.id,
-          earned_badge.badge.name,
-          earned_badge.feedback,
-          earned_badge.created_at.to_formatted_s(:db)
-        ]
+        csv.add_row earned_badge_data(earned_badge)
       end
     end
   end
@@ -22,5 +13,18 @@ class EarnedBadgeExporter
 
   def baseline_headers
     ["First Name", "Last Name", "Uniqname", "Email", "Badge ID", "Badge Name", "Feedback", "Awarded Date"].freeze
+  end
+
+  def earned_badge_data(earned_badge)
+    [
+      earned_badge.student.first_name,
+      earned_badge.student.last_name,
+      earned_badge.student.username,
+      earned_badge.student.email,
+      earned_badge.badge.id,
+      earned_badge.badge.name,
+      earned_badge.feedback,
+      earned_badge.created_at.to_formatted_s(:db)
+    ]
   end
 end
