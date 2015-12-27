096c079907d3b63c5324d6bcd6615ff2e28ac5f7
chcholman <caitlin.holman@gmail.com>
2015-12-27 18:47:54 -0500

    Passing feature spec for editing a badge

---
+++ spec/features/editing_a_badge_spec.rb
@@ -0,0 +1,39 @@
+require "rails_spec_helper"
+
+feature "editing a badge" do
+  context "as a professor" do
+    let(:course) { create :course, badge_setting: true}
+    let!(:course_membership) { create :professor_course_membership, user: professor, course: course }
+    let(:professor) { create :user }
+    let!(:badge) { create :badge, name: "Fancy Badge", course: course}
+
+    before(:each) do
+      login_as professor
+      visit dashboard_path
+    end
+
+    scenario "successfully" do
+      within(".sidebar-container") do
+        click_link "badges"
+      end
+
+      expect(current_path).to eq badges_path
+
+      within(".pageContent") do
+        click_link "Fancy Badge"
+      end
+
+      expect(current_path).to eq badge_path(badge.id)
+
+      within(".context_menu") do
+        click_link "Edit"
+      end
+
+      within(".pageContent") do
+        fill_in "Name", with: "Edited Badge Name"
+        click_button "Update badge"
+      end
+      expect(page).to have_notification_message('notice', 'Edited Badge Name badge successfully updated')
+    end
+  end
+end
---
+++ spec/features/editing_a_team_spec.rb
@@ -0,0 +1,40 @@
+require "rails_spec_helper"
+
+feature "editing a team" do
+  context "as a professor" do
+    let(:course) { create :course, team_setting: true}
+    let!(:course_membership) { create :professor_course_membership, user: professor, course: course }
+    let(:professor) { create :user }
+    let!(:team) { create :team, name: "Team Name", course: course }
+
+    before(:each) do
+      login_as professor
+      visit dashboard_path
+    end
+
+    scenario "successfully" do
+      within(".sidebar-container") do
+        click_link "teams"
+      end
+
+      expect(current_path).to eq teams_path
+
+      within(".pageContent") do
+        click_link "Team Name"
+      end
+
+      expect(current_path).to eq team_path(team.id)
+
+      within(".context_menu") do
+        click_link "Edit"
+      end
+
+      within(".pageContent") do
+        fill_in "Name", with: "Edited Team Name"
+        click_button "Update team"
+      end
+
+      expect(page).to have_notification_message('notice', 'Team Edited Team Name successfully updated')
+    end
+  end
+end
