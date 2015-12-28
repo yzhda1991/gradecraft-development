16a062c219c71bfd81dba773784a07d038910001
chcholman <caitlin.holman@gmail.com>
2015-12-27 22:04:39 -0500

    Passing feature spec for grading a team challenge

---
+++ spec/features/grading_a_team_challenge_spec.rb
@@ -0,0 +1,42 @@
+require "rails_spec_helper"
+
+feature "grading a team challenge" do
+  context "as a professor" do
+    let(:course) { create :course, team_challenges: true }
+    let!(:course_membership) { create :professor_course_membership, user: professor, course: course }
+    let(:professor) { create :user }
+    let!(:challenge) { create :challenge, name: "Team Challenge Name", course: course }
+    let!(:team) { create :team, name: "Team Name", course: course }
+
+    before(:each) do
+      login_as professor
+      visit dashboard_path
+    end
+
+    scenario "successfully" do
+      within(".sidebar-container") do
+        click_link "team Challenges"
+      end
+
+      expect(current_path).to eq challenges_path
+
+      within(".pageContent") do
+        click_link "Team Challenge Name"
+      end
+
+      expect(current_path).to eq challenge_path(challenge.id)
+
+      within(".pageContent") do
+        click_link "Grade"
+      end
+
+      expect(current_path).to eq new_challenge_challenge_grade_path(:challenge_id => challenge.id)
+
+      within(".pageContent") do
+        fill_in("challenge_grade_score", with: 100)
+        click_button "Submit Grade"
+      end
+      expect(page).to have_notification_message('notice', "Team Name's Grade for Team Challenge Name Team Challenge successfully graded")
+    end
+  end
+end
