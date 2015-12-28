8f139fec618f589dd43348a4051bbe4e56f06a31
chcholman <caitlin.holman@gmail.com>
2015-12-27 19:19:18 -0500

    passing feature specs for editing an assignment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -62,7 +62,7 @@ class AssignmentsController < ApplicationController
     assignment = current_course.assignments.find(params[:id])
     if assignment.update_attributes(params[:assignment])
       set_assignment_weights(assignment)
-      redirect_to assignments_path, notice: "#{(term_for :assignment).titleize}  <strong>#{assignment.name }</strong> successfully updated" and return
+      redirect_to assignments_path, notice: "#{(term_for :assignment).titleize} #{assignment.name } successfully updated" and return
     end
 
     @title = "Edit #{term_for :assignment}"
