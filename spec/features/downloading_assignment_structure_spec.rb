9d11acf8649cf47d2df9c662d3c7f47338733bc4
chcholman <caitlin.holman@gmail.com>
2015-12-29 13:43:42 -0500

    Feature spec for downloading assignment structure

---
+++ spec/features/downloading_assignment_structure_spec.rb
@@ -0,0 +1,25 @@
+require "rails_spec_helper"
+
+feature "downloading assignment structure file" do
+  context "as a professor" do
+    let(:course) { create :course }
+    let!(:course_membership) { create :professor_course_membership, user: professor, course: course }
+    let(:professor) { create :user }
+
+    before(:each) do
+      login_as professor
+      visit dashboard_path
+    end
+
+    scenario "successfully" do
+
+      within(".sidebar-container") do
+        click_link "assignment Structure"
+      end
+
+      expect(page.response_headers['Content-Type']).to eq('application/octet-stream')
+
+      expect(page).to have_content "Assignment ID,Name,Point Total,Description,Open At,Due At,Accept Until"
+    end
+  end
+end
