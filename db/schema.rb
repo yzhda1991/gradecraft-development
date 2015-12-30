74abfaff5f93e484d71c778df194a39ea20a6946
Jamie Wright <jamie@brilliantfantastic.com>
2015-12-30 12:59:06 -0500

    Add object changes to the versions so the changesets can be stored

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227015352) do
+ActiveRecord::Schema.define(version: 20151230155933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -900,6 +900,7 @@ ActiveRecord::Schema.define(version: 20151227015352) do
     t.text     "object"
     t.datetime "created_at"
     t.integer  "transaction_id"
+    t.text     "object_changes"
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
