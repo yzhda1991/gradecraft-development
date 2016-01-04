cbed488d3caa04a66885ef5909f29f7edf1a5d0d
chcholman <caitlin.holman@gmail.com>
2016-01-04 16:17:32 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230155933) do
-  
+ActiveRecord::Schema.define(version: 20160104191002) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
