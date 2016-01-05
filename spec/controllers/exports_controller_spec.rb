277a6fe9da81480fd581c520bcecc3330f9cf156
Mike Zazaian <mike@zop.io>
2016-01-05 13:15:12 -0500

    add simple examples for testing exports controller index

--- spec/controllers/exports_controller_spec.rb
+++ spec/controllers/exports_controller_spec.rb
@@ -2,9 +2,26 @@ require 'rails_spec_helper'
 
 RSpec.describe ExportsController, type: :controller do
 
+  let(:teams) { create_list(:team, 2) }
+  let(:course) { create(:course, teams: teams) }
+  let(:assignment_exports) { create_list(:assignment_export, 2, course: course) }
+  let(:professor) { create(:professor_course_membership, course: course).user }
+
+  before do
+    login_user(professor)
+    allow(controller).to receive(:current_course) { course }
+  end
+
   describe "GET #index" do
-    # it "returns http success" do
-    # end
+    it "should get the relevant assignment exports" do
+      get :index
+      expect(assigns(:assignment_exports)).to eq(course.assignment_exports.order("updated_at DESC"))
+    end
+
+    it "should get the teams" do
+      get :index
+      expect(assigns(:teams)).to eq(course.teams)
+    end
   end
 
 end
--- spec/factories/assignment_export_factory.rb
+++ spec/factories/assignment_export_factory.rb
@@ -1,10 +1,8 @@
 FactoryGirl.define do
   factory :assignment_export do
-    factory :assignment_export_with_associations do
-      association :course
-      association :professor, factory: :user
-      association :team
-      association :assignment
-    end
+    association :course
+    association :professor, factory: :user
+    association :team
+    association :assignment
   end
 end
