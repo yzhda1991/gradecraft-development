8b6db56b46e1a719b32121bb795ed0978ff76a7f
Mike Zazaian <mike@zop.io>
2016-01-06 13:26:09 -0500

    fix bugs in exports controller spec

--- spec/controllers/exports_controller_spec.rb
+++ spec/controllers/exports_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ExportsController, type: :controller do
 
   let(:teams) { create_list(:team, 2) }
   let(:course) { create(:course, teams: teams) }
-  let(:assignment_exports) { create_list(:assignment_export, 2, course: course) }
+  let(:submissions_exports) { create_list(:submissions_export, 2, course: course) }
   let(:professor) { create(:professor_course_membership, course: course).user }
 
   before do
@@ -15,7 +15,7 @@ RSpec.describe ExportsController, type: :controller do
   describe "GET #index" do
     it "should get the relevant submissions exports" do
       get :index
-      expect(assigns(:assignment_exports)).to eq(course.assignment_exports.order("updated_at DESC"))
+      expect(assigns(:submissions_exports)).to eq(course.submissions_exports.order("updated_at DESC"))
     end
 
     it "should get the teams" do
