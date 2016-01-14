658c2b3dbac232daa72ed8a261b9242d87ac7c9d
Mike Zazaian <mike@zop.io>
2016-01-14 12:28:31 -0500

    add spec examples for export progress reset attributes

--- spec/performers/submissions_export_performer/attributes_spec.rb
+++ spec/performers/submissions_export_performer/attributes_spec.rb
@@ -10,6 +10,28 @@ RSpec.describe "SubmissionsExport attributes", type: :background_job do
 
   subject { performer }
 
+  describe "#submissions_export_attributes" do
+    subject { performer.submissions_export_attributes }
+    before(:each) do
+      performer.instance_variable_set(:@submissions_export, submissions_export)
+      allow(Time).to receive(:now) { Date.parse("Oct 20 1984").to_time }
+    end
+
+    context "the submissions export has been performed at least once" do
+      let(:submissions_export) { create(:submissions_export, last_export_started_at: Time.now) }
+      it "returns the base export attributes combined with the progress reset attributes" do
+        expect(subject).to eq performer.base_export_attributes.merge(performer.clear_progress_attributes)
+      end
+    end
+
+    context "the submissions export has not been performed yet" do
+      let(:submissions_export) { create(:submissions_export, last_export_started_at: nil) }
+      it "just returns the base export attributes" do
+        expect(subject).to eq performer.base_export_attributes
+      end
+    end
+  end
+
   describe "#base_export_attributes" do
     before do
       allow(performer).to receive_messages({
@@ -36,6 +58,25 @@ RSpec.describe "SubmissionsExport attributes", type: :background_job do
 
     it "should include the export filename" do
       expect(subject[:export_filename]).to eq("really_bad_file.zip")
+    end
+  end
+
+  describe "#clear_progress_attributes" do
+    subject { performer.clear_progress_attributes }
+
+    it "returns a list of all progress attributes as nils" do
+      expect(subject).to eq({
+        generate_export_csv: nil,
+        export_csv_successful: nil,
+        create_student_directories: nil,
+        student_directories_created_successfully: nil,
+        create_submission_text_files: nil,
+        create_submission_binary_files: nil,
+        generate_error_log: nil,
+        archive_exported_files: nil,
+        upload_archive_to_s3: nil,
+        check_s3_upload_success: nil
+      })
     end
   end
 end
