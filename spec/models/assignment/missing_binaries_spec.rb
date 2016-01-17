feba86bf08930f86329adc08e0a76fb356aed1ce
Mike Zazaian <mike@zop.io>
2016-01-16 19:48:45 -0500

    adding team-export parameters for submissions exports

--- spec/models/assignment/missing_binaries_spec.rb
+++ spec/models/assignment/missing_binaries_spec.rb
@@ -18,79 +18,129 @@ RSpec.describe "Assignment #missing_binaries methods" do
 
   before(:each) { cache_submission_files }
 
-  describe "#submission_files_with_missing_binaries" do
-    subject { assignment.submission_files_with_missing_binaries }
+  context "no team is given" do
+    describe "#submission_files_with_missing_binaries" do
+      subject { assignment.submission_files_with_missing_binaries }
 
-    context "submission file is marked 'file_missing'" do
-      it "returns missing submission_files for the the assignment" do
-        expect(subject).to include(missing_submission_file)
-      end
-    end
-
-    context "submission file is not marked 'file_missing'" do
-      it "doesn't return non-missing submission files" do
-        expect(subject).not_to include(present_submission_file)
-      end
-    end
-
-    describe "ordering" do
-      let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
-
-      it "orders the students by create_at ASC" do
-        expect(subject.index(missing_submission_file)).to be < subject.index(another_missing_submission_file)
-      end
-    end
-
-    describe "assignment association" do
-      let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
-      let(:another_submission_with_missing_file) { create(:submission, student: student3) } # some other assignment
-
-      context "submission file is associated with submission for that assignment" do
-        it "returns the submission file" do
+      context "submission file is marked 'file_missing'" do
+        it "returns missing submission_files for the the assignment" do
           expect(subject).to include(missing_submission_file)
         end
       end
 
-      context "submission_file is associated with another assignment" do
-        it "doesn't return the submission file" do
-          expect(subject).not_to include(another_missing_submission_file)
+      context "submission file is not marked 'file_missing'" do
+        it "doesn't return non-missing submission files" do
+          expect(subject).not_to include(present_submission_file)
+        end
+      end
+
+      describe "ordering" do
+        let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
+
+        it "orders the students by create_at ASC" do
+          expect(subject.index(missing_submission_file)).to be < subject.index(another_missing_submission_file)
+        end
+      end
+
+      describe "assignment association" do
+        let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
+        let(:another_submission_with_missing_file) { create(:submission, student: student3) } # some other assignment
+
+        context "submission file is associated with submission for that assignment" do
+          it "returns the submission file" do
+            expect(subject).to include(missing_submission_file)
+          end
+        end
+
+        context "submission_file is associated with another assignment" do
+          it "doesn't return the submission file" do
+            expect(subject).not_to include(another_missing_submission_file)
+          end
+        end
+      end
+    end
+
+    describe "#students_with_missing_binaries" do
+      subject { assignment.students_with_missing_binaries }
+
+      it "returns students that have submissions for the assignment" do
+        expect(subject).to include(student1)
+      end
+
+      it "doesn't return students that don't have a submission for the assignment" do
+        expect(subject).not_to include(student2)
+      end
+
+      describe "ordering" do
+        let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
+
+        it "orders the students by name" do
+          expect(subject.first.alphabetical_name_key < subject.last.alphabetical_name_key).to be_truthy
+        end
+      end
+
+      describe "assignment association" do
+        let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
+        let(:another_submission_with_missing_file) { create(:submission, student: student3) } # some other assignment
+
+        context "user has submissions with missing files for the assignment" do
+          it "returns the user with those files" do
+            expect(subject).to include(student1)
+          end
+        end
+
+        context "user has submissions with missing files that don't belong to the assignment" do
+          it "doesn't return the user whose files belong to the third-party assignment" do
+            expect(subject).not_to include(student3)
+          end
         end
       end
     end
   end
 
-  describe "#students_with_missing_binaries" do
-    subject { assignment.students_with_missing_binaries }
+  context "a team is given" do
+    let(:team)( { create(:team) } )
+    let(:team_membership) { create(:team_membership, team: team, student: student1) }
 
-    it "returns students that have submissions for the assignment" do
-      expect(subject).to include(student1)
-    end
+    let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
 
-    it "doesn't return students that don't have a submission for the assignment" do
-      expect(subject).not_to include(student2)
-    end
+    describe "#submission_files_with_missing_binaries_for_team" do
+      subject { assignment.submission_files_with_missing_binaries_for_team(team) }
 
-    describe "ordering" do
-      let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
-
-      it "orders the students by name" do
-        expect(subject.first.alphabetical_name_key < subject.last.alphabetical_name_key).to be_truthy
-      end
-    end
-
-    describe "assignment association" do
-      let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
-      let(:another_submission_with_missing_file) { create(:submission, student: student3) } # some other assignment
-
-      context "user has submissions with missing files for the assignment" do
-        it "returns the user with those files" do
-          expect(subject).to include(student1)
+      context "submission file is marked 'file_missing'" do
+        it "returns missing submission_files for the the assignment" do
+          expect(subject).to include(missing_submission_file)
         end
       end
 
-      context "user has submissions with missing files that don't belong to the assignment" do
-        it "doesn't return the user whose files belong to the third-party assignment" do
-          expect(subject).not_to include(student3)
+      context "submission file is not marked 'file_missing'" do
+        it "doesn't return non-missing submission files" do
+          expect(subject).not_to include(present_submission_file)
+        end
+      end
+
+      describe "ordering" do
+        let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
+
+        it "orders the students by create_at ASC" do
+          expect(subject.index(missing_submission_file)).to be < subject.index(another_missing_submission_file)
+        end
+      end
+
+      describe "assignment association" do
+        let(:cache_submission_files) { missing_submission_file; present_submission_file; another_missing_submission_file }
+        let(:another_submission_with_missing_file) { create(:submission, student: student3) } # some other assignment
+
+        context "submission file is associated with submission for that assignment" do
+          it "returns the submission file" do
+            expect(subject).to include(missing_submission_file)
+          end
+        end
+
+        context "submission_file is associated with another assignment" do
+          it "doesn't return the submission file" do
+            expect(subject).not_to include(another_missing_submission_file)
+          end
         end
       end
     end
