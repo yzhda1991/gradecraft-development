f24dfb93e9294d595ae5e6125df332ed8b587de1
Mike Zazaian <mike@zop.io>
2016-01-23 06:30:29 -0500

    more refactoring of notification mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -17,58 +17,38 @@ class NotificationMailer < ApplicationMailer
   end
 
   def grade_export(course, user, csv_data)
-    @course = course
-    @user = user
+    set_export_ivars(course, user)
     attachments["grade_export_#{course.id}.csv"] = {:mime_type => 'text/csv',:content => csv_data }
-    mail(to: @user.email,
-         bcc:ADMIN_EMAIL,
-         subject: "Grade export for #{course.name} is attached") do |format|
-      format.text
-    end
+    send_export_email "Grade export for #{course.name} is attached"
   end
 
   def gradebook_export(course, user, export_type, csv_data)
-    @course = course
-    @user = user
+    set_export_ivars(course, user)
     attachments["gradebook_export_#{course.id}.csv"] = {:mime_type => 'text/csv',:content => csv_data }
     @export_type = export_type
-    mail(to:  @user.email,
-         bcc: ADMIN_EMAIL,
-         subject: "Gradebook export for #{@course.name} #{@export_type} is attached") do |format|
-      format.text
-    end
+    send_export_email "Gradebook export for #{@course.name} #{@export_type} is attached"
   end
 
   def successful_submission(submission_id)
     set_submission_ivars(submission_id)
-    mail(to: @user.email, subject: "#{@course.courseno} - #{@assignment.name} Submitted") do |format|
-      format.text
-      format.html
-    end
+    send_assignment_email_to_user "Submitted"
   end
 
   def updated_submission(submission_id)
     set_submission_ivars_with_user(submission_id)
-    mail(to: @user.email, subject: "#{@course.courseno} - #{@assignment.name} Submission Updated") do |format|
-      format.text
-      format.html
-    end
+    send_assignment_email_to_user "Submission updated"
   end
 
   def new_submission(submission_id, professor)
     set_submission_ivars_with_student(submission_id)
     @professor = professor
-    mail(to: @professor.email, subject: "#{@course[:courseno]} - #{@assignment.name} - New Submission to Grade") do |format|
-      format.text
-    end
+    send_assignment_email_to_professor "New Submission to Grade"
   end
 
   def revised_submission(submission_id, professor)
     set_submission_ivars_with_student(submission_id)
     @professor = professor
-    mail(to: @professor.email, subject: "#{@course[:courseno]} - #{@assignment.name} - Updated Submission to Grade") do |format|
-      format.text
-    end
+    send_assignment_email_to_professor "New Submission to Grade"
   end
 
   def grade_released(grade_id)
@@ -93,6 +73,30 @@ class NotificationMailer < ApplicationMailer
   end
 
   private
+
+  def send_assignment_email_to_professor(subject)
+    mail(to: @professor.email, subject: "#{@course[:courseno]} - #{@assignment.name} - #{subject}") do |format|
+      format.text
+    end
+  end
+
+  def send_assignment_email_to_user(subject)
+    mail(to: @user.email, subject: "#{@course.courseno} - #{@assignment.name} #{subject}") do |format|
+      format.text
+      format.html
+    end
+  end
+
+  def send_export_email(subject)
+    mail(to: @user.email, bcc:ADMIN_EMAIL, subject: subject) do |format|
+      format.text
+    end
+  end
+
+  def set_export_ivars(course, user)
+    @course = course
+    @user = user
+  end
 
   def set_grade_ivars(grade_id)
     @grade = Grade.find grade_id
