3529b508124586cce2ca65543502ce9b5a6df124
Mike Zazaian <mike@zop.io>
2016-01-23 04:29:33 -0500

    remove focus from grade and grade export specs

--- spec/models/assignment/exports_spec.rb
+++ spec/models/assignment/exports_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Assignment grade export methods" do
   let(:students) { build_list(:user, 2) }
   let(:course) { build(:course) }
 
-  describe "#grade_import", focus: true do
+  describe "#grade_import" do
     subject { assignment.grade_import(students, options) }
 
     let(:options) {{ encoding: "UTF-8" }}
@@ -26,7 +26,7 @@ RSpec.describe "Assignment grade export methods" do
     end
   end
 
-  describe "#gradebook_for_assignment", focus: true do
+  describe "#gradebook_for_assignment" do
     subject { assignment.gradebook_for_assignment(options) }
 
     let(:options) {{ encoding: "UTF-8" }}
