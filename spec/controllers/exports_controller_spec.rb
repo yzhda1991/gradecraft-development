6dfb588cf607bc4e2157605f8240bc18bb05eca8
Mike Zazaian <mike@zop.io>
2016-01-25 16:33:30 -0500

    remove deprecated teams spec from exports controller spec

--- spec/controllers/exports_controller_spec.rb
+++ spec/controllers/exports_controller_spec.rb
@@ -17,10 +17,5 @@ RSpec.describe ExportsController, type: :controller do
       get :index
       expect(assigns(:submissions_exports)).to eq(course.submissions_exports.order("updated_at DESC"))
     end
-
-    it "should get the teams" do
-      get :index
-      expect(assigns(:teams)).to eq(course.teams)
-    end
   end
 end
