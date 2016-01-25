ad98d89d4386d7b4007f3b32a3c655f44398ac9e
Mike Zazaian <mike@zop.io>
2016-01-25 10:58:40 -0500

    clear some comments out of application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,24 +4,14 @@ class ApplicationMailer < ActionMailer::Base
   SENDER = "GradeCraft <#{SENDER_EMAIL}>"
 
   default from: SENDER
-  # layout -> (mailer) { default_layout(mailer) }
 
   private
 
-  # @mz todo: add specs for these
   def default_layout(mailer)
     "mailers/" + mailer_name(mailer).gsub(/_mailer/,"_layout")
   end
 
-  # def template_path(mailer)
-  #   "mailers/#{mailer_name(mailer)}"
-  # end
-
   def mailer_name(mailer)
     mailer.class.name.underscore
   end
-
-  # def self.user_locale(user)
-  #   user.try(:locale) || ENV['DEFAULT_LOCALE'] || :en
-  # end
 end
