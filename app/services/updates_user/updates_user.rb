2fcf419fd8e7142dc7100b3d9ebd7354ce67a4d6
Jamie Wright <jamie@brilliantfantastic.com>
2016-01-27 10:31:09 -0500

    UpdatesUser action updates the user based on the new attributes

--- app/services/updates_user/updates_user.rb
+++ app/services/updates_user/updates_user.rb
@@ -3,7 +3,21 @@ module Services
     class UpdatesUser
       extend LightService::Action
 
-      executed do |execute|
+      expects :attributes
+
+      executed do |context|
+        attributes = context[:attributes]
+        id = attributes[:id]
+
+        begin
+          user = User.find id
+        rescue ActiveRecord::RecordNotFound => e
+          context.fail_with_rollback!(e.message, error_code: 404)
+        end
+
+        context.fail_with_rollback!("The user is invalid and cannot be saved", error_code: 422) \
+          unless user.update_attributes attributes
+        context[:user] = user
       end
     end
   end
