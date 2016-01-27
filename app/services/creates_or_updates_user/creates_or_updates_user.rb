87e74aad8d1a5015224f58ebd2bb95f0596d8290
Jamie Wright <jamie@brilliantfantastic.com>
2016-01-27 07:24:06 -0500

    Call the CreatesNewUser service

--- app/services/creates_or_updates_user/creates_or_updates_user.rb
+++ app/services/creates_or_updates_user/creates_or_updates_user.rb
@@ -1,11 +1,17 @@
+require_relative "../creates_new_user"
+
 module Services
   module Actions
     class CreatesOrUpdatesUser
       extend LightService::Action
 
-      expects :attributes, :course
+      expects :attributes, :course, :send_welcome_email
 
       executed do |context|
+        attributes = context[:attributes]
+        course = attributes[:course]
+
+        Services::CreatesNewUser.create attributes
       end
     end
   end
--- spec/services/creates_or_updates_user/creates_or_updates_user_spec.rb
+++ spec/services/creates_or_updates_user/creates_or_updates_user_spec.rb
@@ -7,13 +7,32 @@ describe Services::Actions::CreatesOrUpdatesUser do
   let(:user) { build :user }
   let(:attributes) { user.attributes }
 
+  before do
+    user_mailer = double(:user_mailer, activation_needed_email: double(:mailer, deliver_now: nil))
+    stub_const "UserMailer", user_mailer
+  end
+
   it "expects attributes to assign to the user" do
-    expect { described_class.execute course: course }.to \
+    expect { described_class.execute course: course, send_welcome_email: false }.to \
       raise_error LightService::ExpectedKeysNotInContextError
   end
 
   it "expects course to assign to the user" do
-    expect { described_class.execute attributes: attributes }.to \
+    expect { described_class.execute attributes: attributes, send_welcome_email: false }.to \
       raise_error LightService::ExpectedKeysNotInContextError
   end
+
+  it "expects the option to send the welcome email" do
+    expect { described_class.execute attributes: attributes, course: course }.to \
+      raise_error LightService::ExpectedKeysNotInContextError
+  end
+
+  it "creates the user if they do not exist" do
+    expect(Services::CreatesNewUser).to receive(:create).and_call_original
+    described_class.execute attributes: attributes, course: course, send_welcome_email: false
+  end
+
+  xit "updates the user if they do not exist"
+  xit "fails if the attributes do not have an email address to check"
+  xit "promises the created or updated user"
 end
