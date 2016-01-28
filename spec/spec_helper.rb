c11ce1bf8687279802c9b560d56a37523f308e27
Mike Zazaian <mike@zop.io>
2016-01-28 06:58:45 -0500

    remove express requirement of random file from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
 ENV["RAILS_ENV"] ||= 'test'
 
-Dir["./spec/fixtures/random_file/*.rb"].each { |f| require f }
-
 RSpec.configure do |config|
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
