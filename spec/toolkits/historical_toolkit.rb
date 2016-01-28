3d16be55a3e41dea04a6c2a12a81bb1102276e82
Jamie Wright <jamie@brilliantfantastic.com>
2016-01-28 15:30:54 -0500

    Extracted the multiple expectations where it made sense so they only test one behavior at a time

--- spec/toolkits/historical_toolkit.rb
+++ spec/toolkits/historical_toolkit.rb
@@ -33,25 +33,70 @@ RSpec.shared_examples "a historical model" do |fixture, updated_attributes|
       model.save
     end
 
-    it "returns the history for the created #{fixture}" do
-      expect(model.history.length).to eq 1
-      expect(model.history.first.changeset.keys).to include("created_at")
-      expect(model.history.first.changeset).to include({ "object" => described_class.name })
-      expect(model.history.first.changeset).to include({ "event" => "create" })
-      expect(model.history.first.changeset).to include({ "actor_id" => user.id.to_s })
-      expect(model.history.first.changeset).to include({ "recorded_at" => model.versions.last.created_at })
+    subject { model.history }
+
+    it "only contains the one event" do
+      expect(subject.length).to eq 1
     end
 
-    it "returns the changesets for an updated #{fixture}" do
-      model.update_attributes updated_attributes
-      expect(model.history.length).to eq 2
-      updated_attributes.each do |key, value|
-        expect(model.history.first.changeset).to include({ key.to_s => [nil, value] })
+    context "the changeset for the #{fixture}" do
+      subject { model.history.first.changeset }
+
+      it "includes a created at date" do
+        expect(subject.keys).to include("created_at")
       end
-      expect(model.history.first.changeset).to include({ "object" => described_class.name })
-      expect(model.history.first.changeset).to include({ "event" => "update" })
-      expect(model.history.first.changeset).to include({ "actor_id" => user.id.to_s })
-      expect(model.history.first.changeset).to include({ "recorded_at" => model.versions.last.created_at })
+
+      it "includes the class name" do
+        expect(subject).to include({ "object" => described_class.name })
+      end
+
+      it "includes the event" do
+        expect(subject).to include({ "event" => "create" })
+      end
+
+      it "includes the actor id" do
+        expect(subject).to include({ "actor_id" => user.id.to_s })
+      end
+
+      it "includes the timestamp of when the changeset occured" do
+        expect(subject).to include({ "recorded_at" => model.versions.last.created_at })
+      end
+    end
+
+    context "with an updated changeset" do
+      subject { model.history }
+
+      before { model.update_attributes updated_attributes }
+
+      it "contains the updated event" do
+        expect(subject.length).to eq 2
+      end
+
+      context "the changeset for the #{fixture}" do
+        subject { model.history.first.changeset }
+
+        it "returns the changesets for an updated #{fixture}" do
+          updated_attributes.each do |key, value|
+            expect(subject).to include({ key.to_s => [nil, value] })
+          end
+        end
+
+        it "includes the class name" do
+          expect(subject).to include({ "object" => described_class.name })
+        end
+
+        it "includes the event" do
+          expect(subject).to include({ "event" => "update" })
+        end
+
+        it "includes the actor id" do
+          expect(subject).to include({ "actor_id" => user.id.to_s })
+        end
+
+        it "includes the timestamp of when the changeset occured" do
+          expect(subject).to include({ "recorded_at" => model.versions.last.created_at })
+        end
+      end
     end
 
     it "orders the changesets so the newest changes are at the top" do
