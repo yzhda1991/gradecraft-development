e1119dd3294c3d28e00a58c0a9c098dd11fc918e
Jamie Wright <jamie@brilliantfantastic.com>
2016-01-28 07:31:22 -0500

    Helper methods to return the counts for submissions

---
+++ spec/helpers/submissions_helper_spec.rb
@@ -0,0 +1,40 @@
+require "rails_spec_helper"
+require "./app/helpers/submissions_helper"
+
+describe SubmissionsHelper do
+  let(:course) { create :course }
+
+  describe "#resubmission_count_for" do
+    let!(:grade) { create :grade, course: course, status: "Released", submission: submission,
+                   graded_at: 1.day.ago }
+    let(:submission) { create :submission, course: course, submitted_at: DateTime.now }
+
+    it "returns the number of resubmitted submissions" do
+      expect(resubmission_count_for(course)).to eq 1
+    end
+
+    it "caches the result" do
+      expect(course).to receive(:submissions).and_call_original.at_most(:twice)
+      resubmission_count_for(course)
+      resubmission_count_for(course)
+      Rails.cache.delete resubmission_count_cache_key(course)
+      resubmission_count_for(course)
+    end
+  end
+
+  describe "#ungraded_submissions_count_for" do
+    let!(:submission) { create :submission, course: course }
+
+    it "returns the number of ungraded submissions" do
+      expect(ungraded_submissions_count_for(course)).to eq 1
+    end
+
+    it "caches the result" do
+      expect(course).to receive(:submissions).and_call_original.at_most(:twice)
+      ungraded_submissions_count_for(course)
+      ungraded_submissions_count_for(course)
+      Rails.cache.delete ungraded_submissions_count_cache_key(course)
+      ungraded_submissions_count_for(course)
+    end
+  end
+end
