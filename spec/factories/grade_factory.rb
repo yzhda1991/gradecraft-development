f50dc621a65ad3dd54928912c43a43d19efac061
Mike Zazaian <mike@zop.io>
2016-01-31 15:58:43 +0100

    remove new scored grade rebase artifact from grade factory

--- spec/factories/grade_factory.rb
+++ spec/factories/grade_factory.rb
@@ -8,11 +8,6 @@ FactoryGirl.define do
       status "Released"
     end
 
-    factory :scored_grade do
-      raw_score { Faker::Number.number(5) }
-      score { Faker::Number.number(5) }
-    end
-
     # minimal conditions when a grade has been graded but not visible to students
     factory :unreleased_grade do
       raw_score { Faker::Number.number(5) }
