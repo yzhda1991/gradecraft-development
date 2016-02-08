e80b29c7b8e461ebb62544a8083e3a1e0649937e
Jamie Wright <jamie@brilliantfantastic.com>
2016-02-08 13:08:22 -0500

    Timeline#events to retrieve all the events for a course

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -4,4 +4,24 @@ class Timeline
   def initialize(course)
     @course = course
   end
+
+  def assignment_events
+    course.assignments.timelineable.with_dates
+  end
+
+  def event_events
+    course.events.with_dates
+  end
+
+  def challenge_events
+    course.challenges.with_dates if course.has_team_challenges?
+  end
+
+  def events
+    @events ||= [
+      assignment_events.to_a,
+      challenge_events.to_a,
+      event_events.to_a
+    ].flatten
+  end
 end
--- spec/models/timeline_spec.rb
+++ spec/models/timeline_spec.rb
@@ -8,4 +8,33 @@ describe Timeline do
       expect(described_class.new(course).course).to eq course
     end
   end
+
+  describe "#events" do
+    subject { described_class.new(course) }
+
+    it "includes assignments that have due dates or open dates" do
+      assignment = create(:assignment, course: course, due_at: Date.today)
+      assignment_no_date = create(:assignment, course: course, due_at: nil)
+      expect(subject.events).to eq [assignment]
+    end
+
+    it "includes events that have due dates or open dates" do
+      event = create(:event, course: course, due_at: Date.today)
+      event_no_date = create(:event, course: course, due_at: nil)
+      expect(subject.events).to eq [event]
+    end
+
+    it "includes challenge that have due dates or open dates if the course allows it" do
+      course.update_attributes team_challenges: true
+      challenge = create(:challenge, course: course, due_at: Date.today)
+      challenge_no_date = create(:challenge, course: course, due_at: nil)
+      expect(subject.events).to eq [challenge]
+    end
+
+    it "does not include challenge that have due dates or open dates if the course does not allows it" do
+      challenge = create(:challenge, course: course, due_at: Date.today)
+      challenge_no_date = create(:challenge, course: course, due_at: nil)
+      expect(subject.events).to eq []
+    end
+  end
 end
