88199376d6bcc0e571553e1599d395937becff3f
Jamie Wright <jamie@brilliantfantastic.com>
2016-02-10 07:37:55 -0500

    Ignore file_missing, file_processing, and last_confirmed_at attributes for a submission file

--- app/models/submission_file.rb
+++ app/models/submission_file.rb
@@ -8,6 +8,7 @@ class SubmissionFile < ActiveRecord::Base
 
   mount_uploader :file, AttachmentUploader
   process_in_background :file
+  has_paper_trail ignore: [:file_missing, :file_processing, :last_confirmed_at]
 
   validates :filename, presence: true, length: { maximum: 60 }
   validates :file, file_size: { maximum: 40.megabytes.to_i }
