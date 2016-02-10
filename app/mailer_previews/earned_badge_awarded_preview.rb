d75074c49a57e3e7a2742af4d53351d282d917ca
chcholman <caitlin.holman@gmail.com>
2016-02-10 09:43:36 -0500

    Sass changes

--- app/mailer_previews/earned_badge_awarded_preview.rb
+++ app/mailer_previews/earned_badge_awarded_preview.rb
@@ -1,7 +1,6 @@
 class EarnedBadgeAwardedPreview
   def earned_badge_awarded
     earned_badge = EarnedBadge.last
-    student = earned_badge.student
     NotificationMailer.earned_badge_awarded earned_badge.id
   end
 end
