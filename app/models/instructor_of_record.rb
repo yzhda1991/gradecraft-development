e4588ae7b0f73ca6ed6fc8d2907acf3874dcb84a
Jamie Wright <jamie@brilliantfantastic.com>
2016-02-11 10:09:19 -0500

    Remove instructors of record that are not included in the user ids

--- app/models/instructor_of_record.rb
+++ app/models/instructor_of_record.rb
@@ -6,6 +6,10 @@ class InstructorOfRecord
   end
 
   def update_course_memberships(user_ids)
+    current_instructors_of_record(user_ids).each do |membership|
+      membership.update_attributes instructor_of_record: false
+    end
+
     not_instructors_of_record(user_ids).map do |membership|
       membership.update_attributes instructor_of_record: true
       membership
@@ -13,6 +17,12 @@ class InstructorOfRecord
   end
 
   private
+
+  def current_instructors_of_record(user_ids)
+    course.course_memberships.select do |membership|
+      !user_ids.include?(membership.user_id) && membership.instructor_of_record?
+    end
+  end
 
   def not_instructors_of_record(user_ids)
     course.course_memberships.select do |membership|
--- spec/models/instructor_of_record_spec.rb
+++ spec/models/instructor_of_record_spec.rb
@@ -11,14 +11,24 @@ describe InstructorOfRecord do
   end
 
   describe "#update_course_memberships" do
-    context "for new instructors of record" do
-      let(:membership) { create :staff_course_membership, course: course }
+    let(:membership) { create :staff_course_membership, course: course }
 
+    context "for new instructors of record" do
       it "adds instructors of record to the course membership" do
         memberships = InstructorOfRecord.new(course).update_course_memberships([membership.user_id])
 
         expect(course.instructors_of_record).to eq [membership.user]
         expect(memberships.map(&:id)).to eq [membership.id]
+      end
+    end
+
+    context "for existing instructors of record" do
+      it "removes instructors of record to the course membership if they are not included" do
+        membership.update_attributes instructor_of_record: true
+        memberships = InstructorOfRecord.new(course).update_course_memberships([])
+
+        expect(course.instructors_of_record).to be_empty
+        expect(memberships.map(&:id)).to be_empty
       end
     end
   end
