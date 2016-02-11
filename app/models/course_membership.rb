55ac7b79c4b00a53178c2afcee8a8bfad59a0ad4
Jamie Wright <jamie@brilliantfantastic.com>
2016-02-11 11:34:02 -0500

    Add instructors_of_record scope to retrieve memberships with instructors of record set to true

--- app/models/course_membership.rb
+++ app/models/course_membership.rb
@@ -18,6 +18,7 @@ class CourseMembership < ActiveRecord::Base
 
   scope :auditing, -> { where( :auditing => true ) }
   scope :being_graded, -> { where( :auditing => false) }
+  scope :instructors_of_record, -> { where(instructor_of_record: true) }
 
   validates_presence_of :course, :user, :role
 
