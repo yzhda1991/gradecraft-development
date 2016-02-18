13d94d4f5b6c51d307c28b2868b0514673993004
Jamie Wright <jamie@brilliantfantastic.com>
2016-02-18 12:10:05 -0500

    String class to wrap additional functionality. No 🐒 patching

---
+++ spec/lib/string_spec.rb
@@ -0,0 +1,51 @@
+require "./lib/string"
+
+describe Gradecraft::String do
+  describe "#past_tense" do
+    xit "adds a 'd' to the end of the string" do
+      subject = described_class.new("create")
+
+      expect(subject.past_tense).to eq "created"
+    end
+  end
+
+  describe "#to_s" do
+    it "returns a string representation" do
+      expect(described_class.new(123).to_s).to eq "123"
+    end
+  end
+
+  describe "#==" do
+    it "returns true for other strings" do
+      expect(described_class.new("blah") == "blah").to eq true
+    end
+
+    it "returns true for values that respond to to_s" do
+      expect(described_class.new(123) == "123").to eq true
+    end
+
+    it "returns true using eql method" do
+      expect(described_class.new("blah")).to eq "blah"
+    end
+  end
+
+  describe "#method_missing" do
+    it "returns the same thing a string would" do
+      expect(described_class.new("123").chars).to eq ["1", "2", "3"]
+    end
+
+    it "wraps the result in this string class for chaining" do
+      expect(described_class.new("blah").capitalize).to be_a_kind_of described_class
+    end
+
+    it "raises a NoMethodError if the method is not defined on string" do
+      expect { described_class.new("123").blah }.to raise_error NoMethodError
+    end
+  end
+
+  describe "#respond_to_missing?" do
+    it "responds to the string interface" do
+      expect(described_class.new("123").respond_to?(:chars)).to eq true
+    end
+  end
+end
