91adf482b44e7727ec1ca2a227f039330a1c9872
Mike Zazaian <mike@zop.io>
2016-02-20 17:33:17 +0100

    update PageviewEventLogger spec examples for #event_attrs and #page

--- spec/event_loggers/pageview_event_logger_spec.rb
+++ spec/event_loggers/pageview_event_logger_spec.rb
@@ -27,14 +27,11 @@ RSpec.describe PageviewEventLogger, type: :background_job do
 
   describe "#event_attrs" do
     subject { new_logger.event_attrs }
-    let(:request_path) { "/path/to/chaos" }
 
-    before do
-      allow(request).to receive(:original_fullpath) { request_path }
-    end
+    before { allow(new_logger).to receive(:page) { "some great page" } }
 
     it "merges the page from the original request with the base_attrs" do
-      expect(subject).to eq new_logger.base_attrs.merge(page: request_path)
+      expect(subject).to eq new_logger.base_attrs.merge(page: "some great page")
     end
 
     it "caches the #event_attrs" do
@@ -48,4 +45,29 @@ RSpec.describe PageviewEventLogger, type: :background_job do
       expect(new_logger.instance_variable_get(:@event_attrs)).to eq(new_logger.event_attrs)
     end
   end
+
+  describe "#page" do
+    subject { new_logger.page }
+    let(:request_path) { "/path/to/chaos" }
+
+    before do
+      allow(request).to receive(:original_fullpath) { request_path }
+    end
+
+    it "gets the original fullpath from the request" do
+      expect(subject).to eq request_path
+    end
+
+    it "caches the #page" do
+      subject
+      expect(request).not_to receive(:try).with(:original_fullpath)
+      subject
+    end
+
+    it "sets the page to @page" do
+      subject
+      expect(new_logger.instance_variable_get(:@page)).to eq(request_path)
+    end
+  end
+
 end
