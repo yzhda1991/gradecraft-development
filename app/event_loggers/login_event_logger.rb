bf057595a778bbde2fcdc58832ee1943b1bdc21e
Mike Zazaian <mike@zop.io>
2016-02-20 14:25:42 +0100

    move last_login_at attribute merge into perform block to ensure course membership is pulled when the job is run rather than when the event is triggered

--- app/event_loggers/login_event_logger.rb
+++ app/event_loggers/login_event_logger.rb
@@ -6,12 +6,22 @@ class LoginEventLogger < EventLogger::Base
   @event_name = "Login"
   @analytics_class = Analytics::LoginEvent
 
+  # instance methods, for use as a LoginEventLogger instance
+  def event_type
+    "login"
+  end
+
   # class methods, for use when being called directly by Resque
   # perform block that is ultimately called by Resque
   def self.perform(event_type, data={})
     @data = data
+    data.merge!(last_login_at: previous_last_login_at)
     super
     update_last_login
+  end
+
+  def self.previous_last_login_at
+    course_membership.last_login_at.to_i if course_membership.last_login_at
   end
 
   def self.update_last_login
@@ -19,31 +29,10 @@ class LoginEventLogger < EventLogger::Base
   end
 
   def self.course_membership
-    CourseMembership.where(course_membership_attrs).first
+    @course_membership ||= CourseMembership.where(course_membership_attrs).first
   end
 
   def self.course_membership_attrs
     { course_id: @data[:course_id], user_id: @data[:user_id] }
-  end
-
-  # instance methods, for use as a LoginEventLogger instance
-  def event_type
-    "login"
-  end
-
-  def event_attrs
-    @event_attrs ||= base_attrs.merge({ last_login_at: previous_last_login_at })
-  end
-
-  def course_membership
-    @course_membership ||= CourseMembership.where(course_membership_attrs).first
-  end
-
-  def previous_last_login_at
-    course_membership.last_login_at.to_i if course_membership.last_login_at
-  end
-
-  def course_membership_attrs
-    { course_id: base_attrs[:course_id], user_id: base_attrs[:user_id] }
   end
 end
