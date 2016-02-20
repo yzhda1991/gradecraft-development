054bfb0d3f8b0a340ca3ab5530d631001b45e8f7
Mike Zazaian <mike@zop.io>
2016-02-20 14:31:58 +0100

    revert EventLoggerSpecs to build event_attrs in #perform block rather than after event instantiation

--- spec/event_loggers/login_event_logger_spec.rb
+++ spec/event_loggers/login_event_logger_spec.rb
@@ -33,11 +33,18 @@ RSpec.describe LoginEventLogger, type: :background_job do
   it_behaves_like "an EventLogger subclass", LoginEventLogger, "login"
   it_behaves_like "EventLogger::Enqueue is included", LoginEventLogger, "login"
 
+
   describe "class methods" do
     describe "self#peform" do
       subject { class_instance.perform('login', logger_attrs) }
 
       before(:each) { course_membership }
+
+      it "merges the previous last_login_at value into the data hash" do
+        allow(class_instance).to receive(:previous_last_login_at) { last_login.to_i }
+        expect(logger_attrs).to receive(:merge!).with({ last_login_at: last_login.to_i })
+        subject
+      end
 
       it "sets the data hash to @data" do
         subject
@@ -85,6 +92,17 @@ RSpec.describe LoginEventLogger, type: :background_job do
       it "returns the correct course membership" do
         expect(subject).to eq(course_membership)
       end
+
+      it "caches the course membership" do
+        subject
+        expect(CourseMembership).not_to receive(:where)
+        subject
+      end
+
+      it "sets the course membership to @course_membership" do
+        subject
+        expect(class_instance.instance_variable_get(:@course_membership)).to eq(course_membership)
+      end
     end
 
     describe "self#course_membership_attrs" do
@@ -100,60 +118,11 @@ RSpec.describe LoginEventLogger, type: :background_job do
       end
     end
 
-    describe "#event_attrs" do
-      subject { new_logger.event_attrs }
-      let(:previous_last_login_at) { (Time.zone.now - 5.days).to_i }
+    describe "self#previous_last_login_at" do
+      subject { class_instance.previous_last_login_at }
 
       before do
-        allow(new_logger).to receive(:previous_last_login_at) { previous_last_login_at }
-      end
-
-      it "merges the page from the original request with the base_attrs" do
-        expect(subject).to eq new_logger.base_attrs.merge(last_login_at: previous_last_login_at)
-      end
-
-      it "caches the #event_attrs" do
-        subject
-        expect(new_logger.base_attrs).not_to receive(:merge)
-        subject
-      end
-
-      it "sets the event attrs to @event_attrs" do
-        subject
-        expect(new_logger.instance_variable_get(:@event_attrs)).to eq(new_logger.event_attrs)
-      end
-    end
-
-    describe "#course_membership" do
-      subject { new_logger.course_membership }
-
-      before do
-        new_logger.instance_variable_set(:@course_membership, nil)
-        course_membership # cache the course membership
-        allow(new_logger).to receive(:course_membership_attrs) {{ course_id: course.id, user_id: user.id }}
-      end
-
-      it "returns the correct course membership" do
-        expect(subject).to eq(course_membership)
-      end
-
-      it "caches the course membership" do
-        subject
-        expect(CourseMembership).not_to receive(:where)
-        subject
-      end
-
-      it "sets the course membership to @course_membership" do
-        subject
-        expect(new_logger.instance_variable_get(:@course_membership)).to eq(course_membership)
-      end
-    end
-
-    describe "previous_last_login_at" do
-      subject { new_logger.previous_last_login_at }
-
-      before do
-        allow(new_logger).to receive(:course_membership) { course_membership }
+        allow(class_instance).to receive(:course_membership) { course_membership }
       end
 
       context "course membership has a last_login_at value" do
@@ -170,17 +139,6 @@ RSpec.describe LoginEventLogger, type: :background_job do
         end
       end
     end
-
-    describe "#course_membership_attrs" do
-      subject { new_logger.course_membership_attrs }
-      let(:funky_attrs) {{ course_id: 90, user_id: 150 }}
-      before { allow(new_logger).to receive(:base_attrs) { funky_attrs }}
-
-      it "returns the timestamp as an integer in seconds" do
-        expect(subject).to eq(funky_attrs)
-      end
-    end
-
 
   end
 end
