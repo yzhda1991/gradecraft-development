5336e7cd4c0c9015c1994619f878ba3eec6be321
Mike Zazaian <mike@zop.io>
2016-02-22 12:38:29 +0100

    remove #stub_zone_now from UniMock support module

--- spec/support/uni_mock/stub_time.rb
+++ spec/support/uni_mock/stub_time.rb
@@ -3,9 +3,5 @@ module UniMock
     def stub_now(parseable_date)
       allow(Time).to receive(:now) { Date.parse(parseable_date).to_time }
     end
-
-    def stub_zone_now(parseable_date)
-      allow(Time.zone).to receive(:now) { Date.parse(parseable_date).to_time }
-    end
   end
 end
