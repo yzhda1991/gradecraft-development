acdc3a8247e0b26b87040113369abd6cb6fc84e9
Mike Zazaian <mike@zop.io>
2016-02-23 16:53:30 +0100

    add a complete demo configuration for testing configurability in the Analytics module spec

--- spec/lib/analytics_spec.rb
+++ spec/lib/analytics_spec.rb
@@ -8,13 +8,17 @@ describe Analytics, type: :vendor_library do
   # for the target class in which IsConfigurable is included
   demo_config = {
     event_aggregates: {
-      predictor: [
-        "some_constant_here_actually"
-      ]
+      predictor: ["some_constant_here_actually"]
     },
-    default_granularity_options_for_select: ,
-    default_range_options_for_select: [['Past Day', 'past_day']],
-    exports: []
+    default_granularity_options_for_select: [
+      ["Monthly", :monthly],
+      ["Weekly", :weekly]
+    ],
+    default_range_options_for_select: [
+      ['Past Day', 'past_day'],
+      ['Past Year', 'past_year']
+    ],
+    exports: ["some_other_constant_here"]
   }
 
   it_behaves_like "it is configurable", Analytics, demo_config
