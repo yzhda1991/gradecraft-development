aa7ca886c1bfa4c7fba1861d7e43e07624a65ccb
Mike Zazaian <mike@zop.io>
2016-02-23 17:08:36 +0100

    tweak self.notify_event_outcome block in EventLogger::Base spec to explicitly expire stale test doubles

--- spec/lib/event_logger/base_spec.rb
+++ spec/lib/event_logger/base_spec.rb
@@ -48,10 +48,10 @@ describe EventLogger::Base, type: :vendor_library do
   end
 
   describe "self.notify_event_outcome(event, data)" do
-    let(:valid_event) { double(Logger, valid?: true ) }
-    let(:invalid_event) { double(Logger, valid?: false ) }
+    let!(:valid_event) { double(Logger, valid?: true ) }
+    let!(:invalid_event) { double(Logger, valid?: false ) }
 
-    before do
+    before(:each) do
       described_class.instance_variable_set(:@success_message, "great stuff happened")
       described_class.instance_variable_set(:@failure_message, "bad stuff happened")
     end
