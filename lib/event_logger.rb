c1d660851ed7508749d66f2a600005389dac3123
Mike Zazaian <mike@zop.io>
2016-02-24 15:28:59 +0100

    add top-level vendor library summaries for EventLogger and ResqueJob modules

--- lib/event_logger.rb
+++ lib/event_logger.rb
@@ -1,6 +1,13 @@
 require 'resque-retry'
 require 'resque/errors'
 
+# this module is the initial declaration and entry point for the EventLogger
+# library. It should be used for all system Resque jobs that directly interact
+# with the Analytics library or log events. If you need to create a background
+# job that doesn't interact with analytics or events it's probably best to use
+# the ResqueJob library as it's behaviors are agnostic to how it's being used,
+# whereas the EventLogger jobs presume interaction with event Analytics and
+# logging to the Mongo backend
 module EventLogger
   extend IsConfigurable
 end
--- lib/resque_job.rb
+++ lib/resque_job.rb
@@ -1,3 +1,8 @@
+# this module is the initial declaration and entry point for the ResqueJob
+# library. It should be used for all system Resque jobs that don't directly
+# interact with the Analytics library or log events. In those instances the
+# similar EventLogger library should be used since it's tailored to
+# specifically handling jobs that are responsible for logging events in mongo
 module ResqueJob
   extend IsConfigurable
 end
