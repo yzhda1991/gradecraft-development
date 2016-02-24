0b417e463fcc4fc456144b07b7f67cad5ddd45bd
Mike Zazaian <mike@zop.io>
2016-02-24 16:33:13 +0100

    add a top-level comment overview of ApplicationEventLogger

--- app/event_loggers/application_event_logger.rb
+++ app/event_loggers/application_event_logger.rb
@@ -1,3 +1,13 @@
+# This should be inherited by all other EventLogger classes in the application,
+# or at least ones being defined in /app/event_loggers.
+#
+# The most pertinent behaviors here are the definition of an application-
+# specific #base_attrs method which will be used as a basis for all other
+# event_attrs hashes in EventLogger classes throughout the application
+#
+# Also event_session[:params] here is being redefined as #params in the
+# interest of added clarity for the use of EventLogger params in situations
+# where they're passed directly into the event logger class
 class ApplicationEventLogger < EventLogger::Base
   @queue = :application_event_logger
   @event_name = "Application"
