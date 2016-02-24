16f980027a97b84249b7ed720c6087ed5d3cbc3d
Mike Zazaian <mike@zop.io>
2016-02-24 16:06:12 +0100

    add a top-level comment and description for EventLogger::Enqueue

--- lib/event_logger/enqueue.rb
+++ lib/event_logger/enqueue.rb
@@ -1,4 +1,15 @@
 module EventLogger
+  # This module should be included in classes that inherit from
+  # EventLogger::Base, or really any class that you'd like to use for enqueuing
+  # job behaviors without writing your own DSL for that process.
+  #
+  # The one caveat on using this on a class beside EventLogger::Base is that
+  # Resque will expect a .perform method on the target class in the event that
+  # the enqueue fails and the fallback is used.
+  #
+  # For reference the self.class.perform calls for the fallback here should
+  # probably just replaced with a fallback method which can be overwritten
+  # without having to overwrite either of the enqueue_with_fallback methods.
   module Enqueue
     def initialize(event_session={})
       @event_session = event_session
@@ -22,31 +33,27 @@ module EventLogger
     end
 
     def enqueue_with_fallback
-      begin
-        # schedule the event in the background if Resque is available
-        enqueue
-      rescue
-        # otherwise insert directly into Mongo
-        self.class.perform(event_type, event_attrs)
-      end
+      # schedule the event in the background if Resque is available
+      enqueue
+    rescue
+      # otherwise insert directly into Mongo
+      self.class.perform(event_type, event_attrs)
     end
 
     def enqueue_in_with_fallback(time_until_start)
-      begin
-        # schedule the event for later if Resque is available
-        enqueue_in(time_until_start)
-      rescue
-        # otherwise insert directly into Mongo
-        self.class.perform(event_type, event_attrs)
-      end
+      # schedule the event for later if Resque is available
+      enqueue_in(time_until_start)
+    rescue
+      # otherwise insert directly into Mongo
+      self.class.perform(event_type, event_attrs)
     end
 
     # this is the default attribute set for EventLogger classes
     # should be extended in #event_attrs inside of child classes for better
     # granularity when more specific attributes are needed.
     #
-    # Ideally this will be reorganized data from #event_session into a hash that's
-    # persistable in Mongo or whatever the back-end event store is
+    # Ideally this will be reorganized data from #event_session into a hash
+    # that's persistable in Mongo or whatever the back-end event store is
     def base_attrs
       @base_attrs ||= { created_at: Time.zone.now }
     end
