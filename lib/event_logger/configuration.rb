b398aef98c59dbbaafa81a1f5f6360aae6c470f0
Mike Zazaian <mike@zop.io>
2016-02-24 15:07:32 +0100

    add better commenting for Configuration classes used by IsConfigurable

--- lib/event_logger/configuration.rb
+++ lib/event_logger/configuration.rb
@@ -1,9 +1,20 @@
+# this is the default configuration module for EventLogger. Any configuration
+# values that need to be modified on the system-wide configuration for
+# EventLogger should have an attr_accessor value set here.
+
 module EventLogger
   class Configuration
+
+    # all accessible configuration values
     attr_accessor :backoff_strategy
 
+    # these are the default configuration values for EventLogger
+    # If you'd like to override these configuration values please do so in
+    # /config/initializers/resque_job.rb or wherever the inititalizers are
+    # stored in the application
     def initialize
       self.backoff_strategy = []
     end
+
   end
 end
--- lib/resque_job/configuration.rb
+++ lib/resque_job/configuration.rb
@@ -1,9 +1,20 @@
+# this is the default configuration module for ResqueJob. Any configuration
+# values that need to be modified on the system-wide configuration for
+# ResqueJob should have an attr_accessor value set here.
+
 module ResqueJob
   class Configuration
+
+    # all accessible configuration values
     attr_accessor :backoff_strategy
 
+    # these are the default configuration values for ResqueJob
+    # If you'd like to override these configuration values please do so in
+    # /config/initializers/resque_job.rb or wherever the inititalizers are
+    # stored in the application
     def initialize
       self.backoff_strategy = []
     end
+
   end
 end
