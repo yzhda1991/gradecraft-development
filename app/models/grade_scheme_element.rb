73ba76ed8e88a1e472ca87ce1f7c5f395d97b5fd
chcholman <caitlin.holman@gmail.com>
2016-02-26 15:42:16 -0500

    Removing default scope on grade scheme elements, cleaning up per rubocop

--- app/models/grade_scheme_element.rb
+++ app/models/grade_scheme_element.rb
@@ -1,22 +1,25 @@
+# Provides the mapping between student's progress and the overall course grade
 class GradeSchemeElement < ActiveRecord::Base
   include Copyable
-  attr_accessible :letter, :low_range, :high_range, :level, :description, :course_id, :course, :updated_at
+  attr_accessible :letter, :low_range, :high_range, :level, :description,
+                  :course_id, :course, :updated_at
 
   belongs_to :course, touch: true
 
   validates_presence_of :low_range, :high_range, :course
-  validates_numericality_of :high_range, greater_than: Proc.new { |e| e.low_range.to_i }
-
-  default_scope { order "high_range DESC" }
+  validates_numericality_of :high_range,
+                            greater_than: proc { |e| e.low_range.to_i }
 
   scope :for_course, -> (course_id) { where(course_id: course_id) }
   scope :order_by_low_range, -> { order "low_range ASC" }
+  scope :order_by_high_range, -> { order "high_range DESC" }
 
   def self.default
     GradeSchemeElement.new(level: "Not yet on board")
   end
 
-  # Getting the name of the Grade Scheme Element - the Level if it's present, the Letter if not
+  # Getting the name of the Grade Scheme Element - the Level if it's present,
+  # the Letter if not
   def name
     if level? && letter?
       "#{letter} / #{level}"
@@ -29,23 +32,23 @@ class GradeSchemeElement < ActiveRecord::Base
     end
   end
 
-  #Calculating the range that covers this element
+  # Calculating the range that covers this element
   def range
     high_range.to_f - low_range.to_f
   end
 
-  #Figuring out how many points a student has to earn the next level
+  # Figuring out how many points a student has to earn the next level
   def points_to_next_level(student, course)
-    #if high range, +1
+    # if high range, +1
     high_range - student.cached_score_for_course(course) + 1
   end
 
-  #Calculating how far a student is through this level
+  # Calculating how far a student is through this level
   def progress_percent(student)
-    ((student.cached_score_for_course(self.course) - low_range)/(range)) * 100
+    ((student.cached_score_for_course(course) - low_range) / range) * 100
   end
 
   def within_range?(score)
-    score >= self.low_range && score <= self.high_range
+    score >= low_range && score <= high_range
   end
 end
