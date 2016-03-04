1216d29bcd94f750e803e41bc659eb4d84cb8bf7
Mike Zazaian <mike@zop.io>
2016-03-04 13:40:21 +0100

    improve 190-bit secret key regex by adding secure \A and \z start and end characters

--- config/initializers/regex.rb
+++ config/initializers/regex.rb
@@ -3,8 +3,9 @@ REGEX = {
   "UUID" => /\A[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}\z/,
 
   # regex for testing formatting of 190-bit secure keys, 254 characters
-  "190_BIT_SECRET_KEY" => /[a-zA-Z0-9_\-]{254}/,
+  "190_BIT_SECRET_KEY" => /\A[a-zA-Z0-9_\-]{254}\z/,
 
-  # regex for testing formatting of 512-bit secure encrypted keys, 1049 or 1050 characters
+  # regex for testing formatting of 512-bit secure encrypted keys,
+  # 1049 or 1050 characters
   "512_BIT_ENCRYPTED_KEY" => /\A[a-f0-9\$]{1049,1050}\z/
 }
