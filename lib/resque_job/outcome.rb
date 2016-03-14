218c905af8bcd32753f099c76ba81e07692291e3
Mike Zazaian <mike@zop.io>
2016-03-14 13:56:22 +0100

    minor style modifications in ResqueJob::Outcome, clean up specs

--- lib/resque_job/outcome.rb
+++ lib/resque_job/outcome.rb
@@ -1,25 +1,21 @@
 class ResqueJob::Outcome
   def initialize(result, options={})
     @result = result
-    @message = nil
     @options = options
   end
 
-  attr_reader :result, :options
-  attr_accessor :message
+  attr_accessor :message, :result, :options
 
-  def truthy?
-    @result != false && @result != nil
+  def success?
+    result != false && result != nil
   end
-  alias success? truthy?
 
-  def falsey?
-    @result == false || @result.nil?
+  def failure?
+    result == false || result.nil?
   end
-  alias failure? falsey?
 
   def result_excerpt
     return result unless options[:max_result_size]
-    "#{result}"[0..@options[:max_result_size].to_i] rescue "#{result}"
+    "#{result}"[0..options[:max_result_size].to_i]
   end
 end
