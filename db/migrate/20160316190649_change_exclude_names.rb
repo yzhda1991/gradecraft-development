2787d9384a5d15cf6e6ceaac62516aaa24840a40
chcholman <caitlin.holman@gmail.com>
2016-03-16 17:13:06 -0400

    Migrations fix

--- db/migrate/20160316190649_change_exclude_names.rb
+++
@@ -1,6 +0,0 @@
-class ChangeExcludeNames < ActiveRecord::Migration
-  def change
-    rename_column :grades, :excluded_date, :excluded_at
-    rename_column :grades, :excluded_by, :excluded_by_id
-  end
-end
