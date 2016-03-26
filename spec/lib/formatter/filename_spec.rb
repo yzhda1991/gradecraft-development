5ee05e6a3846a2a5a9365d4f9a381e312dcec4f8
Mike Zazaian <mike@zop.io>
2016-03-26 18:45:13 +0100

    add some spec examples for accessible attributes and initial filename sanitization spec

--- spec/lib/formatter/filename_spec.rb
+++ spec/lib/formatter/filename_spec.rb
@@ -11,4 +11,28 @@ describe Formatter::Filename do
       expect(subject.original_filename).to eq foo_filename
     end
   end
+
+  describe "accessible attributes" do
+    it "has an accessible filename" do
+      subject.filename = "rex harrison"
+      expect(subject.filename).to eq "rex harrison"
+    end
+
+    it "has an accessible original filename" do
+      subject.original_filename = "rex chapman"
+      expect(subject.original_filename).to eq "rex chapman"
+    end
+  end
+
+  describe "#sanitize" do
+    let(:result) { subject.sanitize }
+
+    it "downcases the filename" do
+      # note that we can't access the filename directly from the #sanitize call
+      # because Formatter::Filename#sanitize returns the object rather than the
+      # resulting filename attribute
+      subject.filename = "SERIOUSLY_THIS_IS_CAPPY"
+      expect(subject.sanitize.filename).to eq "seriously_this_is_cappy"
+    end
+  end
 end
