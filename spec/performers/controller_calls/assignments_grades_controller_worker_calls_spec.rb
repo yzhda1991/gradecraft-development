651341e8fbc90484efc8281de98f96b875a6c99b
Jamie Wright <jamie@brilliantfantastic.com>
2016-04-27 15:35:41 -0400

    Moved the worker calls specs to their appropriate controller

--- spec/performers/controller_calls/assignments_grades_controller_worker_calls_spec.rb
+++ spec/performers/controller_calls/assignments_grades_controller_worker_calls_spec.rb
@@ -10,12 +10,26 @@ RSpec.describe Assignments::GradesController, type: :controller, background_job:
   let(:job_attributes) {{ grade_id: grade.id }} # for GradeUpdaterJob calls
   let(:grade_attributes) {{ course_id: course.id, student_id: student.id, assignment_id: assignment.id }}
   let(:grade) { create(:grade, grade_attributes) }
+  let(:cache_everything) { course; assignment; grade; student; professor }
 
   let(:student) { create(:user) }
   let(:professor) { create(:user) }
   let(:enroll_professor) { CourseMembership.create(user_id: professor.id, course_id: course.id, role: "professor") }
+  let(:enroll_student) { CourseMembership.create(user_id: student.id, course_id: course.id, role: "student") }
 
   before(:each) { ResqueSpec.reset! }
+
+  context "triggering jobs as a student" do
+    describe "#self_log", focus: true do
+      before { allow_any_instance_of(Assignment).to receive(:student_logged?) { true }}
+      let(:request_attrs) {{ assignment_id: assignment.id }}
+      subject { post :self_log, request_attrs }
+      before { enroll_and_login_student }
+      before(:each) { cache_everything }
+
+      it_behaves_like "a successful resque job", GradeUpdaterJob
+    end
+  end
 
   describe "triggering jobs as a professor" do
     before(:each) { enroll_and_login_professor }
@@ -60,6 +74,42 @@ RSpec.describe Assignments::GradesController, type: :controller, background_job:
           it_behaves_like "a failed resque job", GradeUpdaterJob
         end
       end
+
+      describe "POST #upload" do
+        subject { post :upload, request_attrs }
+        before { enroll_and_login_professor }
+        let(:file) { fixture_file "grades.csv", "text/csv" }
+        let(:result_double) { double(:import_result) }
+
+        context "params[:file] is present" do
+          # only call the job if a file is present
+          let(:request_attrs) {{
+            id: assignment.id,
+            file: file,
+            assignment_id: assignment.id
+          }}
+
+          before do
+            # establishing state for controller to trigger job
+            allow(controller).to receive_message_chain(:current_course, :assignments, :find) { assignment }
+            allow(course).to receive(:students) { students }
+            allow(professor).to receive(:admin?) { true } # let's call the prof an admin for now
+            # stub away before filters
+            allow(request).to receive_message_chain(:format, :html?) { false } # increment_page_views
+            allow(controller).to receive_message_chain(:current_student, :present?) { false } # get_course_scores
+            # some more stubs
+            allow(controller).to receive_message_chain(:current_course, :students) { students }
+            allow(GradeImporter).to receive_message_chain(:new, :import) { result_double }
+            allow(result_double).to receive(:successful).and_return(grades)
+          end
+
+          let(:batch_attributes) do
+            [{ grade_id: grades.first.id }, { grade_id: grades.last.id }]
+          end
+
+          it_behaves_like "a batch of successful resque jobs", 2, GradeUpdaterJob
+        end
+      end
     end
   end
 
@@ -67,6 +117,13 @@ RSpec.describe Assignments::GradesController, type: :controller, background_job:
     allow(controller).to receive_messages(current_student: student)
     enroll_professor
     login_user(professor)
+    session[:course_id] = course.id
+  end
+
+  def enroll_and_login_student
+    allow(controller).to receive_messages(current_student: student)
+    enroll_student
+    login_user(student)
     session[:course_id] = course.id
   end
 end
--- spec/performers/controller_calls/grades_controller_worker_calls_spec.rb
+++ spec/performers/controller_calls/grades_controller_worker_calls_spec.rb
@@ -5,32 +5,18 @@ include ResqueJobSharedExamplesToolkit
 
 RSpec.describe GradesController, type: :controller, background_job: true do
   include InQueueHelper
+
   let(:course) { create(:course_accepting_groups) }
   let(:assignment) { create(:assignment, course_id: course.id) }
   let(:job_attributes) {{ grade_id: grade.id }} # for GradeUpdaterJob calls
   let(:grade_attributes) {{ course_id: course.id, student_id: student.id, assignment_id: assignment.id }}
   let(:grade) { create(:grade, grade_attributes) }
-  let(:cache_everything) { course; assignment; grade; student; professor }
 
   let(:student) { create(:user) }
-  let(:enroll_student) { CourseMembership.create(user_id: student.id, course_id: course.id, role: "student") }
   let(:professor) { create(:user) }
   let(:enroll_professor) { CourseMembership.create(user_id: professor.id, course_id: course.id, role: "professor") }
 
   before(:each) { ResqueSpec.reset! }
-
-  context "triggering jobs as a student" do
-    describe "#self_log" do
-
-      before { allow_any_instance_of(Assignment).to receive(:student_logged?) { true } }
-      let(:request_attrs) {{ id: assignment.id }}
-      subject { get :self_log, request_attrs }
-      before { enroll_and_login_student }
-      before(:each) { cache_everything }
-
-      it_behaves_like "a successful resque job", GradeUpdaterJob
-    end
-  end
 
   describe "triggering jobs as a professor" do
     before(:each) { enroll_and_login_professor }
@@ -77,42 +63,6 @@ RSpec.describe GradesController, type: :controller, background_job: true do
       let(:grades) { [grade, grade2] }
       let(:grade_ids) { [grade.id, grade2.id] }
 
-      describe "POST #upload" do
-        subject { post :upload, request_attrs }
-        before { enroll_and_login_professor }
-        let(:file) { fixture_file "grades.csv", "text/csv" }
-        let(:result_double) { double(:import_result) }
-
-        context "params[:file] is present" do
-          # only call the job if a file is present
-          let(:request_attrs) {{
-            id: assignment.id,
-            file: file,
-            assignment_id: assignment.id
-          }}
-
-          before do
-            # establishing state for controller to trigger job
-            allow(controller).to receive_message_chain(:current_course, :assignments, :find) { assignment }
-            allow(course).to receive(:students) { students }
-            allow(professor).to receive(:admin?) { true } # let's call the prof an admin for now
-            # stub away before filters
-            allow(request).to receive_message_chain(:format, :html?) { false } # increment_page_views
-            allow(controller).to receive_message_chain(:current_student, :present?) { false } # get_course_scores
-            # some more stubs
-            allow(controller).to receive_message_chain(:current_course, :students) { students }
-            allow(GradeImporter).to receive_message_chain(:new, :import) { result_double }
-            allow(result_double).to receive(:successful).and_return(grades)
-          end
-
-          let(:batch_attributes) do
-            [{ grade_id: grades.first.id }, { grade_id: grades.last.id }]
-          end
-
-          it_behaves_like "a batch of successful resque jobs", 2, GradeUpdaterJob
-        end
-      end
-
       describe "PUT #update_status" do
         subject { put :update_status, request_attrs }
         before { enroll_and_login_professor }
@@ -130,13 +80,6 @@ RSpec.describe GradesController, type: :controller, background_job: true do
     allow(controller).to receive_messages(current_student: student)
     enroll_professor
     login_user(professor)
-    session[:course_id] = course.id
-  end
-
-  def enroll_and_login_student
-    allow(controller).to receive_messages(current_student: student)
-    enroll_student
-    login_user(student)
     session[:course_id] = course.id
   end
 end
