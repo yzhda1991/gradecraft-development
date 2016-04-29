e48520984a1239aba514520379240deb5da57967
Jamie Wright <jamie@brilliantfantastic.com>
2016-04-29 15:24:30 -0400

    Extract to methods to reduce cyclomatic complexity issues

--- lib/grade_proctor/updatable.rb
+++ lib/grade_proctor/updatable.rb
@@ -13,15 +13,32 @@ class GradeProctor
     def updatable?(options={})
       return false if grade.nil?
 
-      user = options[:user] || grade.student
-      course = options[:course] || grade.course
-      student_logged = options[:student_logged].nil? ? true :
-        options[:student_logged]
+      user = user_from_options(options)
+      course = grade_from_options(options)
+      student_logged = student_logged_from_options(options)
 
       grade_for_course?(course) &&
         (user.is_staff?(course) ||
           (grade_for_user?(user) && (!student_logged ||
                                      grade.assignment.student_logged?)))
+    end
+
+    private
+
+    def grade_from_options(options)
+      options[:course] || grade.course
+    end
+
+    def student_logged_from_options(options)
+      student_logged = true
+      unless options[:student_logged].nil?
+        student_logged = options[:student_logged]
+      end
+      student_logged
+    end
+
+    def user_from_options(options)
+      options[:user] || grade.student
     end
   end
 end
