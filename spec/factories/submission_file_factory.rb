ef19dcc45d871742b066f301646425548f55374e
Mike Zazaian <mike@zop.io>
2016-04-29 14:34:41 -0400

    use a tempfile instead of fixture in the submission file factory

--- spec/factories/submission_file_factory.rb
+++ spec/factories/submission_file_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :submission_file do
     association :submission
     filename "test_image.jpg"
-    file { fixture_file("test_image.jpg", "img/jpg") }
+    file { Tempfile.new ["test_image", ".jpg"] }
 
     factory :confirmed_submission_file do
       last_confirmed_at Time.now
