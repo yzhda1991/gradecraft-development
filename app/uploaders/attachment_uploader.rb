edd77fd8cb5e5d6c2e03f4e1a31f66096389b24e
Mike Zazaian <mike@zop.io>
2016-05-02 11:13:52 -0400

    remove 'public' root dir from test run

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -13,7 +13,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
     if Rails.env == "development"
       [ENV["AWS_S3_DEVELOPER_TAG"]].concat(store_dir_pieces).join("/")
     elsif Rails.env == "test"
-      ["public"].concat(store_dir_pieces).join("/")
+      store_dir_pieces.join("/")
     else
       store_dir_pieces.join("/")
     end
