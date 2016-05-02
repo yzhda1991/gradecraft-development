0c5d72a4f57cd6d40978df55eb92d88a0257ee0c
Mike Zazaian <mike@zop.io>
2016-05-02 12:04:19 -0400

    add a Rails.root/upload directory on spec run, let AttachmentUploader handle store_dir

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -13,7 +13,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
     if Rails.env == "development"
       [ENV["AWS_S3_DEVELOPER_TAG"]].concat(store_dir_pieces).join("/")
     elsif Rails.env == "test"
-      store_dir_pieces.join("/")
+      ["support"].concat(store_dir_pieces).join("/")
     else
       store_dir_pieces.join("/")
     end
