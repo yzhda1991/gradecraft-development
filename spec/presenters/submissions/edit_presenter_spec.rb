82b9e50e253b8dc9bc9d25abde4874d9f433e885
Mike Zazaian <mike@zop.io>
2016-05-05 22:41:16 -0400

    get rid of showing a submission spec with shared example, move corresponding examples into edit submission presenter spec

--- spec/presenters/submissions/edit_presenter_spec.rb
+++ spec/presenters/submissions/edit_presenter_spec.rb
@@ -5,7 +5,35 @@ describe Submissions::EditPresenter do
   let(:assignment) { double(:assignment) }
   before { allow(subject).to receive(:assignment).and_return assignment }
 
-  it_behaves_like "showing a submission"
+  describe "ported from shared examples" do
+    let(:assignment) { double(:assignment) }
+    let(:submission_id) { 123 }
+    subject { described_class.new id: submission_id }
+
+    before { allow(subject).to receive(:assignment).and_return assignment }
+
+    describe "#submission" do
+      it "returns the submission from the assignment based on the id" do
+        submission = double(:submission)
+        submissions = double(:active_record_relation)
+        allow(submissions).to receive(:find).with(submission_id).and_return submission
+        allow(assignment).to receive(:submissions).and_return submissions
+        expect(subject.submission).to eq submission
+      end
+    end
+
+    describe "#student" do
+      let(:submission) { double(:submission, student: student) }
+      let(:student) { double(:user) }
+
+      before { allow(subject).to receive(:submission).and_return submission }
+
+      it "returns the student for the submission" do
+        expect(subject.student).to eq student
+      end
+    end
+  end
+
 
   describe "#initialize" do
     it "allows a submission to be set" do
