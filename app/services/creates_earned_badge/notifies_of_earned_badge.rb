5c924b528ef9eebbfd11e12d1c4ab63aacf8f153
Jamie Wright <jamie@brilliantfantastic.com>
2016-05-11 16:36:09 -0400

    Implement notifies of earned badge

--- app/services/creates_earned_badge/notifies_of_earned_badge.rb
+++ app/services/creates_earned_badge/notifies_of_earned_badge.rb
@@ -3,7 +3,13 @@ module Services
     class NotifiesOfEarnedBadge
       extend LightService::Action
 
+      expects :earned_badge
+
       executed do |context|
+        if context.earned_badge.student_visible?
+          NotificationMailer.earned_badge_awarded(context.earned_badge.id)
+            .deliver_now
+        end
       end
     end
   end
