cdf18029ea891b8314b4f3caa4d3e4174f868fda
Mike Zazaian <mike@zop.io>
2016-05-23 12:20:41 -0400

    remove call to proctor/proctors

--- lib/proctor.rb
+++ lib/proctor.rb
@@ -3,7 +3,6 @@ require_relative "proctor/conditions"
 require_relative "proctor/conditions/class_methods"
 require_relative "proctor/requirement"
 require_relative "proctor/override"
-require_relative "proctor/proctors"
 
 module Proctor
 end
