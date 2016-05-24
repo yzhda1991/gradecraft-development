14410e0999795653d3bea4b74c63e31fdd69fe86
Mike Zazaian <mike@zop.io>
2016-05-24 15:04:24 -0400

    add documentation and example for Proctor::Conditions::ClassMethods

--- lib/proctor/conditions/class_methods.rb
+++ lib/proctor/conditions/class_methods.rb
@@ -1,6 +1,26 @@
 module Proctor
   module Conditions
     module ClassMethods
+      # this will help our conditions set defer specific methods to the included
+      # Proctor class so we don't have to re-define all of these relationships
+      # in the conditions set in order to make the comparison
+      #
+      # Example:
+      #
+      # class FooProctor
+      #   def foo_file
+      #     "this is the foo file!!"
+      #   end
+      # end
+      #
+      # class FooConditionSet
+      #   include Proctor::ConditionSet
+      # end
+      #
+      # condition_set = FooConditionSet.new(proctor: FooProctor.new)
+      # condition_set.foo_file
+      # => "this is the foo file!!"
+      #
       def defer_to_proctor(*deferred_methods)
         deferred_methods.each do |method_name|
           define_method(method_name) { proctor.send(method_name) }
