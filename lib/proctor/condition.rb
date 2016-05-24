c9dec3bbbf9114ac44c08702ec222233659284c7
Mike Zazaian <mike@zop.io>
2016-05-24 14:48:23 -0400

    more comments on conditions and overrides

--- lib/proctor/condition.rb
+++ lib/proctor/condition.rb
@@ -1,20 +1,29 @@
+# This class is used as the base condition class for wrapping the conditional
+# methods that determine whether a condition set has passed for a given user
 module Proctor
   class Condition
+    # the condition attribute is the uncalled Proc containing the comparison
+    #
     attr_reader :condition, :name
 
     def initialize(name:, &condition)
+      # give the condition the name in case we need it later to figure out
+      # what happened
       @name = name.to_s
       @condition = condition
     end
 
+    # call the comparison to see what happened
     def outcome
       condition.call
     end
 
+    # did the condition pass?
     def passed?
       !!outcome
     end
 
+    # did the condition fail?
     def failed?
       !outcome
     end
--- lib/proctor/override.rb
+++ lib/proctor/override.rb
@@ -1,4 +1,9 @@
 module Proctor
+  # all we need to do is subclass Condition because
+  # the behaviors are the same. We may want to add this later
+  # but for better nomenclature this should be called an Override for now
+  # citing how this is going to be handled.
+  #
   class Override < Proctor::Condition
   end
 end
