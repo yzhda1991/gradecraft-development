677a5a690f1550e78a07646474a779a5fc95b2b2
Jamie Wright <jamie@brilliantfantastic.com>
2016-05-24 16:26:45 -0400

    New controller action to handle the find or create for a grade

---
+++ spec/controllers/assignments/students_controller_spec.rb
@@ -0,0 +1,36 @@
+require "rails_spec_helper"
+
+describe Assignments::StudentsController do
+  let(:world) { World.create.with(:course, :assignment, :professor, :student) }
+
+  describe "#grade" do
+
+    context "as a professor" do
+      before(:each) { login_user(world.professor) }
+
+      it "creates the grade for the assignment and student if it doesn't exist" do
+        get :grade, assignment_id: world.assignment.id, student_id: world.student.id
+
+        grade = Grade.unscoped.last
+        expect(grade.assignment_id).to eq world.assignment.id
+        expect(grade.student_id).to eq world.student.id
+      end
+
+      it "redirects to the edit grade view" do
+        get :grade, assignment_id: world.assignment.id, student_id: world.student.id
+
+        expect(response).to redirect_to edit_grade_path(Grade.unscoped.last)
+      end
+    end
+
+    context "as a student" do
+      before(:each) { login_user(world.student) }
+
+      it "redirects to the root" do
+        get :grade, assignment_id: world.assignment.id, student_id: world.student.id
+
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+end
