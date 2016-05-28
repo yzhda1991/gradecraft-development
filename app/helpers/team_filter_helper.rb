31bb0544491841390497cc10a8851cbbadc4c7dd
chcholman <caitlin.holman@gmail.com>
2016-05-28 12:24:31 -0400

    Pulled team filter out into helper

--- app/helpers/team_filter_helper.rb
+++ app/helpers/team_filter_helper.rb
@@ -1,16 +1,11 @@
 module TeamFilterHelper
 
-  def team_filter(redirect_to)
-    capture_haml do
-      haml_tag :div, class: "team-filter" do
-        haml_tag :form_tag, redirect_to
-
-        # = form_tag redirect_to, name: "see_team", onchange: ("javascript: document.see_team.submit();"), method: :get do
-        #   %label.sr-only{:for => "team_id"}
-        #     Select #{term_for :team} –
-        #   = select_tag :team_id, options_for_select(presenter.teams.map { |t| [t.name, t.id] }, presenter.team.try(:id)), prompt: "– Select #{(term_for :team).titleize} –"
+  def team_filter
+    content_tag :div, class: 'team-filter' do
+      form_tag(request.url, name: "see_team", onchange: ("javascript: document.see_team.submit();"), method: :get) do
+        label_tag :team_id, "Select #{(term_for :team).titleize}", class: 'sr-only'
+        select_tag(:team_id, options_for_select(current_course.teams.map { |t| [t.name, t.id] }, params[:team_id]), prompt: "– Select #{(term_for :team).titleize} –")
       end
     end
   end
-
 end
