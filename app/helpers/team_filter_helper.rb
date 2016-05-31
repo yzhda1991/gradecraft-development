518e707b61358fd4dd2920d8408ceeb68be92053
chcholman <caitlin.holman@gmail.com>
2016-05-31 14:40:57 -0400

    Updated naming convention for filter

--- app/helpers/team_filter_helper.rb
+++ app/helpers/team_filter_helper.rb
@@ -2,7 +2,7 @@ module TeamFilterHelper
 
   def team_filter(teams)
     content_tag :div, class: 'team-filter' do
-      form_tag(request.url, name: "team_selector", onchange: ("javascript: document.team_selector.submit();"), method: :get) do
+      form_tag(request.url, class: "team-selector", onchange: "$('.team-selector').submit();", method: :get) do
         label_tag :team_id, "Select #{(term_for :team).titleize}", class: 'sr-only'
         select_tag(:team_id, options_for_select(teams.alpha.map { |t| [t.name, t.id] }, params[:team_id]), prompt: "– Select #{(term_for :team).titleize} –")
       end
