940e48c6f3005372e97aac1dcee67c9961cc72ae
Mike Zazaian <mike@zop.io>
2016-05-31 11:05:09 -0400

    rename SubmissionFilesPresenter to Presenters::SubmissionFiles::Base

--- app/controllers/submission_files_controller.rb
+++ app/controllers/submission_files_controller.rb
@@ -4,7 +4,12 @@ class SubmissionFilesController < ApplicationController
 
     # let's use the object_stream here because there's no reason to hit S3 twice
     if presenter.submission_file_streamable?
-      send_data(presenter.stream_submission_file, filename: presenter.filename)
+      tmp_dir = Dir.mktmpdir
+      filepath = [tmp_dir, presenter.filename].join "/"
+      streamed_file = File.new(filepath, "w") {|f| f << presenter.stream_submission_file }
+      send_file streamed_file
+
+      # send_data(presenter.stream_submission_file, filename: presenter.filename)
     else
       presenter.mark_submission_file_missing
       flash[:alert] = "The requested file was not found on the server."
---
+++ app/presenters/submission_files/base.rb
@@ -0,0 +1,38 @@
+require "showtime"
+
+module Presenters
+  module SubmissionFiles
+    class Base
+
+      def submission_file
+        return nil unless params[:submission_file_id]
+        @submission_file ||= ::SubmissionFile.find params[:submission_file_id]
+      end
+
+      def submission
+        return nil unless submission_file
+        submission_file.submission
+      end
+
+      def submission_file_streamable?
+        return false unless submission_file
+        submission_file.object_stream.exists?
+      end
+
+      def stream_submission_file
+        return false unless submission_file_streamable?
+        submission_file.object_stream.stream!
+      end
+
+      def filename
+        submission_file.instructor_filename params[:index].to_i
+      end
+
+      def mark_submission_file_missing
+        return false unless submission_file
+        submission_file.mark_file_missing
+      end
+
+    end
+  end
+end
