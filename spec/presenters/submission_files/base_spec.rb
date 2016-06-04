5304fe234de65723fa08d0ccfb890ddea4710fde
Mike Zazaian <mike@zop.io>
2016-06-04 12:33:40 -0400

    fix a couple of submission file presenter spec examples

--- spec/presenters/submission_files/base_spec.rb
+++ spec/presenters/submission_files/base_spec.rb
@@ -88,8 +88,8 @@ describe Presenters::SubmissionFiles::Base do
     end
   end
 
-  describe "#submission_file_stream" do
-    let(:result) { subject.submission_file_stream }
+  describe "#stream_submission_file" do
+    let(:result) { subject.stream_submission_file }
 
     context "the submission file is streamable" do
       it "streams the object from the submission file" do
