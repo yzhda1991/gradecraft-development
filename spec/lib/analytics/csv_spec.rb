c6b8471e1d60071f0d0f398bf0b012d1a78cc1db
Mike Zazaian <mike@zop.io>
2016-06-07 18:35:28 -0400

    fleshing out the spec examples for Analytics::Export::CSV

--- spec/lib/analytics/csv_spec.rb
+++ spec/lib/analytics/csv_spec.rb
@@ -14,11 +14,22 @@ describe Analytics::Export::CSV do
   let(:filename) { "bro.txt" }
   let(:schema_record_set) { ["the-set"] }
 
-  it "has readable attributes" do
-    expect(subject.export).to eq export
-    expect(subject.path).to eq path
-    expect(subject.filename).to eq filename
-    expect(subject.schema_records).to eq schema_record_set
+  describe "readable attributes" do
+    it "has a readable export" do
+      expect(subject.export).to eq export
+    end
+
+    it "has a readable path" do
+      expect(subject.path).to eq path
+    end
+
+    it "has a readable filename" do
+      expect(subject.filename).to eq filename
+    end
+
+    it "has readable schema_records" do
+      expect(subject.schema_records).to eq schema_record_set
+    end
   end
 
   describe "#initialize" do
@@ -28,6 +39,32 @@ describe Analytics::Export::CSV do
       it "builds a filename from the export class name" do
         allow(export).to receive_message_chain(:class, :name) { "ExportClass" }
         expect(subject.filename).to eq "export_class.csv"
+      end
+    end
+
+    context "schema_record_set doesn't exist" do
+      let(:schema_record_set) { nil }
+
+      it "uses the schema_records from the export" do
+        allow(export).to receive(:schema_records) { ["the-records"] }
+        expect(subject.schema_records).to eq ["the-records"]
+      end
+    end
+
+    context "a directory already exists at the 'path'" do
+      it "doesn't create the path" do
+        path # cache the path to make the tmpdir
+        expect(FileUtils).not_to receive(:mkdir_p).with path
+        subject
+      end
+    end
+
+    context "no directory exists at the designated 'path'" do
+      it "creates a directory at the path" do
+        path # cache the path to make the tempdir
+        FileUtils.rmdir path # get rid of the directory
+        expect(FileUtils).to receive(:mkdir_p).with path
+        subject
       end
     end
   end
