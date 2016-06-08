39c065c9c63b075f8cf53c5fe6004e68dd3398b2
Mike Zazaian <mike@zop.io>
2016-06-08 18:52:07 -0400

    start moving messaging concerns from Analytics::Export::SchemaRecords to Analytics::Export::Message to silo messaging concerns in a separate class

--- lib/analytics/export/schema_records.rb
+++ lib/analytics/export/schema_records.rb
@@ -27,14 +27,12 @@ module Analytics
             puts "    => column #{column.inspect}, row #{row.inspect}"
 
             final_hash[column] = records.each_with_index.map do |record, index|
-              # let's get the base-one record number for comparison against the
-              # size of the records array
-              record_number = index + 1
-
               # print a message for the record if it fits into our
               # messaging schema
-              print progress_message(record_number: record_number) \
-                if messageable_record?(record_number: record_number)
+              message = Analytics::Export::RecordMessage.new \
+                record_index: index,
+                total_records: records.size
+              print message.formatted_message if message.printable?
 
               # get the value from the row however we can
               next row.call(record) if row.respond_to? :call # try call first
@@ -48,31 +46,6 @@ module Analytics
       # this is the schema for the target Mongo class
       def schema
         export.class.schema
-      end
-
-      # build the progress message to show how far along we are in the export
-      def progress_message(record_number:)
-        percent_complete = (record_number * 100.0 / total_records).round
-        "\r       record #{record_number} of #{total_records} (#{percent_complete}%)"
-      end
-
-      # is this a record that we want to be printing a message for during
-      # export?
-      def messageable_record?(record_number:)
-        # we shouldn't be messaging for records beyond those in the size of the
-        # records array. this shouldn't happen regardless, but let's return false
-        # to ensure that this anomaly doesn't occur.
-        return false if record_number > total_records
-
-        # the record should be messageable if it's divisible by five, or if it's
-        # the last record in the records array
-        record_number % 5 == 0 || record_number == total_records
-      end
-
-      # this is being used a lot, so let's cache it since we can cheaply save
-      # some cycles
-      def total_records
-        @total_records ||= records.size
       end
     end
   end
