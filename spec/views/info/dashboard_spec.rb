d16849150b02a89f30a1290b9e4bc60d7790a231
Marie Hooper <marie.ashtari@gmail.com>
2016-06-13 15:33:10 -0400

    updated failing spec for dashboard view and updated name of student-dashboard container

--- spec/views/info/dashboard_spec.rb
+++ spec/views/info/dashboard_spec.rb
@@ -40,31 +40,31 @@ describe "info/dashboard" do
 
   context "as a professor" do
 
-    it "shows the timeline" do
+    it "shows the dashboard" do
       allow(view).to receive(:current_user).and_return(@professor)
       render
-      assert_select "#time_line", count: 1
+      assert_select ".student-dashboard", count: 1
     end
 
   end
 
   context "as a student" do
 
-    it "shows the timeline" do
+    it "shows the dashboard" do
       allow(view).to receive(:current_user).and_return(@student_1)
       allow(view).to receive(:current_student).and_return(@student_1)
       render
-      assert_select "#time_line", count: 1
+      assert_select ".student-dashboard", count: 1
     end
 
   end
 
   context "as a GSI" do
 
-    it "shows the timeline" do
+    it "shows the dashboard" do
       allow(view).to receive(:current_user).and_return(@gsi)
       render
-      assert_select "#time_line", count: 1
+      assert_select ".student-dashboard", count: 1
     end
 
   end
