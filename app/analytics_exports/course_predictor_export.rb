16e42315209244338621257be234cd6d41b45fb8
Mike Zazaian <mike@zop.io>
2016-06-24 14:10:34 +0200

    reorganize some methods in the CoursePredictorExport

--- app/analytics_exports/course_predictor_export.rb
+++ app/analytics_exports/course_predictor_export.rb
@@ -1,7 +1,7 @@
 class CoursePredictorExport
   include ::Analytics::Export::Model
 
-  attr_accessor :usernames, :assignment_names
+  attr_accessor :users, :assignments
 
   rows_by :events
 
@@ -14,29 +14,10 @@ class CoursePredictorExport
              possible: :possible_points,
              date_time: lambda { |event| event.created_at.to_formatted_s(:db) }
 
-  def initialize(loaded_data)
-    @loaded_data = loaded_data
-    get_and_cache_usernames
-    get_and_cache_assignment_names
-  end
-
-  # build an array or the given records in the format of
-  # { user_id => "some_username" }
-  def get_and_cache_usernames
-    @usernames ||= loaded_data[:users].inject({}) do |hash, user|
-      hash[user.id] = user.username
-      hash
-    end
-  end
-
-  # build an array or the given records in the format of
-  # { assignment_id => "some_assignment_name" }
-  def get_and_cache_assignment_names
-    assignments = loaded_data[:assignments]
-    @assignment_names ||= assignments.inject({}) do |hash, assignment|
-      hash[assignment.id] = assignment.name
-      hash
-    end
+  def initialize(context:)
+    @context = context
+    @users = context[:active_record][:users]
+    @assignments = context[:active_record][:assignments]
   end
 
   # filter the given events so that only predictor events are exported
@@ -52,6 +33,30 @@ class CoursePredictorExport
   def assignment_name(event)
     return "[assignment id: nil]" unless event.respond_to? :assignment_id
     assignment_id = event.assignment_id.to_i
-    @assignment_names[assignment_id] || "[assignment id: #{assignment_id}]"
+    assignment_names[assignment_id] || "[assignment id: #{assignment_id}]"
+  end
+
+  # since these filtering mechanisms for pulling usernames out of users
+  # and assignment_names out of assignments are being used in multiple exports,
+  # I wonder whether these should also be extracted into some kind of export
+  # presenter helper module so we can keep all of this logic in one place.
+  #
+
+  # build an array or the given records in the format of
+  # { user_id => "some_username" }
+  def usernames
+    @usernames ||= users.inject({}) do |hash, user|
+      hash[user.id] = user.username
+      hash
+    end
+  end
+
+  # build an array or the given records in the format of
+  # { assignment_id => "some_assignment_name" }
+  def assignment_names
+    @assignment_names ||= assignments.inject({}) do |hash, assignment|
+      hash[assignment.id] = assignment.name
+      hash
+    end
   end
 end
