17822e3c63fdf5c1e9077a86278680466c1b5680
Mike Zazaian <mike@zop.io>
2016-06-26 16:14:14 +0200

    digging through more of the Analytics::Export process and trying to add as much clarity and sanity as possible

--- app/analytics_exports/course_event_export.rb
+++ app/analytics_exports/course_event_export.rb
@@ -3,16 +3,32 @@ class CourseEventExport
 
   attr_reader :users
 
-  rows_by :events
+  # this is the mapping of the columns in our export to the key that should
+  # be used to filter or render the data in the row. For example:
+  #
+  # role: :user_role
+  #
+  # On the second line of this hash we say that we should have a column in the
+  # final export called "role", and we should use the "user_role" method to get
+  # the data that we want for the records in that column. Because we have no
+  # "user_role" method on the export itself, we can presume that user_role is
+  # being delegated to the #user_role method on the target record for export.
+  #
+  export_mapping username: :username,
+                 role: :user_role,
+                 user_id: :user_id,
+                 page: :page,
+                 date_time: lambda { |event| event.created_at.to_formatted_s(:db) }
 
-  set_schema  username: :username,
-              role: :user_role,
-              user_id: :user_id,
-              page: :page,
-              date_time: lambda { |event| event.created_at.to_formatted_s(:db) }
-
+  # Remember that records: is the collection of mongo records that we're
+  # ultimately going to filter. If we want to get more clever about how we're
+  # defining this later we're welcome to, but for now let's just pass this
+  # in since we're performing the queries for these records before the object
+  # is constructed anyway.
+  #
   def initialize(context:)
     @context = context
+    @records = context[:mongoid][:events]
     @users = context[:active_record][:users]
   end
 
@@ -23,6 +39,13 @@ class CourseEventExport
     end
   end
 
+  # these are the methods being used to filter the 'row' values for the final
+  # export CSV. There's probably a better way to organize them, but for now
+  # let's at least keep them together and keep them commented.
+  #
+  # These correspond to the values in the export_mapping hash defined on the
+  # class at the top of this file
+  #
   def username(event)
     usernames[event.user_id] || "[user id: #{event.user_id}]"
   end
