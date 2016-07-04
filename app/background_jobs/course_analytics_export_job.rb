c711b787646b54c62425e2e38d4909ba45a00dc0
Mike Zazaian <mike@zop.io>
2016-07-04 15:29:18 +0200

    fix all course analytics exports naming inconsistencies

--- app/background_jobs/course_analytics_export_job.rb
+++ app/background_jobs/course_analytics_export_job.rb
@@ -1,4 +1,6 @@
-class CourseAnalyticsExportsJob < ResqueJob::Base
+require_relative "../performers/course_analytics_export_performer"
+
+class CourseAnalyticsExportJob < ResqueJob::Base
   @queue = :course_analytics_exports
-  @performer_class = CourseAnalyticsExportsPerformer
+  @performer_class = CourseAnalyticsExportPerformer
 end
--- spec/background_jobs/course_analytics_export_job_spec.rb
+++ spec/background_jobs/course_analytics_export_job_spec.rb
@@ -1,16 +1,16 @@
 require "resque_job"
-require "./app/background_jobs/course_analytics_exports_job"
+require "./app/background_jobs/course_analytics_export_job"
 
-describe CourseAnalyticsExportsJob do
+describe CourseAnalyticsExportJob do
   subject { described_class }
 
   it "inherits from ResqueJob::Base" do
-    expect(subject.super_class).to eq ResqueJob::Base
+    expect(subject.superclass).to eq ResqueJob::Base
   end
 
   it "uses the course_analytics_exports queue" do
   end
 
-  it "performs the job with the CourseAnalyticsExportsPerformer" do
+  it "performs the job with the CourseAnalyticsExportPerformer" do
   end
 end
