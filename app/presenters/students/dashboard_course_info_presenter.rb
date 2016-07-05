d60756faa6c4eeb4033acd3bac1f29f753b00ba3
Jonathan Gabel <hello@jonathangabel.com>
2016-07-05 14:30:57 -0400

    refactor has_info? method in presenter

--- app/presenters/students/dashboard_course_info_presenter.rb
+++ app/presenters/students/dashboard_course_info_presenter.rb
@@ -13,9 +13,21 @@ class Students::DashboardCourseInfoPresenter < Showtime::Presenter
 
   def has_info?
     if student
-      course.instructors_of_record.present? || course.office_hours.present? || course.office.present? || course.office_hours.present? || student.team_leaders(course).present? || student.team_for_course(course).present? || course.syllabus.present? || course.phone.present? || course.class_email.present? || course.twitter_handle.present? || course.twitter_hashtag.present? || course.meeting_times.present?
+      has_student_info? && has_course_info?
     else
-      course.instructors_of_record.present? || course.office_hours.present? || course.office.present? || course.office_hours.present? || course.syllabus.present? || course.phone.present? || course.class_email.present? || course.twitter_handle.present? || course.twitter_hashtag.present? || course.meeting_times.present?
+      has_course_info?
     end
+  end
+
+  private
+
+  def has_student_info?
+    student.team_leaders(course).present? || student.team_for_course(course).present?
+  end
+
+  def has_course_info?
+    [:instructors_of_record, :office_hours, :office, :office_hours, :syllabus,
+     :phone, :class_email, :twitter_handle, :twitter_hashtag, :meeting_times
+    ].any? { |attr| course[attr].present? }
   end
 end
