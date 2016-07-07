12dc8357c0466b990a2acb5d871dfa26e8071899
Jamie Wright <jamie@brilliantfantastic.com>
2016-07-07 10:54:48 -0400

    Disable external API access for only a specific type of test

--- spec/api_spec_helper.rb
+++ spec/api_spec_helper.rb
@@ -1,7 +1,7 @@
 require "webmock/rspec"
 
 RSpec.configure do |config|
-  config.around(:each) do |example|
+  config.around(:each, type: :disable_external_api) do |example|
     WebMock.disable_net_connect!(allow_localhost: true)
     example.run
     WebMock.allow_net_connect!
--- spec/lib/canvas/api_spec.rb
+++ spec/lib/canvas/api_spec.rb
@@ -1,7 +1,7 @@
 require "api_spec_helper"
 require "./lib/canvas"
 
-describe Canvas::API do
+describe Canvas::API, type: :disable_external_api do
   let(:access_token) { "BLAH" }
 
   describe "#initialize" do
