5e8b6fd4b0c8c71be7c79da8279c471f5e34441d
Mike Zazaian <mike@zop.io>
2016-07-08 15:50:25 +0200

    paring down a new CourseAnalyticsExportMailer to help keep our mailer logic tidy

---
+++ app/mailers/course_analytics_export_mailer.rb
@@ -0,0 +1,69 @@
+class CourseAnalyticsExportsMailer < ApplicationMailer
+  layout "mailers/exports_layout"
+
+  attr_accessor :professor, :course, :export
+
+  # the SecureTokenHelper brings in the #secure_downloads_url method which we
+  # need for building the secure download method on success emails
+  add_template_helper(SecureTokenHelper)
+
+  def export_started(professor, course)
+    mail_submissions_export("is being created", professor, assignment)
+  end
+
+  def submissions_export_success(professor, assignment, submissions_export,
+                                 secure_token)
+    cache_success_mailer_attrs(submissions_export, secure_token)
+    mail_submissions_export("is ready", professor, assignment)
+  end
+
+  def submissions_export_failure(professor, assignment)
+    mail_submissions_export("failed to build", professor, assignment)
+  end
+
+  private
+
+  def set_export_ivars(course, user)
+    @course = course
+    @user = user
+  end
+
+  def csv_attachment(content)
+    { mime_type: "text/csv", content: content }
+  end
+
+  def send_export_email(subject)
+    mail(to: @user.email, bcc: ADMIN_EMAIL, subject: subject) do |format|
+      format.html
+      format.text
+    end
+  end
+
+  def mail_submissions_export(message, professor, assignment)
+    cache_submission_attrs(professor, assignment)
+    mail_message_with_subject "Submissions export for #{@course.assignment_term.downcase} #{@assignment.name} #{message}"
+  end
+
+  def mail_message_with_subject(subject)
+    mail(default_attrs.merge(subject: subject)) do |format|
+      format.text
+      format.html
+    end
+  end
+
+  def default_attrs
+    { to: professor.email, bcc: ExportsMailer::ADMIN_EMAIL }
+  end
+
+  def cache_success_mailer_attrs(submissions_export, secure_token)
+    @submissions_export = submissions_export
+    @secure_token = secure_token
+  end
+
+  def cache_submission_attrs(professor, assignment)
+    @professor = professor
+    @assignment = assignment
+    @course = assignment.course
+  end
+end
+
