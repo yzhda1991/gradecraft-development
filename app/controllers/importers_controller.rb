05107233fb468310ba1563b824dfeb6138205d7e
Jamie Wright <jamie@brilliantfantastic.com>
2016-07-08 11:51:28 -0400

    Remove some duplication in the creation of the syllabus

--- app/controllers/importers_controller.rb
+++ app/controllers/importers_controller.rb
@@ -9,28 +9,29 @@ class ImportersController < ApplicationController
 
   def courses
     @provider = params[:importer_id]
-    @courses = ActiveLMS::Syllabus.new(@provider,
-                                       ENV["#{@provider.upcase}_ACCESS_TOKEN"])
-      .courses
+    @courses = syllabus.courses
   end
 
   def assignments
     @provider = params[:importer_id]
-    importer = ActiveLMS::Syllabus.new(@provider,
-                                       ENV["#{@provider.upcase}_ACCESS_TOKEN"])
-    @course = importer.course(params[:id])
-    @assignments = importer.assignments(params[:id])
+    @course = syllabus.course(params[:id])
+    @assignments = syllabus.assignments(params[:id])
     @assignment_types = current_course.assignment_types
   end
 
   def assignments_import
     @provider = params[:importer_id]
 
-    assignments = ActiveLMS::Syllabus.new(@provider,
-                                          ENV["#{@provider.upcase}_ACCESS_TOKEN"])
-      .assignments(params[:id], params[:assignment_ids])
+    assignments = syllabus.assignments(params[:id], params[:assignment_ids])
     @result = CanvasAssignmentImporter.new(assignments).import current_course,
                                                                params[:assignment_type_id]
     render :assignments_import_results
+  end
+
+  private
+
+  def syllabus
+    @syllabus ||= ActiveLMS::Syllabus.new(@provider,
+                                          ENV["#{@provider.upcase}_ACCESS_TOKEN"])
   end
 end
