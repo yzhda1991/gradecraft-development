1d3ea37d2eddab2d7492d5fe11f6b43541c5945f
Marie Hooper <marie.ashtari@gmail.com>
2016-07-12 16:28:32 -0400

    took method for empty message out of presenter and added logic back to the view

--- app/presenters/students/dashboard_course_planner_presenter.rb
+++ app/presenters/students/dashboard_course_planner_presenter.rb
@@ -54,17 +54,4 @@ class Students::DashboardCoursePlannerPresenter < Showtime::Presenter
   def due_dates?
     assignments.any?{ |assignment| assignment.due_at? }
   end
-
-  def empty_message(list_class)
-    assignment_term = course.assignment_term.downcase.pluralize
-    if list_class == "course-planner-list"
-      if due_dates?
-        "You don't have any #{assignment_term} due in the next week!"
-      else
-        "This class has flexible assignment due dates. Check your course rules to learn when to turn in certain assignment."
-      end
-    elsif list_class == "my-planner-list"
-      "You have not predicted any #{assignment_term}! Check out the grade predictor to add #{assignment_term} to this planner."
-    end
-  end
 end
