8c46d0504a691b473b6dd6d5ab8a0a37a1257a79
Jamie Wright <jamie@brilliantfantastic.com>
2016-07-12 14:35:08 -0400

    Pass the provider to dynaically create the importer

--- app/services/imports_lms_assignments/imports_lms_assignments.rb
+++ app/services/imports_lms_assignments/imports_lms_assignments.rb
@@ -4,16 +4,19 @@ module Services
   module Actions
     class ImportsLMSAssignments
       extend LightService::Action
+      extend ActiveSupport::Inflector
 
-      expects :assignments, :assignment_type_id, :course
+      expects :assignments, :assignment_type_id, :course, :provider
       promises :import_result
 
       executed do |context|
         assignments = context.assignments
         course = context.course
         assignment_type_id = context.assignment_type_id
+        provider = context.provider
 
-        context.import_result = CanvasAssignmentImporter.new(assignments)
+        klass = constantize("#{camelize(provider)}AssignmentImporter")
+        context.import_result = klass.new(assignments)
           .import course, assignment_type_id
       end
     end
--- spec/services/imports_lms_assignments/imports_lms_assignments_spec.rb
+++ spec/services/imports_lms_assignments/imports_lms_assignments_spec.rb
@@ -7,27 +7,35 @@ describe Services::Actions::ImportsLMSAssignments do
   let(:assignments) { [{ "name" => "Assignment 1" }] }
   let(:assignment_type) { create :assignment_type, course: course }
   let(:course) { create :course }
+  let(:provider) { :canvas }
 
   it "expects assignments to import" do
     expect { described_class.execute course: course,
-             assignment_type_id: assignment_type.id }.to \
+             assignment_type_id: assignment_type.id, provider: provider }.to \
       raise_error LightService::ExpectedKeysNotInContextError
   end
 
   it "expects a course to create the assignments for" do
     expect { described_class.execute assignments: assignments,
-             assignment_type_id: assignment_type.id }.to \
+             assignment_type_id: assignment_type.id, provider: provider }.to \
       raise_error LightService::ExpectedKeysNotInContextError
   end
 
   it "expects an assignment type id to create the assignments for" do
-    expect { described_class.execute assignments: assignments, course: course }.to \
+    expect { described_class.execute assignments: assignments,
+             course: course, provider: provider }.to \
+      raise_error LightService::ExpectedKeysNotInContextError
+  end
+
+  it "expects a provider to create the correct importer object" do
+    expect { described_class.execute assignments: assignments,
+             assignment_type_id: assignment_type.id, course: course }.to \
       raise_error LightService::ExpectedKeysNotInContextError
   end
 
   it "creates the assignments" do
     result = described_class.execute assignments: assignments,
-      assignment_type_id: assignment_type.id, course: course
+      assignment_type_id: assignment_type.id, course: course, provider: provider
 
     expect(result.import_result.successful.count).to eq 1
     expect(assignment.name).to eq "Assignment 1"
