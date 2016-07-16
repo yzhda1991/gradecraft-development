7f40ffae6fbbdf3841cce0806cf387a7340d3655
chcholman <caitlin.holman@gmail.com>
2016-07-16 10:21:27 -0400

    Removed bullets for list items in assignment form, added descriptor to hidden names, Rubocop -11

--- app/models/unlock_condition.rb
+++ app/models/unlock_condition.rb
@@ -49,16 +49,13 @@ class UnlockCondition < ActiveRecord::Base
   # assignment
   def count_unlocked_in_group(group)
     unlocked_count = 0
-    if group.present?
-      group.students.each do |student|
-        if self.is_complete?(student)
-          unlocked_count += 1
-        end
+    return false unless group.present?
+    group.students.each do |student|
+      if self.is_complete?(student)
+        unlocked_count += 1
       end
-      return unlocked_count
-    else
-      return false
     end
+    return unlocked_count
   end
 
   private
@@ -101,36 +98,27 @@ class UnlockCondition < ActiveRecord::Base
 
   def check_badge_condition(student)
     badge = student.earned_badge_for_badge(condition_id)
-    if badge.present?
-      if condition_value? && condition_date?
-        check_if_badge_earned_enough_times_by_date(student)
-      elsif condition_value?
-        check_if_badge_earned_enough_times(student)
-      else
-        return true
-      end
+    return false unless badge.present?
+    if condition_value? && condition_date?
+      check_if_badge_earned_enough_times_by_date(student)
+    elsif condition_value?
+      check_if_badge_earned_enough_times(student)
     else
-      return false
+      return true
     end
   end
 
   def check_if_badge_earned_enough_times(student)
     badge_count = student.earned_badges_for_badge_count(condition_id)
-    if badge_count >= condition_value
-      return true
-    else
-      return false
-    end
+    return false unless badge_count >= condition_value
+    return true
   end
 
   def check_if_badge_earned_enough_times_by_date(student)
     badge_count = student.earned_badges_for_badge_count(condition_id)
-    if badge_count >= condition_value &&
+    return false unless badge_count >= condition_value &&
       student.earned_badges.where(badge_id: condition_id).last.created_at < condition_date
-      return true
-    else
-      return false
-    end
+    return true
   end
 
   def check_assignment_condition(student)
@@ -151,15 +139,9 @@ class UnlockCondition < ActiveRecord::Base
     elsif assignment.is_individual?
       submission = student.submission_for_assignment(assignment)
     end
-    if submission.present?
-      if condition_date?
-        check_if_submitted_by_condition_date(submission)
-      else
-        return true
-      end
-    else
-      return false
-    end
+    return false unless submission.present?
+    return true unless condition_date?
+    check_if_submitted_by_condition_date(submission)
   end
 
   def check_if_submitted_by_condition_date(submission)
@@ -187,12 +169,9 @@ class UnlockCondition < ActiveRecord::Base
   end
 
   def check_if_grade_earned_meets_condition_value(grade)
-    if GradeProctor.new(grade).viewable? &&
+    return false unless GradeProctor.new(grade).viewable? &&
         grade.score >= condition_value
-      return true
-    else
-      return false
-    end
+    return true
   end
 
   def check_if_grade_earned_met_condition_date(grade)
@@ -202,30 +181,18 @@ class UnlockCondition < ActiveRecord::Base
 
   def check_feedback_read_condition(student)
     grade = student.grade_for_assignment_id(condition_id).first
-    if grade.present? && grade.feedback_read?
-      if condition_date?
-        if grade.feedback_read_at < condition_date
-          return true
-        else
-          return false
-        end
-      else
-        return true
-      end
-    else
-      return false
-    end
+    return false unless grade.present? && grade.feedback_read?
+    return true unless condition_date?
+    return false unless grade.feedback_read_at < condition_date
+    return true
   end
 
   # Checking if the number of students who have completed the condition match
   # the size of the group, returning true if so.
   def check_condition_for_each_student(group)
     unlocked_count = count_unlocked_in_group(group)
-    if unlocked_count == group.students.count
-      return true
-    else
-      return false
-    end
+    return false unless unlocked_count == group.students.count
+    return true
   end
 
 end
