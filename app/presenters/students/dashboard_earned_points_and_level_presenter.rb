0b928ec8bc73a33b9b23c9935ed11c143d353a07
chcholman <caitlin.holman@gmail.com>
2016-08-01 20:24:36 -0400

    Added level display to student dashboard

--- app/presenters/students/dashboard_earned_points_and_level_presenter.rb
+++ app/presenters/students/dashboard_earned_points_and_level_presenter.rb
@@ -12,11 +12,14 @@ class Students::DashboardEarnedPointsAndLevelPresenter < Showtime::Presenter
   end
 
   def earned_level
-    student.grade_for_course(course).name
+    student.grade_for_course(course).try(:name)
+  end
+
+  def earned_level_sentence
+    "You have achieved the #{ earned_level } level" || "You have not yet earned a level"
   end
 
   def score
     student.cached_score_for_course(course)
   end
-
 end
