e99a8db0250b5ec0ef734ccc065805cad31b021b
Jamie Wright <jamie@brilliantfantastic.com>
2016-08-02 17:04:58 -0400

    Use the canvase base url instead of client url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :canvas, ENV["CANVAS_CLIENT_ID"], ENV["CANVAS_CLIENT_SECRET"],
     setup: lambda { |env|
-      env["omniauth.strategy"].options[:client_options].site = ENV["CANVAS_CLIENT_SITE"]
+      env["omniauth.strategy"].options[:client_options].site =
+        "#{ENV["CANVAS_BASE_URL"]}/login/canvas"
   }
   provider :developer unless Rails.env.production?
   provider :lti, :oauth_credentials => { ENV["LTI_CONSUMER_KEY"] => ENV["LTI_CONSUMER_SECRET"] }
