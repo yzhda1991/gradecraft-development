63494475bba836e2937120fbcd3267916c9279c4
Jamie Wright <jamie@brilliantfantastic.com>
2016-08-02 16:26:16 -0400

    UserAuthorization to create or update a record with the oauth information

--- app/models/user_authorization.rb
+++ app/models/user_authorization.rb
@@ -1,6 +1,15 @@
 class UserAuthorization < ActiveRecord::Base
   belongs_to :user
 
+  def self.create_by_auth_hash(auth_hash, user)
+    provider = auth_hash["provider"]
+    access_token = auth_hash["credentials"]["token"]
+
+    authorization = self.find_or_initialize_by provider: provider, user_id: user.id
+    authorization.update_attributes access_token: access_token
+    authorization
+  end
+
   def self.for(user, provider)
     where(user_id: user.id, provider: provider).first
   end
--- spec/models/user_authorization_spec.rb
+++ spec/models/user_authorization_spec.rb
@@ -1,6 +1,37 @@
 require "active_record_spec_helper"
 
 describe UserAuthorization do
+  describe ".create_by_auth_hash" do
+    let(:canvas_auth_hash) do
+      {
+        provider: "canvas",
+        credentials: {
+          token: "BLAH",
+          expires_at: (Time.now + (30 * 24 * 60 * 60)).to_i,
+          expires: true
+        }
+      }.deep_stringify_keys
+    end
+    let(:user) { create :user }
+
+    it "creates an authorization for the specified user" do
+      authorization = described_class.create_by_auth_hash canvas_auth_hash, user
+
+      expect(authorization).to_not be_nil
+      expect(authorization.user_id).to eq user.id
+      expect(authorization.provider).to eq "canvas"
+      expect(authorization.access_token).to eq "BLAH"
+    end
+
+    it "updates the authorization information if one already exists" do
+      create :user_authorization, :canvas, user: user, access_token: "BLEH"
+
+      authorization = described_class.create_by_auth_hash canvas_auth_hash, user
+
+      expect(authorization.access_token).to eq "BLAH"
+    end
+  end
+
   describe ".for" do
     let(:user) { create :user }
 
