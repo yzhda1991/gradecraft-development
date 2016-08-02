24b7eda0395ddc4469d3864f3da16656e6e22f99
Jamie Wright <jamie@brilliantfantastic.com>
2016-08-02 11:33:36 -0400

    Add irregular acryonm for LMS

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,6 +10,7 @@
 # end
 #
 ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym "LMS"
   inflect.acronym "LTI"
   inflect.acronym "API"
   inflect.irregular "criterion", "criteria"
