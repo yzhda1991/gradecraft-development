a0cf3e87c0295508d2fea79933a41558f0155d39
Jamie Wright <jamie@brilliantfantastic.com>
2016-08-02 14:53:04 -0400

    Setup for canvas authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :canvas, ENV["CANVAS_CLIENT_ID"], ENV["CANVAS_CLIENT_SECRET"],
+    setup: lambda { |env|
+      env["omniauth.strategy"].options[:client_options].site = ENV["CANVAS_CLIENT_SITE"]
+  }
   provider :developer unless Rails.env.production?
   provider :lti, :oauth_credentials => { ENV["LTI_CONSUMER_KEY"] => ENV["LTI_CONSUMER_SECRET"] }
   provider :kerberos, uid_field: :username, fields: [ :username, :password ]
