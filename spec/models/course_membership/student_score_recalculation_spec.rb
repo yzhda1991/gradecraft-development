338a816a60dec03c7368b1d7bfcb30c92b9482e9
chcholman <caitlin.holman@gmail.com>
2016-08-03 08:40:39 -0400

    working on specs

--- spec/models/course_membership/student_score_recalculation_spec.rb
+++ spec/models/course_membership/student_score_recalculation_spec.rb
@@ -4,6 +4,7 @@ describe CourseMembership do
   let(:course_membership) { build(:student_course_membership, course: course, user: student) }
   let(:course) { build(:course) }
   let(:student) { build(:user) }
+  let!(:gse) { create(:grade_scheme_element, course: course, lowest_points: 8000, highest_points: 9000) }
 
   describe "#recalculate_and_update_student_score" do
     subject { course_membership.recalculate_and_update_student_score }
@@ -33,6 +34,39 @@ describe CourseMembership do
     it "adds the relevant components of the student score" do
       expect(course_membership.recalculated_student_score).to eq(1800)
     end
+  end
+
+  describe "#check_and_update_student_earned_level" do
+    subject { course_membership.check_and_update_student_earned_level }
+
+    it "updates the earned level value" do
+      expect(course_membership.earned_grade_scheme_element_id).to eq(nil)
+      allow(course_membership).to receive(:score) { 8200 }
+      subject
+      expect(course_membership.reload.earned_grade_scheme_element_id).to eq(gse.id)
+    end
+  end
+
+  describe "#earned_grade_scheme_element" do
+    let!(:low_gse) { create(:grade_scheme_element, course: course, lowest_points: 7000, highest_points: 7999) }
+    let!(:high_gse) { create(:grade_scheme_element, course: course, lowest_points: 9001, highest_points: 10000) }
+
+    it "returns the element that matches the highest points the student has earned" do
+      allow(course_membership).to receive(:score) { 8200 }
+      expect(course_membership.earned_grade_scheme_element).to eq(gse)
+    end
+
+    it "does not return an element if it is still locked" do
+
+    end
+  # #   elements_earned = []
+  # #   course.grade_scheme_elements.order_by_lowest_points.each do |gse|
+  # #     if gse.is_unlocked_for_student?(user) && gse.lowest_points < score
+  # #       elements_earned << gse
+  # #     end
+  # #   end
+  # #   return elements_earned.last
+  #
   end
 
   describe "#assignment_type_totals_for_student" do
--- spec/models/course_membership_spec.rb
+++ spec/models/course_membership_spec.rb
@@ -2,10 +2,9 @@ require "active_record_spec_helper"
 
 describe CourseMembership do
   describe ".instructors_of_record" do
-    let!(:instructor_membership) { create :staff_course_membership, instructor_of_record: true }
-    let!(:no_instructor_membership) { create :course_membership, instructor_of_record: false }
-
     it "returns all memberships that have the instructor of record turned on" do
+      let!(:instructor_membership) { create :staff_course_membership, instructor_of_record: true }
+      let!(:staff_membership) { create :staff_course_membership, instructor_of_record: false }
       expect(described_class.instructors_of_record).to eq [instructor_membership]
     end
   end
