3e1d0fbfe3603b26722f9aaa5b89670690625f42
Jamie Wright <jamie@brilliantfantastic.com>
2016-08-04 16:31:00 -0400

    Removed unnecessary curly braces

--- app/helpers/partial_helper.rb
+++ app/helpers/partial_helper.rb
@@ -1,9 +1,9 @@
 module PartialHelper
   def partial_with(partial, options={}, &block)
-    options.merge!({ partial: partial })
+    options.merge!(partial: partial)
     if block_given?
       options[:locals] = {} if options[:locals].nil?
-      options[:locals].merge!({ content: block })
+      options[:locals].merge!(content: block)
     end
     render options
   end
