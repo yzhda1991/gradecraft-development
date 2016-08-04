03c7c2cd547acd933569d73d265ee24530a649e4
Jamie Wright <jamie@brilliantfantastic.com>
2016-08-04 16:40:41 -0400

    Organized the specs a bit better and made use of context

--- spec/controllers/grades/importers_controller_spec.rb
+++ spec/controllers/grades/importers_controller_spec.rb
@@ -35,20 +35,6 @@ describe Grades::ImportersController do
         expect(response.body).to include "2 Grades Imported Successfully"
       end
 
-      it "renders any errors that have occured" do
-        post :upload, assignment_id: world.assignment.id, importer_id: :csv, file: file
-
-        expect(response.body).to include "3 Grades Not Imported"
-        expect(response.body).to include "Student not found in course"
-      end
-
-      it "adds error and redirects without a file" do
-        post :upload, assignment_id: world.assignment.id, importer_id: :csv
-
-        expect(flash[:notice]).to eq("File is missing")
-        expect(response).to redirect_to(assignment_grades_importer_path(world.assignment, :csv))
-      end
-
       it "enqueues the resque job to update the grades" do
         world.student.reload.update_attribute :email, "robert@example.com"
         second_student = create(:user, username: "jimmy")
@@ -58,6 +44,24 @@ describe Grades::ImportersController do
         post :upload, assignment_id: world.assignment.id, importer_id: :csv, file: file
 
         expect(GradeUpdaterJob).to have_queue_size_of(2)
+      end
+
+      context "with students that are no a part of the current course" do
+        it "renders any errors that have occured" do
+          post :upload, assignment_id: world.assignment.id, importer_id: :csv, file: file
+
+          expect(response.body).to include "3 Grades Not Imported"
+          expect(response.body).to include "Student not found in course"
+        end
+      end
+
+      context "without a file to import with" do
+        it "renders the missing file error" do
+          post :upload, assignment_id: world.assignment.id, importer_id: :csv
+
+          expect(flash[:notice]).to eq("File is missing")
+          expect(response).to redirect_to(assignment_grades_importer_path(world.assignment, :csv))
+        end
       end
     end
   end
