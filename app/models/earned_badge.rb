489809fc0be10f60a23e3294703da95844e75a06
chcholman <caitlin.holman@gmail.com>
2016-08-11 14:36:14 -0400

    Removed criterion_grade_id from earned badge

--- app/models/earned_badge.rb
+++ app/models/earned_badge.rb
@@ -1,7 +1,7 @@
 class EarnedBadge < ActiveRecord::Base
   attr_accessible :points, :feedback, :student, :badge, :student_id, :badge_id,
-    :submission_id, :course_id, :assignment_id, :level_id, :criterion_id,
-    :student_visible, :grade, :_destroy, :course, :grade_id, :feedback
+    :submission_id, :course_id, :assignment_id, :level_id, :student_visible,
+    :grade, :_destroy, :course, :grade_id, :feedback
 
   STATUSES= ["Predicted", "Earned"]
 
