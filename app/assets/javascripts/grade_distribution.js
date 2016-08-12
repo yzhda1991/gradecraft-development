3417a542e4121d555f9fde87513d1fb17b5841bf
Marie Hooper <marie.ashtari@gmail.com>
2016-08-12 15:20:14 -0400

    revise js for boxplot to show to student and instructor

--- app/assets/javascripts/grade_distribution.js
+++ app/assets/javascripts/grade_distribution.js
@@ -4,10 +4,13 @@
     width: '80%',
   };
 
-  if ($('#student_grade_distro').length) {
-    var data = JSON.parse($('#student_grade_distro').attr('data-scores'));
+  var $gradeDistro = $('#grade_distro');
+  if ($gradeDistro.length) {
+    var data = JSON.parse($('#grade_distro').attr('data-scores'));
     if (data !== null) {
-      sparkOpts.target = data.user_score[0];
+      if ($gradeDistro.hasClass('student-distro')) {
+        sparkOpts.target = data.user_score[0];
+      }
       sparkOpts.tooltipOffsetY = -130;
       sparkOpts.height = '35';
       sparkOpts.tooltipOffsetY = -80;
@@ -20,6 +23,6 @@
       sparkOpts.outlierFillColor = '#F4A425';
       sparkOpts.spotRadius = '10';
       sparkOpts.medianColor = '#0D9AFF';
-      $('#student_grade_distro').sparkline(data.scores, sparkOpts);
+      $('#grade_distro').sparkline(data.scores, sparkOpts);
     }
   }
