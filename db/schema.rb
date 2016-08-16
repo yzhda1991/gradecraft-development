18c318cf2c5e44146bd8d89c2ddbd19353db15eb
chcholman <caitlin.holman@gmail.com>
2016-08-16 10:15:22 -0400

    Cleaned up grades table

--- db/schema.rb
+++ db/schema.rb
@@ -404,31 +404,19 @@ ActiveRecord::Schema.define(version: 20160830154453) do
 
   create_table "grades", force: :cascade do |t|
     t.integer  "raw_points"
-    t.integer  "assignment_id"
+    t.integer  "assignment_id",                              null: false
     t.text     "feedback"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "complete"
-    t.boolean  "semis"
-    t.boolean  "finals"
-    t.string   "type"
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
     t.string   "status"
-    t.boolean  "attempted"
-    t.boolean  "substantial"
     t.integer  "final_points"
     t.integer  "submission_id"
-    t.integer  "course_id"
-    t.boolean  "shared"
-    t.integer  "student_id"
-    t.integer  "task_id"
+    t.integer  "course_id",                                  null: false
+    t.integer  "student_id",                                 null: false
     t.integer  "group_id"
-    t.string   "group_type"
     t.integer  "score"
-    t.integer  "assignment_type_id"
     t.integer  "full_points"
-    t.text     "admin_notes"
     t.integer  "graded_by_id"
-    t.integer  "team_id"
     t.integer  "predicted_score",            default: 0,     null: false
     t.boolean  "instructor_modified",        default: false
     t.string   "pass_fail_status"
@@ -446,13 +434,9 @@ ActiveRecord::Schema.define(version: 20160830154453) do
   end
 
   add_index "grades", ["assignment_id", "student_id"], name: "index_grades_on_assignment_id_and_student_id", unique: true, using: :btree
-  add_index "grades", ["assignment_id", "task_id", "submission_id"], name: "index_grades_on_assignment_id_and_task_id_and_submission_id", unique: true, using: :btree
   add_index "grades", ["assignment_id"], name: "index_grades_on_assignment_id", using: :btree
-  add_index "grades", ["assignment_type_id"], name: "index_grades_on_assignment_type_id", using: :btree
   add_index "grades", ["course_id"], name: "index_grades_on_course_id", using: :btree
-  add_index "grades", ["group_id", "group_type"], name: "index_grades_on_group_id_and_group_type", using: :btree
   add_index "grades", ["score"], name: "index_grades_on_score", using: :btree
-  add_index "grades", ["task_id"], name: "index_grades_on_task_id", using: :btree
 
   create_table "group_memberships", force: :cascade do |t|
     t.integer  "group_id"
