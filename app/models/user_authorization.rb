8766905d14487b102493fb6b6aab1cdc3e33b726
Jamie Wright <jamie@brilliantfantastic.com>
2016-08-16 17:51:41 -0400

    Create hash of attributes instead of delcaring variables with the same keys

--- app/models/user_authorization.rb
+++ app/models/user_authorization.rb
@@ -5,13 +5,12 @@ class UserAuthorization < ActiveRecord::Base
 
   def self.create_by_auth_hash(auth_hash, user)
     provider = auth_hash["provider"]
-    access_token = auth_hash["credentials"]["token"]
-    refresh_token = auth_hash["credentials"]["refresh_token"]
-    expires_at = Time.at(auth_hash["credentials"]["expires_at"])
+    attributes = { access_token:  auth_hash["credentials"]["token"],
+                   refresh_token: auth_hash["credentials"]["refresh_token"],
+                   expires_at:    Time.at(auth_hash["credentials"]["expires_at"]) }
 
     authorization = self.find_or_initialize_by provider: provider, user_id: user.id
-    authorization.update_attributes access_token: access_token, refresh_token: refresh_token,
-      expires_at: expires_at
+    authorization.update_attributes attributes
     authorization
   end
 
