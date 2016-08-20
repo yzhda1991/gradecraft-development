f7e23645360ecd8b0d4a5562a7f3ffbff736e14b
chcholman <caitlin.holman@gmail.com>
2016-08-20 14:12:10 -0400

    Updated color on highcharts display

--- app/assets/javascripts/student_dashboard.js
+++ app/assets/javascripts/student_dashboard.js
@@ -6,21 +6,43 @@ $(document).ready(function() {
       type: 'bar',
       backgroundColor: null
     },
+    
+    // $color-blue-1: #00274F
+    // $color-blue-2: #215089
+    // $color-blue-3: #2E70BE
+    // $color-blue-4: #B9D6F2
+    // $color-blue-5: #8ED0FF
+    // $blues: ( "1": #00274F, 2: #215089, 3: #2E70BE, 4: #B9D6F2, 5: #8ED0FF )
+    // 
+    // $color-purple-1: #490454
+    // $color-purple-2: #8F27A1
+    // $purples: (1: #490454, 2: #8F27A1)
+    // 
+    // $color-green-1: #9FE88D
+    // $color-green-2: #6DD677
+    // $color-green-3: #25B256
+    // $greens: (1: #9FE88D, 2: #6DD677, 3: #25B256 )
+    // 
+    // $color-red-1: #D1495B
+    // $reds: (1: #D1495B)
+    // 
+    // $color-orange-1: #EDAE49
+    // $oranges: (1: #EDAE49)
+    // 
+    // $color-yellow-1: #FFCF06
+    // $yellows: (1: #FFCF06)
     colors: [
-     '#48BDEB',
-     '#8BDEFF',
-     '#E7F8FF',
-     '#1C7200',
-     '#68A127',
-     '#7CC02F',
-     '#EFFFED',
-     '#F1592A',
-     '#F7941E',
-     '#FCB040',
-     '#FFCF06',
-     '#FFFD73',
-     '#1E267F',
-     '#1460AE'
+     '#00274F',
+     '#215089',
+     '#2E70BE',
+     '#B9D6F2',
+     '#8ED0FF',
+     '#490454',
+     '#8F27A1',
+     '#9FE88D',
+     '#6DD677',
+     '#EDAE49',
+     '#FFCF06'
   ],
 
     credits: {
