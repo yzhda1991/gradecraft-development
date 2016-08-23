7d7839170a98341717f0fd342ff81714ca60b21b
chcholman <caitlin.holman@gmail.com>
2016-08-23 14:29:02 -0400

    Remove unused methods on current scopes

--- lib/current_scopes.rb
+++ lib/current_scopes.rb
@@ -9,31 +9,6 @@ module CurrentScopes
     @__current_course ||= CourseRouter.current_course_for(current_user, session[:course_id])
   end
 
-  def current_user_is_staff?
-    return unless current_user && current_course
-    current_user.is_staff?(current_course)
-  end
-
-  def current_user_is_admin?
-    return unless current_user && current_course
-    current_user.is_admin?(current_course)
-  end
-
-  def current_user_is_gsi?
-    return unless current_user && current_course
-    current_user.is_gsi?(current_course)
-  end
-
-  def current_user_is_student?
-    return unless current_user && current_course
-    current_user.is_student?(current_course)
-  end
-
-  def current_user_is_professor?
-    return unless current_user && current_course
-    current_user.is_professor?(current_course)
-  end
-
   def current_student
     if current_user_is_staff?
       @__current_student ||= (current_course.students.find_by(id: params[:student_id]) if params[:student_id])
