703eb6fc49d400e3ca77be1caed71925109f6b7e
chcholman <caitlin.holman@gmail.com>
2016-08-25 13:47:52 -0400

    Fix ordering of gse on instructor dashboard

--- app/presenters/info/dashboard_grading_scheme_presenter.rb
+++ app/presenters/info/dashboard_grading_scheme_presenter.rb
@@ -20,12 +20,12 @@ class Info::DashboardGradingSchemePresenter < Showtime::Presenter
   end
 
   def course_elements
-    GradeSchemeElement.unscoped.for_course(course).order_by_lowest_points
+    GradeSchemeElement.for_course(course).order_by_lowest_points
   end
 
   # showing first element of grading scheme if current score does not reflect a level
   def first_element
-    GradeSchemeElement.unscoped.for_course(course).order_by_lowest_points.first
+    GradeSchemeElement.for_course(course).order_by_lowest_points.first
   end
 
   def current_element
