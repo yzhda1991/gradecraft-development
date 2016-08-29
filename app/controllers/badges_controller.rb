f1f473028986e7f1550cb68ebabaa989b18641ee
chcholman <caitlin.holman@gmail.com>
2016-08-29 15:34:02 -0400

    Fixed bug in table display of analytics downloads, allowed destroy method to be passed to controller method

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -79,8 +79,8 @@ class BadgesController < ApplicationController
       :badge_file_ids, :badge_files_attributes, :badge_file, :position,
       :visible_when_locked, :course_id, :course, :show_name_when_locked,
       :show_points_when_locked, :show_description_when_locked,
-      unlock_conditions_attributes: [:unlockable_id, :unlockable_type, :condition_id,
-        :condition_type, :condition_state, :condition_value, :condition_date],
+      unlock_conditions_attributes: [:id, :unlockable_id, :unlockable_type, :condition_id,
+        :condition_type, :condition_state, :condition_value, :condition_date, :_destroy],
       badge_files_attributes: [:id, file: []])
   end
 
