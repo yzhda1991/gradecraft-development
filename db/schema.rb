10a96bc3ea858bcd5c3677e4915202e05375b937
chcholman <caitlin.holman@gmail.com>
2016-09-15 12:49:57 -0400

    Remove character limits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915161739) do
+ActiveRecord::Schema.define(version: 20160915164717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
