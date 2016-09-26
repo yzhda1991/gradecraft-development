19d84b8ebe880a821a9004edb21947ee8256e3bd
Jamie Wright <jamie@brilliantfantastic.com>
2016-09-26 17:20:16 -0400

    Display the available courses to link

--- app/controllers/integrations_controller.rb
+++ app/controllers/integrations_controller.rb
@@ -4,6 +4,9 @@ class IntegrationsController < ApplicationController
   oauth_provider_param :integration_id
 
   before_filter :ensure_staff?
+  before_filter except: :index do |controller|
+    controller.redirect_path(courses_integration_path(params[:integration_id]))
+  end
   before_filter :require_authorization, except: :index
 
   def index
@@ -12,6 +15,22 @@ class IntegrationsController < ApplicationController
   end
 
   def create
-    redirect_to integration_courses_path(params[:integration_id])
+    redirect_to courses_integration_path(params[:integration_id])
+  end
+
+  def courses
+    @course = current_course
+    authorize! :read, @course
+
+    @provider_name = params[:integration_id]
+    @courses = syllabus(@provider_name).courses
+  end
+
+  private
+
+  def syllabus(provider)
+    @syllabus ||= ActiveLMS::Syllabus.new \
+      provider,
+      authorization(provider).access_token
   end
 end
