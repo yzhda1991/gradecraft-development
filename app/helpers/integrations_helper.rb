402591e254647f194f9147c7d8cb4e2ea517e0dc
Jamie Wright <jamie@brilliantfantastic.com>
2016-09-27 14:12:41 -0400

    Use partials for rendering the integrations

---
+++ app/helpers/integrations_helper.rb
@@ -0,0 +1,27 @@
+module IntegrationsHelper
+  def integration_card_for(provider, course)
+    if course.linked?(provider)
+      linked_integration_card_for(course.linked_for(provider))
+    else
+      unlinked_integration_card_for provider
+    end
+  end
+
+  def linked_integration_card_for(linked_course)
+    render partial: "integrations/courses/linked_card",
+      locals: { linked_course: syllabus_course(linked_course),
+                provider: linked_course.provider }
+  end
+
+  def unlinked_integration_card_for(provider)
+    render partial: "integrations/courses/unlinked_#{provider}_card"
+  end
+
+  private
+
+  def syllabus_course(linked_course)
+    authorization = UserAuthorization.for(current_user, linked_course.provider)
+    syllabus = ActiveLMS::Syllabus.new linked_course.provider, authorization.access_token
+    syllabus.course(linked_course.provider_resource_id)
+  end
+end
