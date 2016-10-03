f0f42b40ad2d1611297b520d5bb2c9c97f6dc9e4
Jamie Wright <jamie@brilliantfantastic.com>
2016-10-03 17:52:05 -0400

    Add the canvas student importer to the service

--- app/services/imports_lms_grades/imports_lms_users.rb
+++ app/services/imports_lms_grades/imports_lms_users.rb
@@ -1,9 +1,21 @@
+require_relative "../../importers/student_importers"
+
 module Services
   module Actions
     class ImportsLMSUsers
       extend LightService::Action
+      extend ActiveSupport::Inflector
+
+      expects :course, :provider, :users
+      promises :import_result
 
       executed do |context|
+        course = context.course
+        users = context.users
+        provider = context.provider
+
+        klass = constantize("#{camelize(provider)}StudentImporter")
+        context.import_result = klass.new(users).import course
       end
     end
   end
---
+++ spec/services/imports_lms_grades/imports_lms_users_spec.rb
@@ -0,0 +1,34 @@
+require "rails_spec_helper"
+require "./app/services/imports_lms_grades/imports_lms_users"
+
+describe Services::Actions::ImportsLMSUsers do
+  let(:course) { create :course }
+  let(:provider) { :canvas }
+  let(:student) { User.unscoped.last }
+  let(:users) { [{ "id" => "USER_1",
+                   "primary_email" => "jimmy@example.com",
+                   "name" => "Jimmy Page" }] }
+
+  it "expects users to import" do
+    expect { described_class.execute course: course, provider: provider }.to \
+      raise_error LightService::ExpectedKeysNotInContextError
+  end
+
+  it "expects a course to assign to the users" do
+    expect { described_class.execute provider: provider, users: users }.to \
+      raise_error LightService::ExpectedKeysNotInContextError
+  end
+
+  it "expects a provider to create the correct importer object" do
+    expect { described_class.execute course: course, users: users }.to \
+      raise_error LightService::ExpectedKeysNotInContextError
+  end
+
+  it "creates the students" do
+    result = described_class.execute course: course,
+      provider: provider, users: users
+
+    expect(result.import_result.successful.count).to eq 1
+    expect(result.import_result.successful.first).to eq student
+  end
+end
