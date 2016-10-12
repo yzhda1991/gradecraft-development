e62613c498848da9429173c0acbff63c82960523
Jamie Wright <jamie@brilliantfantastic.com>
2016-10-12 10:42:19 -0400

    Add initializer so migrations are added to the umbrella application

--- components/gradebook/lib/gradebook/engine.rb
+++ components/gradebook/lib/gradebook/engine.rb
@@ -1,5 +1,13 @@
 module Gradebook
   class Engine < ::Rails::Engine
     isolate_namespace Gradebook
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s + File::SEPARATOR
+        config.paths["db/migrate"].expanded.each do |path|
+          app.config.paths["db/migrate"] << path
+        end
+      end
+    end
   end
 end
