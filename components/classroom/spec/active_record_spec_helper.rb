26d6942b57a8ae20da0e60d178fcb8a24359f6a8
Jamie Wright <jamie@brilliantfantastic.com>
2016-10-12 13:03:00 -0400

    Spec helper to load the database definition for models

---
+++ components/classroom/spec/active_record_spec_helper.rb
@@ -0,0 +1,25 @@
+ENV["RAILS_ENV"] ||= "test"
+require "spec_helper"
+require "active_record"
+require "factory_girl"
+require "faker"
+require "yaml"
+
+Dir["./app/models/concerns/*.rb"].each { |f| require f }
+Dir["./app/models/*.rb"].each { |f| require f }
+
+FactoryGirl.factories.clear
+FactoryGirl.find_definitions
+
+connection_info = YAML.load_file("../../config/database.yml")["test"]
+ActiveRecord::Base.establish_connection(connection_info)
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  config.around do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
+  end
+end
