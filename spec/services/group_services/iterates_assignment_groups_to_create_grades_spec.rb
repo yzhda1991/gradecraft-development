339009c099db6c4cdd116e90d2423a294a9adc4e
Jon Yu <jonathoy@umich.edu>
2016-10-13 12:22:26 -0400

    Fix inconsistent spacing

--- spec/services/group_services/iterates_assignment_groups_to_create_grades_spec.rb
+++ spec/services/group_services/iterates_assignment_groups_to_create_grades_spec.rb
@@ -4,39 +4,39 @@ require "./app/services/group_services/iterates_assignment_groups_to_create_grad
 describe Services::Actions::IteratesAssignmentGroupsToCreateGrades do
   let(:assignment) { create(:assignment) }
   let(:professor) { create(:user) }
+  let(:context) { { assignment_id: assignment.id, grades_by_group_params: grades_by_group_params, graded_by_id: professor.id } }
   let(:grades_by_group_params) { { grades_by_group: {
     "0" => { "instructor_modified" => "true", "raw_points" => "10", "status" => "graded" },
     "1" => { "instructor_modified" => "true", "raw_points" => "20", "status" => "graded" } } }
   }
-  let(:context) { { assignment_id: assignment.id, grades_by_group_params: grades_by_group_params, graded_by_id: professor.id } }
 
-    it "expects an grades_by_group_params" do
-      expect { described_class.execute assignment_id: assignment.id, graded_by_id: professor.id  }.to \
-        raise_error LightService::ExpectedKeysNotInContextError
-    end
+  it "expects an grades_by_group_params" do
+    expect { described_class.execute assignment_id: assignment.id, graded_by_id: professor.id  }.to \
+      raise_error LightService::ExpectedKeysNotInContextError
+  end
 
-    it "expects an assignment_id" do
-      expect { described_class.execute grades_by_group_params: grades_by_group_params, graded_by_id: professor.id }.to \
-        raise_error LightService::ExpectedKeysNotInContextError
-    end
+  it "expects an assignment_id" do
+    expect { described_class.execute grades_by_group_params: grades_by_group_params, graded_by_id: professor.id }.to \
+      raise_error LightService::ExpectedKeysNotInContextError
+  end
 
-    it "expects a graded_by_id" do
-      expect { described_class.execute grades_by_group_params: grades_by_group_params, assignment_id: assignment.id }.to \
-        raise_error LightService::ExpectedKeysNotInContextError
-    end
+  it "expects a graded_by_id" do
+    expect { described_class.execute grades_by_group_params: grades_by_group_params, assignment_id: assignment.id }.to \
+      raise_error LightService::ExpectedKeysNotInContextError
+  end
 
-    it "promises the successful grades" do
-      result = described_class.execute context
-      expect(result).to have_key :successful
-    end
+  it "promises the successful grades" do
+    result = described_class.execute context
+    expect(result).to have_key :successful
+  end
 
-    it "promises the unsuccessful grades" do
-      result = described_class.execute context
-      expect(result).to have_key :unsuccessful
-    end
+  it "promises the unsuccessful grades" do
+    result = described_class.execute context
+    expect(result).to have_key :unsuccessful
+  end
 
-    it "iterates the assignment groups" do
-      expect(Services::CreatesGroupGrades).to receive(:create).exactly(grades_by_group_params[:grades_by_group].length).times.and_call_original
-      described_class.execute context
-    end
+  it "iterates the assignment groups" do
+    expect(Services::CreatesGroupGrades).to receive(:create).exactly(grades_by_group_params[:grades_by_group].length).times.and_call_original
+    described_class.execute context
+  end
 end
