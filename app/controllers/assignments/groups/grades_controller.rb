ef75c56866d0e1bf07e722adb5ee5e9ddd6864ac
Jon Yu <jonathoy@umich.edu>
2016-10-20 10:04:15 -0400

    Make method name consistent with other instance

--- app/controllers/assignments/groups/grades_controller.rb
+++ app/controllers/assignments/groups/grades_controller.rb
@@ -14,7 +14,7 @@ class Assignments::Groups::GradesController < ApplicationController
 
   # PUT /assignments/:assignment_id/groups/grades/mass_update
   def mass_update
-    filter_params_with_raw_points! :grades_by_group
+    filter_params_with_no_grades! :grades_by_group
     result = Services::CreatesManyGroupGrades.create @assignment.id, current_user.id, assignment_group_grades_params
 
     if result.success?
@@ -38,7 +38,7 @@ def find_assignment
   @assignment = current_course.assignments.find(params[:assignment_id])
 end
 
-def filter_params_with_raw_points!(attribute_name)
+def filter_params_with_no_grades!(attribute_name)
   params[:assignment][attribute_name] = params[:assignment][attribute_name].delete_if do |key, value|
     value[:raw_points].nil? || value[:raw_points].empty?
   end
