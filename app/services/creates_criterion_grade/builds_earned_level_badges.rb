d2c2d75bfd1708035ec254f725237f9399d089ca
Jonathan Gabel <hello@jonathangabel.com>
2016-10-21 14:38:15 -0400

    refactor build earned badge service

--- app/services/creates_criterion_grade/builds_earned_level_badges.rb
+++ app/services/creates_criterion_grade/builds_earned_level_badges.rb
@@ -8,52 +8,30 @@ module Services
     class BuildsEarnedLevelBadges
       extend LightService::Action
 
-      expects :raw_params, :student, :assignment, :student_visible_status
+      expects :student
+      expects :assignment
+      expects :criterion_grades
+      expects :grade
+
       promises :earned_level_badges
 
-      # Returns an empty array if no level_badges in raw_params
-      # expects: "level_badges" => [{ "badge_id"=>1, "level_id"=>1 },..]
       executed do |context|
-        if context[:raw_params]["level_badges"].nil?
-          context[:earned_level_badges] = []
-        else
-          context[:earned_level_badges] = new_earned_level_badges(
-            context[:assignment],
-            context[:student],
-            !context[:student_visible_status].nil?,
-            context[:raw_params]["level_badges"]
-          )
-        end
-      end
-
-      class << self
-        private
-
-        def new_earned_level_badges(assignment, student, visible_status, level_badges_params)
-          destroy_exisiting_earned_badges(student.id, assignment.id)
-          level_badges_params.collect do |params|
-            level_badge = LevelBadge.where(badge_id: params["badge_id"], level_id: params["level_id"]).first
-            EarnedBadge.new(course_id: assignment.course.id, assignment_id: assignment.id,
-                            student_id: student.id, student_visible: visible_status,
-                            badge_id: level_badge.badge.id, level_id: level_badge.level.id,
-                            points: level_badge.badge.full_points)
+        context[:earned_level_badges] = []
+        level_ids = context[:criterion_grades].map { |cg| cg.level_id }
+        level_ids.each do |id|
+          level = Level.find(id)
+          level.level_badges.each do |level_badge|
+            context[:earned_level_badges] << EarnedBadge.find_or_create_by(
+              student_id: context[:student].id,
+              badge_id: level_badge.badge.id,
+              level_id: level.id
+            ).update(
+              assignment_id: context[:assignment].id,
+              course_id: context[:assignment].course.id,
+              points: level_badge.badge.full_points,
+              grade_id: context[:grade].id
+            )
           end
-        end
-
-        # I am adding this back in for now to handle all rubric update scenarios:
-        #   1. If this is a rubric grading resubmission, we don't want to re-award a badge
-        #   2. When a badge cannot be earned multiple times, duplicates will also cause an error on save
-        #   3. More than one instance of a badge can be associated with a level, all instances are
-        #      passed in the params.
-        #
-        # This should be handled better because:
-        #   1. Level badges to award should not be accepted blindly from and API call, they should be determined through
-        #      backend logic and current earnable badges in the database
-        #   2. Currently there is a (race?) condition that causes badges to sometimes not load in the front end, in which
-        #      case this will also mean badges that should be awarded will be lost.
-        #   3. This is terrible for performace reasons, and won't allow us to track history through existing models.
-        def destroy_exisiting_earned_badges(student_id, assignment_id)
-          EarnedBadge.where(student_id: student_id, assignment_id: assignment_id).destroy_all
         end
       end
     end
--- spec/services/creates_criterion_grade/builds_earned_level_badges_spec.rb
+++ spec/services/creates_criterion_grade/builds_earned_level_badges_spec.rb
@@ -3,38 +3,37 @@ require "active_record_spec_helper"
 require "./app/services/creates_criterion_grade/builds_earned_level_badges"
 
 describe Services::Actions::BuildsEarnedLevelBadges do
-  let(:world) { World.create.with(:course, :student, :assignment, :rubric, :criterion, :criterion_grade, :badge) }
-  let(:raw_params) { RubricGradePUT.new(world).params }
+  let(:world) { World.create.with(:course, :student, :assignment, :rubric, :criterion, :criterion_grade, :grade, :badge) }
+  let!(:level_badge) { create :level_badge, level: world.criterion_grade.level, badge: world.badge }
+
   let(:context) do
-    { raw_params: raw_params,
-      student_visible_status: true,
+    {
       student: world.student,
-      assignment: world.assignment
+      assignment: world.assignment,
+      criterion_grades: world.criterion_grades,
+      grade: world.grade
     }
   end
-  let(:badge_id) { context[:raw_params]["level_badges"][0]["badge_id"] }
-  let(:level_id) { context[:raw_params]["level_badges"][0]["level_id"] }
-
-  it "expects attributes to assign to criterion grades" do
-    context.delete(:raw_params)
-    expect { described_class.execute context }.to \
-      raise_error LightService::ExpectedKeysNotInContextError
-  end
-
-  it "expect student to be added to the context" do
+  it "expects attributes to assign to student" do
     context.delete(:student)
     expect { described_class.execute context }.to \
       raise_error LightService::ExpectedKeysNotInContextError
   end
 
-  it "expect assignment to be added to the context" do
+  it "expects attributes to assign to assignment" do
     context.delete(:assignment)
     expect { described_class.execute context }.to \
       raise_error LightService::ExpectedKeysNotInContextError
   end
 
-  it "expect student visible status to be added to the context" do
-    context.delete(:student_visible_status)
+  it "expects attributes to assign to criterion grades" do
+    context.delete(:criterion_grades)
+    expect { described_class.execute context }.to \
+      raise_error LightService::ExpectedKeysNotInContextError
+  end
+
+  it "expects attributes to assign to grade" do
+    context.delete(:grade)
     expect { described_class.execute context }.to \
       raise_error LightService::ExpectedKeysNotInContextError
   end
@@ -42,23 +41,10 @@ describe Services::Actions::BuildsEarnedLevelBadges do
   it "promises the built earned level badges" do
     result = described_class.execute context
     expect(result).to have_key :earned_level_badges
-    expect(result[:earned_level_badges].first.student_visible).to be_truthy
   end
 
-  it "builds an earned badge for each record in the params" do
+  it "assigns level badges to the student for earned levels" do
     result = described_class.execute context
-    expect(result[:earned_level_badges].length).to \
-      eq(raw_params["level_badges"].length)
-  end
-
-  it "transfers the student visible state to the EarnedBadge" do
-    result = described_class.execute context
-  end
-
-  # See note above #destroy_exisiting_earned_badges
-  # This should not be the expected behavior
-  it "clears out old badges" do
-    EarnedBadge.create(badge_id: badge_id, student_id: world.student.id, assignment_id: world.assignment.id)
-    expect { described_class.execute context }.to change { EarnedBadge.count }.by(-1)
+    expect(world.student.earned_badges.count).to eq(1)
   end
 end
