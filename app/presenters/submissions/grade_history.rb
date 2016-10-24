539e17584108096a4b7969363a04f9bd648953de
Jamie Wright <jamie@brilliantfantastic.com>
2016-10-24 11:23:21 -0400

    Remove the previous changeset explanation if the previous change was not viewable by the student

--- app/presenters/submissions/grade_history.rb
+++ app/presenters/submissions/grade_history.rb
@@ -25,18 +25,43 @@ module Submissions::GradeHistory
           history_item.changeset["event"] = "upload"
         end
       end
+      .transform do |history_item|
+        # Check to see if this change is for a grade and it's an update
+        if history_item.changeset["object"] == "Grade" &&
+            history_item.version.event == "update" &&
+            only_student_visible_grades
+
+          version = history_item.version.reify
+
+          # Check to see if the current version is viewable. If it is, check the previous version based on the attributes
+          # we care about (feedback and raw score). If that version is not viewable
+          if GradeProctor.new(version).viewable?
+            ["feedback", "raw_points"].each do |attribute|
+              previous_changeset = history_item.version.changeset[attribute]
+              if previous_changeset.present?
+                previous_version = version.versions.select { |v| v.changeset[attribute].present? && v.changeset[attribute][1] == previous_changeset[0] }.last
+                if previous_version.present?
+                  history_item.changeset[attribute][0] = "" if !GradeProctor.new(previous_version.reify).viewable?
+                end
+              end
+            end
+          end
+        end
+      end
       .rename("SubmissionFile" => "Attachment")
       .include do |history_item, history|
         viewable = true
 
-        if history_item.changeset["object"] == "Grade" && only_student_visible_grades
+        if history_item.changeset["object"] == "Grade" &&
+            history_item.version.event == "update" &&
+            only_student_visible_grades
+
           version = history_item.version.reify
           viewable = GradeProctor.new(version).viewable?
 
-          # Make the change viewable if the grade was updated first but then it was
-          # released. This displays the changeset where the grade was updated
-
-          if !viewable && history_item.version.event == "update"
+          # Make the change viewable if the grade was updated first but then it
+          # was released. This displays the changeset where the grade was updated
+          if !viewable
             last_raw_points_change = last_change?(history, history_item, "Grade",
                                                   "raw_points")
 
