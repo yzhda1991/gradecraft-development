2e3761880e7dae39281aa9ef25eac83d5dae6057
Jonathan Gabel <hello@jonathangabel.com>
2016-11-02 10:54:59 -0400

    rubocop and spec fix

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -25,7 +25,7 @@ describe UsersHelper do
     it "handles the summing of earned badges, including old badges cached with nil points" do
       course = double(:course, assignment_types: [], badge_term: "Badgeinskies", total_points: 0)
 
-      earned_badges = double(:earned_badges, inject: 1000)
+      earned_badges = double(:earned_badges, sum: 1000)
       user = double(:user, earned_badges: earned_badges)
       expect(helper.total_scores_for_chart(user,course)).to eq({scores: [{ data: [1000], name: "Badgeinskies" }], course_total: 1000 })
     end
