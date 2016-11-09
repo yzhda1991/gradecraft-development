cade1044bc5a7fcbf1a40303c4b84a097e8394a4
Marie Hooper <marie.ashtari@gmail.com>
2016-11-09 09:55:51 -0500

    added slider dots, made modal slide size consistent

--- app/assets/javascripts/student_onboarding.js
+++ app/assets/javascripts/student_onboarding.js
@@ -3,5 +3,6 @@ $('.student-onboarding-slides').slick({
   prevArrow: '<a class="fa fa-chevron-left previous slider-direction-button"></a>',
   nextArrow: '<a class="fa fa-chevron-right next slider-direction-button"></a>',
   adaptiveHeight: true,
+  dots: true,
   infinite: false
 });
