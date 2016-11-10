c8eaf851b83759363da112d704331dd9b000c4d5
Mike Zazaian <mike@zop.io>
2016-11-10 15:48:31 +0100

    add group submissions scope

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -41,6 +41,8 @@ class Submission < ActiveRecord::Base
   scope :for_course, ->(course) { where(course_id: course.id) }
   scope :for_student, ->(student) { where(student_id: student.id) }
 
+  scope :with_group, -> { where "group_id is not null" }
+
   before_validation :cache_associations
 
   validates :link, format: URI::regexp(%w(http https)), allow_blank: true
