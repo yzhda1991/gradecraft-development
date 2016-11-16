0b42f8b083da26abcbefe4f2a45e5dfd62287a61
Marie Hooper <marie.ashtari@gmail.com>
2016-11-16 07:41:36 -0500

    update js for modal so it only runs when modal is there

--- app/assets/javascripts/student_onboarding.js
+++ app/assets/javascripts/student_onboarding.js
@@ -17,87 +17,88 @@ $('.student-onboarding-slides').slick({
 
 //modal for student onboarding from Scott O'Hara (https://www.smashingmagazine.com/2014/09/making-modal-windows-better-for-everyone/)
 // helper function to place modal window as the first child
-// of the #page node
-var m = document.getElementById('modal-window'),
-    p = document.getElementById('dashboard-timeline');
+// of the #dashboard-timeline node
+var m = document.getElementById('modal-window');
+var p = document.querySelector('.student .offscreen-sidebar');
 
 function swap () {
   p.parentNode.insertBefore(m, p);
 }
 
-swap();
+if (m && p) {
+  swap();
 
+  // modal window
+  (function() {
 
-// modal window
-(function() {
+    'use strict';
 
-  'use strict';
+    // list out the vars
+    var mOverlay = getId('modal-window'),
+        mOpen = getId('modal-open'),
+        mClose = getId('modal-close'),
+        modal = getId('modal-holder'),
+        allNodes = document.querySelectorAll("*"),
+        modalOpen = false,
+        lastFocus,
+        i;
 
-  // list out the vars
-  var mOverlay = getId('modal-window'),
-      mOpen = getId('modal-open'),
-      mClose = getId('modal-close'),
-      modal = getId('modal-holder'),
-      allNodes = document.querySelectorAll("*"),
-      modalOpen = false,
-      lastFocus,
-      i;
-
-  // Let's cut down on what we need to type to get an ID
-  function getId ( id ) {
-    return document.getElementById(id);
-  }
-
-  // Let's open the modal
-  function modalShow () {
-    lastFocus = document.activeElement;
-    mOverlay.setAttribute('aria-hidden', 'false');
-    modalOpen = true;
-    modal.setAttribute('tabindex', '0');
-    modal.focus();
-  }
-
-  // binds to both the button click and the escape key to close the modal window
-  // but only if modalOpen is set to true
-  function modalClose ( event ) {
-    if (modalOpen && ( !event.keyCode || event.keyCode === 27 ) ) {
-      mOverlay.setAttribute('aria-hidden', 'true');
-      modal.setAttribute('tabindex', '-1');
-      modalOpen = false;
-      lastFocus.focus();
+    // Let's cut down on what we need to type to get an ID
+    function getId ( id ) {
+      return document.getElementById(id);
     }
-  }
 
-  // Restrict focus to the modal window when it's open.
-  // Tabbing will just loop through the whole modal.
-  // Shift + Tab will allow backup to the top of the modal,
-  // and then stop.
-  function focusRestrict ( event ) {
-    if ( modalOpen && !modal.contains( event.target ) ) {
-      event.stopPropagation();
+    // Let's open the modal
+    function modalShow () {
+      lastFocus = document.activeElement;
+      mOverlay.setAttribute('aria-hidden', 'false');
+      modalOpen = true;
+      modal.setAttribute('tabindex', '0');
       modal.focus();
     }
-  }
 
-  // Close modal window by clicking on the overlay
-  mOverlay.addEventListener('click', function( e ) {
-    if (e.target == modal.parentNode) {
-       modalClose( e );
-     }
-  }, false);
+    // binds to both the button click and the escape key to close the modal window
+    // but only if modalOpen is set to true
+    function modalClose ( event ) {
+      if (modalOpen && ( !event.keyCode || event.keyCode === 27 ) ) {
+        mOverlay.setAttribute('aria-hidden', 'true');
+        modal.setAttribute('tabindex', '-1');
+        modalOpen = false;
+        lastFocus.focus();
+      }
+    }
 
-  // open modal by btn click/hit
-  mOpen.addEventListener('click', modalShow);
+    // Restrict focus to the modal window when it's open.
+    // Tabbing will just loop through the whole modal.
+    // Shift + Tab will allow backup to the top of the modal,
+    // and then stop.
+    function focusRestrict ( event ) {
+      if ( modalOpen && !modal.contains( event.target ) ) {
+        event.stopPropagation();
+        modal.focus();
+      }
+    }
 
-  // close modal by btn click/hit
-  mClose.addEventListener('click', modalClose);
+    // Close modal window by clicking on the overlay
+    mOverlay.addEventListener('click', function( e ) {
+      if (e.target == modal.parentNode) {
+         modalClose( e );
+       }
+    }, false);
 
-  // close modal by keydown, but only if modal is open
-  document.addEventListener('keydown', modalClose);
+    // open modal by btn click/hit
+    mOpen.addEventListener('click', modalShow);
 
-  // restrict tab focus on elements only inside modal window
-  for (i = 0; i < allNodes.length; i++) {
-    allNodes.item(i).addEventListener('focus', focusRestrict);
-  }
+    // close modal by btn click/hit
+    mClose.addEventListener('click', modalClose);
 
-})();
+    // close modal by keydown, but only if modal is open
+    document.addEventListener('keydown', modalClose);
+
+    // restrict tab focus on elements only inside modal window
+    for (i = 0; i < allNodes.length; i++) {
+      allNodes.item(i).addEventListener('focus', focusRestrict);
+    }
+
+  })();
+}
