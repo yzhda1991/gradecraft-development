df891a5721e60de47b0cf5e6adb69d039d18375a
Mike Zazaian <mike@zop.io>
2016-11-18 17:37:20 +0100

    condense submitter_id changes into single migration

--- db/migrate/20161013144318_add_group_id_to_submissions_export.rb
+++
@@ -1,5 +0,0 @@
-class AddGroupIdToSubmissionsExport < ActiveRecord::Migration
-  def change
-    add_column :submissions_exports, :group_id, :integer
-  end
-end
--- db/migrate/20161026131150_rename_submissions_export_student_ids.rb
+++ db/migrate/20161026131150_rename_submissions_export_student_ids.rb
@@ -1,11 +1,5 @@
 class RenameSubmissionsExportStudentIds < ActiveRecord::Migration
-  def up
+  def change
     rename_column :submissions_exports, :student_ids, :submitter_ids
-    remove_column :submissions_exports, :group_ids
-  end
-
-  def down
-    rename_column :submissions_exports, :submitter_ids, :student_ids
-    add_column :submissions_exports, :group_ids, :integer, null: false, array: true, default: []
   end
 end
