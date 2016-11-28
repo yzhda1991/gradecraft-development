c7b60f9e6c0f486cd00a00c25a1ea3f0a1b2e487
Marie Hooper <marie.ashtari@gmail.com>
2016-11-28 12:04:18 -0500

    hide auditors on page load due to default leaderboard selection and wrap filter function in conditional to not run if all is selected

--- app/assets/javascripts/students_table_filter.js
+++ app/assets/javascripts/students_table_filter.js
@@ -1,4 +1,7 @@
 //Filter content on the student index page table for instructors
+//start by hiding auditors in table since default button selected is leaderboard
+$('.student-index-table tbody tr').has('.auditor').hide();
+
 $('.button-table-action').click(function() {
   var $tableRows = $('.student-index-table tbody tr');
   var btnId = $(this).attr('id');
@@ -6,21 +9,26 @@ $('.button-table-action').click(function() {
 
   $(this).addClass("selected").siblings().removeClass("selected");
   $tableRows.show();
-  $tableRows.filter(function() {
-    var rank = $(this).find('td:eq(1)').text();
+  // only run filter function if buttons are any other than All students
+  if (btnId != 'btn-all-students') {
+    $tableRows.filter(function() {
+      var rank = $(this).find('td:eq(1)').text();
 
-    switch(btnId) {
-      case 'btn-leaderboard':
-        return !rank.length;
-      case 'btn-top10':
-        return (rank.length) ? parseInt(rank) > 10 : true;
-      case 'btn-bottom10':
-        return (rank.length) ? parseInt(rank) <= parseInt(lowestRank) - 10 : true;
-      case 'btn-flagged-students':
-        var flagged = $(this).find('td:eq(0) i').hasClass('flagged');
-        return !flagged;
-      case 'btn-auditors':
-        return rank.length;
-    }
-  }).hide();
+      switch(btnId) {
+        case 'btn-leaderboard':
+        // auditors have no rank, so hide all students that do not have a rank
+          return !rank.length;
+        case 'btn-top10':
+          return (rank.length) ? parseInt(rank) > 10 : true;
+        case 'btn-bottom10':
+          return (rank.length) ? parseInt(rank) <= parseInt(lowestRank) - 10 : true;
+        case 'btn-flagged-students':
+          var flagged = $(this).find('td:eq(0) i').hasClass('flagged');
+          return !flagged;
+        case 'btn-auditors':
+          // auditors have no rank, so hide all students that have a rank in the class
+          return rank.length;
+      }
+    }).hide();
+  }
 });
