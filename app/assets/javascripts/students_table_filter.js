d48eb73459c99d16425e045b4d4cc6da0791f922
Marie Hooper <marie.ashtari@gmail.com>
2016-11-28 11:11:52 -0500

    swap if/else with switch block and reduce repetitive rank assigning

--- app/assets/javascripts/students_table_filter.js
+++ app/assets/javascripts/students_table_filter.js
@@ -2,34 +2,25 @@
 $('.button-table-action').click(function() {
   var $tableRows = $('.student-index-table tbody tr');
   var btnId = $(this).attr('id');
-  var lowestRank = $tableRows.last().find('td:eq(1)').text();
+  var lowestRank = $tableRows.has('.graded').last().find('td:eq(1)').text();
 
   $(this).addClass("selected").siblings().removeClass("selected");
   $tableRows.show();
   $tableRows.filter(function() {
-    var rank;
+    var rank = $(this).find('td:eq(1)').text();
 
-    if (btnId === 'btn-top10') {
-      rank = $(this).find('td:eq(1)').text();
-      if (rank.length) {
-        return parseInt(rank) > 10;
-      }
-      return true;
-    } else if (btnId === 'btn-bottom10') {
-      rank = $(this).find('td:eq(1)').text();
-      if (rank.length) {
-        return parseInt(rank) <= parseInt(lowestRank) - 10;
-      }
-      return true;
-    } else if (btnId === 'btn-flagged-students') {
-      var flagged = $(this).find('td:eq(0) i').hasClass('flagged');
-      return !flagged;
-    } else if (btnId === 'btn-auditors') {
-      var auditor = $(this).find('td:eq(0) span').hasClass('auditor');
-      return !auditor;
-    } else if (btnId === 'btn-leaderboard') {
-      var graded = $(this).find('td:eq(0) span').hasClass('graded');
-      return !graded;
+    switch(btnId) {
+      case 'btn-leaderboard':
+        return !rank.length;
+      case 'btn-top10':
+        return (rank.length) ? parseInt(rank) > 10 : true;
+      case 'btn-bottom10':
+        return (rank.length) ? parseInt(rank) <= parseInt(lowestRank) - 10 : true;
+      case 'btn-flagged-students':
+        var flagged = $(this).find('td:eq(0) i').hasClass('flagged');
+        return !flagged;
+      case 'btn-auditors':
+        return rank.length;
     }
   }).hide();
 });
