d393a971fcd9acc1130cfa471b7f79db2aa60cae
Jamie Wright <jamie@brilliantfantastic.com>
2016-12-07 16:18:00 -0500

    Include the assignment with the Gradebook

--- app/models/gradebook.rb
+++ app/models/gradebook.rb
@@ -1,15 +1,16 @@
 class Gradebook
-  attr_reader :students
+  attr_reader :assignment, :students
 
-  def initialize(*students)
+  def initialize(assignment, *students)
+    @assignment = assignment
     @students = [students].flatten
   end
 
   def grade(student)
-    grades.find { |g| g.student_id == student.id }
+    grades.find { |g| g.assignment_id = assignment.id && g.student_id == student.id }
   end
 
   def grades
-    @grades ||= Grade.where(student_id: students.map(&:id))
+    @grades ||= Grade.where(assignment_id: assignment.id, student_id: students.map(&:id))
   end
 end
--- spec/models/gradebook_spec.rb
+++ spec/models/gradebook_spec.rb
@@ -2,30 +2,38 @@ require "active_record_spec_helper"
 require "db_query_matchers"
 
 describe Gradebook do
+  let(:assignment) { create :assignment }
+
   describe "#initialize" do
     let!(:student1) { create :user }
     let!(:student2) { create :user }
 
+    it "initializes with an assignment" do
+      subject = described_class.new assignment
+
+      expect(subject.assignment).to eq assignment
+    end
+
     it "initializes with a student array" do
-      subject = described_class.new [student1, student2]
+      subject = described_class.new assignment, [student1, student2]
 
       expect(subject.students).to eq [student1, student2]
     end
 
     it "initializes with a single student" do
-      subject = described_class.new student1
+      subject = described_class.new assignment, student1
 
       expect(subject.students).to eq [student1]
     end
 
     it "initializes with a student list" do
-      subject = described_class.new student1, student2
+      subject = described_class.new assignment, student1, student2
 
       expect(subject.students).to eq [student1, student2]
     end
 
     it "initializes with a query" do
-      subject = described_class.new User.where(id: student1.id)
+      subject = described_class.new assignment, User.where(id: student1.id)
 
       expect(subject.students).to eq [student1]
     end
@@ -34,24 +42,36 @@ describe Gradebook do
   describe "#grades" do
     let(:student1) { create :user }
     let(:student2) { create :user }
-    subject { described_class.new(student1, student2) }
+    subject { described_class.new(assignment, student1, student2) }
 
-    it "returns all the grades for the students" do
-      grade = create :grade, student: student2
+    it "returns all the grades for the assignment and students" do
+      grade = create :grade, assignment: assignment, student: student2
 
       expect(subject.grades).to eq [grade]
+    end
+
+    it "only includes grades for the specified assignment" do
+      grade = create :grade, student: student2
+
+      expect(subject.grades).to be_empty
     end
   end
 
   describe "#grade" do
     let(:student1) { create :user }
     let(:student2) { create :user }
-    subject { described_class.new(student1, student2) }
+    subject { described_class.new(assignment, student1, student2) }
 
     it "returns a grade for a specific student" do
-      grade = create :grade, student: student2
+      grade = create :grade, assignment: assignment, student: student2
 
       expect(subject.grade(student2)).to eq grade
+    end
+
+    it "does not return the grade if the assignment is not the same" do
+      grade = create :grade, student: student2
+
+      expect(subject.grade(student2)).to be_nil
     end
 
     it "does not hit the database for multiple grades" do
