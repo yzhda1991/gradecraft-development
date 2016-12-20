f1e8073f2272064fe905d8eab83e7c36ab8b0061
Jamie Wright <jamie@brilliantfantastic.com>
2016-12-20 16:03:50 -0500

    Add impersonate methods to helper

--- app/helpers/impersonation_helper.rb
+++ app/helpers/impersonation_helper.rb
@@ -1,4 +1,15 @@
 module ImpersonationHelper
+  def impersonate!(user)
+    impersonating_agent current_user
+    auto_login user
+  end
+
+  def unimpersonate!
+    agent = User.find impersonating_agent_id
+    auto_login agent
+    delete_impersonating_agent
+  end
+
   def impersonating_agent(user)
     session[:impersonating_agent_id] = user.id
   end
