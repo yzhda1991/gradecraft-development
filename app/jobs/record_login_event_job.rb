0452e46cb0e9fdda4a61b56a6d6a68e7f40c257f
Jamie Wright <jamie@brilliantfantastic.com>
2016-12-22 07:35:25 -0500

    Finished the happy path of recording the login event

--- app/jobs/record_login_event_job.rb
+++ app/jobs/record_login_event_job.rb
@@ -3,5 +3,15 @@ class RecordLoginEventJob < ApplicationJob
 
   def perform(data={}, logger=NilLogger.new)
     logger.info "Starting LoginEventLogger with data #{data}"
+
+    course_membership = CourseMembership.find_by user_id: data[:user_id],
+      course_id: data[:course_id]
+
+    log_data = data.merge(last_login_at: course_membership.last_login_at.try(:to_i))
+    result = Analytics::LoginEvent.create log_data
+
+    course_membership.update_attributes last_login_at: data[:created_at]
+
+    logger.info "Successfully logged login event data #{log_data}"
   end
 end
--- spec/jobs/record_login_event_job_spec.rb
+++ spec/jobs/record_login_event_job_spec.rb
@@ -1,7 +1,4 @@
-require "active_job"
-require "active_record_spec_helper"
-require "./app/jobs/application_job"
-require "./app/jobs/record_login_event_job"
+require "rails_spec_helper"
 
 RSpec.describe RecordLoginEventJob do
   it "enqueues on the login event logger queue" do
@@ -9,8 +6,27 @@ RSpec.describe RecordLoginEventJob do
   end
 
   describe "#perform" do
-    let(:data) { { some: :blah } }
-    let(:logger) { double(:logger) }
+    let(:course) { create :course }
+    let(:student_course_membership) { create :student_course_membership, course: course }
+    let(:data) do
+      { event_type: :login,
+        created_at: Time.now,
+        course_id: course.id,
+        user_id: professor.id,
+        student_id: student_course_membership.user.id,
+        user_role: professor_course_membership.role }
+    end
+    let(:last_login_at) { 2.days.ago }
+    let(:professor_course_membership) do
+      create :professor_course_membership, course: course, last_login_at: last_login_at
+    end
+    let(:professor) { professor_course_membership.user }
+    let(:logger) { double(:logger, info: nil) }
+    let(:valid_analytics_event) { double(:analytics_event, valid?: true) }
+
+    before do
+      allow(Analytics::LoginEvent).to receive(:create).and_return valid_analytics_event
+    end
 
     it "logs a start message" do
       expect(logger).to receive(:info).with \
@@ -19,7 +35,32 @@ RSpec.describe RecordLoginEventJob do
       described_class.perform_now data, logger
     end
 
-    xit "adds a login event to analytics for the user and course"
-    xit "updates the course membership's last login timestamp"
+    it "logs a successful outcome message" do
+      expect(logger).to receive(:info).with \
+        "Successfully logged login event data "\
+        "#{data.merge(last_login_at: last_login_at.to_i)}"
+
+      described_class.perform_now data, logger
+    end
+
+    it "adds a login event to analytics for the user and course" do
+      expect(Analytics::LoginEvent).to receive(:create)
+        .with(data.merge(last_login_at: last_login_at.to_i))
+        .and_return valid_analytics_event
+
+      described_class.perform_now data, logger
+    end
+
+    it "updates the course membership's last login timestamp" do
+      described_class.perform_now data, logger
+
+      expect(professor_course_membership.reload.last_login_at).to eq data[:created_at]
+    end
+
+    context "without a course membership found" do
+    end
+
+    context "without a valid response from the analyitics event" do
+    end
   end
 end
