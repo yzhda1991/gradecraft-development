a9db0d74b33764b396a52bbbd83098e7f8af2dbf
Jamie Wright <jamie@brilliantfantastic.com>
2016-12-22 07:51:44 -0500

    Moved the logging to the job callbacks

--- app/jobs/record_login_event_job.rb
+++ app/jobs/record_login_event_job.rb
@@ -1,17 +1,27 @@
 class RecordLoginEventJob < ApplicationJob
   queue_as :login_event_logger
 
-  def perform(data={}, logger=NilLogger.new)
-    logger.info "Starting LoginEventLogger with data #{data}"
+  attr_reader :logged_data
 
+  before_perform do |job|
+    data = job.arguments.first
+    logger = job.arguments.second
+
+    logger.info "Starting LoginEventLogger with data #{data}"
+  end
+
+  after_perform do |job|
+    logger = job.arguments.second
+    logger.info "Successfully logged login event data #{job.logged_data}"
+  end
+
+  def perform(data={}, logger=NilLogger.new)
     course_membership = CourseMembership.find_by user_id: data[:user_id],
       course_id: data[:course_id]
 
-    log_data = data.merge(last_login_at: course_membership.last_login_at.try(:to_i))
-    result = Analytics::LoginEvent.create log_data
+    @logged_data = data.merge(last_login_at: course_membership.last_login_at.try(:to_i))
+    result = Analytics::LoginEvent.create logged_data
 
     course_membership.update_attributes last_login_at: data[:created_at]
-
-    logger.info "Successfully logged login event data #{log_data}"
   end
 end
