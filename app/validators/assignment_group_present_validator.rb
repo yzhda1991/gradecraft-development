4123c891ed1127a4379beeb9ad1948eaf7814e46
chcholman <caitlin.holman@gmail.com>
2016-12-26 08:20:42 -0500

    Rubocop appeasement

--- app/validators/assignment_group_present_validator.rb
+++ app/validators/assignment_group_present_validator.rb
@@ -1,8 +1,6 @@
 class AssignmentGroupPresentValidator < ActiveModel::EachValidator
-# Checking to make sure the group is actually working on an assignment
+  # Checking to make sure the group is actually working on an assignment
   def validate(record)
-    if record.assignment_groups.to_a.size == 0
-      record.errors[attribute] << "You need to check off which #{(course.assignment_term).downcase} your #{(course.group_term).downcase} will work on."
-    end
+    record.errors[attribute] << "You need to check off which #{(course.assignment_term).downcase} your #{(course.group_term).downcase} will work on." if record.assignment_groups.to_a.size == 0
   end
 end
