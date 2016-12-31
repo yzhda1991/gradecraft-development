c106f71e040223cc353b40f65ca96def3714f54b
chcholman <caitlin.holman@gmail.com>
2016-12-31 13:44:49 -0500

    mailer previews

--- test/mailers/previews/announcement_mailer_preview.rb
+++ test/mailers/previews/announcement_mailer_preview.rb
@@ -1,7 +1,7 @@
 class AnnouncementMailerPreview < ActionMailer::Preview
   def announcement_email
-    @course = Course.create(name: "Videogames and Learning", course_number: "101")
-    @student = User.create(first_name: "Hermione", last_name: "Granger", username: "padfoot", email: "hermione@hogwarts.edu")
+    @course = Course.first
+    @student = User.with_role_in_course("student", @course).first
     @announcement = Announcement.create(title: "Hey", body: "Now", author: User.first, recipient: @student, course: @course)
     AnnouncementMailer.announcement_email @announcement, @student
   end
--- test/mailers/previews/exports_mailer_preview.rb
+++ test/mailers/previews/exports_mailer_preview.rb
@@ -1,12 +1,17 @@
 class ExportsMailerPreview < ActionMailer::Preview
   
   def grade_export
-    course = Course.first
+    @course = Course.first
     ExportsMailer.grade_export(
-      course,
+      @course,
       user = User.first,
       course.assignments.to_csv
     )
+    
+    @course = Course.first
+    @student = User.with_role_in_course("student", @course).first
+    @announcement = Announcement.create(title: "Hey", body: "Now", author: User.first, recipient: @student, course: @course)
+    AnnouncementMailer.announcement_email @announcement, @student
   end
   
   def gradebook_export
