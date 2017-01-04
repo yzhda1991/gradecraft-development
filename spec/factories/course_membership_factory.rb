73359b0580da03c2a96f768ccabe51293dd6a9e6
Jon Yu <jonathoy@umich.edu>
2017-01-04 15:43:04 -0500

    Add instructor_of_record definitions to factory

--- spec/factories/course_membership_factory.rb
+++ spec/factories/course_membership_factory.rb
@@ -2,21 +2,27 @@ FactoryGirl.define do
   factory :course_membership do
     association :course
     association :user
+
+    instructor_of_record false
     role :observer
 
     trait :student do
+      instructor_of_record false
       role :student
     end
 
     trait :staff do
+      instructor_of_record true
       role :gsi
     end
 
     trait :professor do
+      instructor_of_record true
       role :professor
     end
 
     trait :admin do
+      instructor_of_record true
       role :admin
     end
 
