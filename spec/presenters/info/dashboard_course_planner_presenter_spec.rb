cfa0672af59ff92c2d7f3491b76ebe695e7e0ee3
Jon Yu <jonathoy@umich.edu>
2017-01-05 13:13:17 -0500

    Ignore draft submissions for assignment list count
    The instructor assignment list module, typically found on the dashboard,
    should not count non-draft submissions.

---
+++ spec/presenters/info/dashboard_course_planner_presenter_spec.rb
@@ -0,0 +1,18 @@
+require "./app/presenters/info/dashboard_course_planner_presenter"
+
+describe Info::DashboardCoursePlannerPresenter do
+  let(:assignment) { build_stubbed(:assignment) }
+  let(:course) { assignment.course }
+  let(:student) { build_stubbed(:user) }
+
+  subject { described_class.new course: course, student: student, assignments: course.assignments }
+
+  describe "#submitted_submissions_count" do
+    let!(:draft_submission) { create(:draft_submission, course: course, assignment: assignment) }
+    let!(:submitted_submission) { create(:submission, course: course, assignment: assignment) }
+
+    it "returns a count for only non-draft submissions" do
+      expect(subject.submitted_submissions_count(assignment)).to eq 1
+    end
+  end
+end
