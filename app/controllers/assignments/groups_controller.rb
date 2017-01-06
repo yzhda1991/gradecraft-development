6339a4990fc31d148c5769e50f8a95055213fd7e
Jonathan Gabel <hello@jonathangabel.com>
2017-01-06 12:46:05 -0500

    direct group grades to angular directive

--- app/controllers/assignments/groups_controller.rb
+++ app/controllers/assignments/groups_controller.rb
@@ -8,9 +8,10 @@ class Assignments::GroupsController < ApplicationController
     @group = @assignment.groups.find(params[:id])
     @submission = @assignment.submissions.where(group_id: @group.id).first
     @assignment_score_levels = @assignment.assignment_score_levels.order_by_points
+    @grade_next_path = path_for_next_group_grade @assignment, @group
     if @assignment.grade_with_rubric?
       @rubric = @assignment.rubric
-      @grade_next_path = path_for_next_group_grade @assignment, @group
+
     end
   end
 
