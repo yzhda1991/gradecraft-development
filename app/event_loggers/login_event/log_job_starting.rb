14ba776922d47a9b7e09a7bf7460d52f8be5a98a
Jamie Wright <jamie@brilliantfantastic.com>
2017-01-10 09:22:24 -0500

    Spec for logging the job when it starts

---
+++ app/event_loggers/login_event/log_job_starting.rb
@@ -0,0 +1,13 @@
+module EventLoggers
+  module LoginEvent
+    class LogJobStarting
+      def call(context)
+        context.guard! { required(:logger).filled }
+
+        logged_data = context.reject { |k| k == :logger }
+        context[:logger].info "Starting LoginEventLogger with data #{logged_data}"
+        context
+      end
+    end
+  end
+end
