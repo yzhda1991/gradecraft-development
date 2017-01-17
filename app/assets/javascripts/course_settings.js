5f2e866a2b69ddbd1c0371f984ee982bc6a30bf1
Marie Hooper <marie.ashtari@gmail.com>
2017-01-17 11:51:14 -0500

    disable settings button when check is removed by unchecking section card

--- app/assets/javascripts/course_settings.js
+++ app/assets/javascripts/course_settings.js
@@ -16,12 +16,12 @@ $('.button-close').on('click',function(){
 // Disable settings menu button if form card feature is not checked
 function disableSettingsButton($thisInput) {
   var $thisSettingsBtn = $thisInput.closest('.form-card').find('.button-advanced-settings');
-  
+
   if ($thisInput.is(':checked')) {
     $thisSettingsBtn.prop('disabled', false);
   } else {
     $thisSettingsBtn.prop('disabled', true);
-  }  
+  }
 }
 
 $('input.has-settings-menu').change(function() {
@@ -38,6 +38,7 @@ function formDependencies() {
   var $dependentOnSection = $('.dependent-on-section');
   var $card = $dependentOnSection.parent().parent();
   var $cardHeader = $dependentOnSection.parent();
+  var $thisSettingsBtn = $card.find('.button-advanced-settings');
   var $sectionsCheckbox = $('input[id="course_has_teams"]');
 
   if($sectionsCheckbox.is(":checked")) {
@@ -47,6 +48,7 @@ function formDependencies() {
     $dependentOnSection.prop("disabled", true);
     $card.addClass("disabled");
     $cardHeader.removeClass("enabled");
+    $thisSettingsBtn.prop('disabled', true);
     $dependentOnSection.prop("checked", false)
   }
 }
