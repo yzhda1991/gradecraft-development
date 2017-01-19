09acbdcf6c8d3221e4c28d780aaaa5d3b25f5226
Jamie Wright <jamie@brilliantfantastic.com>
2017-01-19 11:28:36 -0500

    Log the data as a hash so it can be serializable

--- app/event_loggers/login_event/record_login_event.rb
+++ app/event_loggers/login_event/record_login_event.rb
@@ -12,7 +12,16 @@ module EventLoggers
 
       context.guard! { required(:user_role).filled }
 
-      result = Analytics::LoginEvent.create(context.merge(event_type: :login))
+      data = {
+        course_id: context.course.id,
+        user_id: context.user.id,
+        student_id: context.student.try(:id),
+        user_role: context.user_role,
+        last_login_at: context.last_login_at,
+        event_type: :login,
+        created_at: Time.now
+      }
+      result = Analytics::LoginEvent.create(data)
 
       unless result.valid?
         context.fail!(Porch::HumanError.new(result.errors).message)
