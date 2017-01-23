ff43058226e7b519ca1563a3049b625815478b44
Jamie Wright <jamie@brilliantfantastic.com>
2017-01-23 11:58:07 -0500

    Use Rails logger for the log job starting step

--- app/event_loggers/login_event/log_job_starting.rb
+++ app/event_loggers/login_event/log_job_starting.rb
@@ -3,10 +3,9 @@ module EventLoggers
     def call(context)
       context.guard! do
         required(:event_data).filled
-        required(:logger).filled
       end
 
-      context.logger.info "Starting LoginEventLogger with data #{context.event_data}"
+      Rails.logger.info "Starting LoginEventLogger with data #{context.event_data}"
       context
     end
   end
--- spec/event_loggers/login_event/log_job_starting_spec.rb
+++ spec/event_loggers/login_event/log_job_starting_spec.rb
@@ -7,6 +7,9 @@ describe EventLoggers::LogJobStarting do
     let(:context) { Porch::Context.new({ logger: logger, event_data: event_data }) }
     let(:event_data) { { blah: :bleh }}
     let(:logger) { NullLogger.new }
+    let(:rails) { double(logger: logger) }
+
+    before { stub_const("Rails", rails) }
 
     it "logs the starting of the job to the logger" do
       expect(logger).to receive(:info)
@@ -17,17 +20,6 @@ describe EventLoggers::LogJobStarting do
 
     it "fails if the event data is not present" do
       context.delete :event_data
-
-      result = nil
-      expect { subject.call context }.to raise_error { |error| result = error.context }
-
-      expect(result).to be_failure
-    end
-
-    it "fails if the logger is not present" do
-      context.delete :logger
-
-      expect(logger).to_not receive(:info)
 
       result = nil
       expect { subject.call context }.to raise_error { |error| result = error.context }
