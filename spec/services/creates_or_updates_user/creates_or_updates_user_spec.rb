0493fabfdf12459ba904ec90111b30173f09d642
Jon Yu <jonathoy@umich.edu>
2017-01-25 11:31:42 -0500

    Modify user creation to not rely on course
    In the case of creating a user from LTI, we do not want to couple the
    user creation with course membership creation, since the logic differs.

--- spec/services/creates_or_updates_user/creates_or_updates_user_spec.rb
+++ spec/services/creates_or_updates_user/creates_or_updates_user_spec.rb
@@ -3,7 +3,6 @@ require "active_record_spec_helper"
 require "./app/services/creates_or_updates_user/creates_or_updates_user"
 
 describe Services::Actions::CreatesOrUpdatesUser do
-  let(:course) { create :course }
   let(:user) { build :user }
   let(:attributes) { user.attributes.symbolize_keys }
 
@@ -13,42 +12,63 @@ describe Services::Actions::CreatesOrUpdatesUser do
   end
 
   it "expects attributes to assign to the user" do
-    expect { described_class.execute course: course, send_welcome_email: false }.to \
-      raise_error LightService::ExpectedKeysNotInContextError
-  end
-
-  it "expects course to assign to the user" do
-    expect { described_class.execute attributes: attributes, send_welcome_email: false }.to \
+    expect { described_class.execute send_welcome_email: false }.to \
       raise_error LightService::ExpectedKeysNotInContextError
   end
 
   it "expects the option to send the welcome email" do
-    expect { described_class.execute attributes: attributes, course: course }.to \
+    expect { described_class.execute attributes: attributes }.to \
       raise_error LightService::ExpectedKeysNotInContextError
   end
 
-  it "creates the user if they do not exist" do
-    expect(Services::CreatesNewUser).to receive(:create).and_call_original
-    expect(Services::UpdatesUser).to_not receive(:update)
-    described_class.execute attributes: attributes, course: course, send_welcome_email: false
-  end
-
-  it "updates the user if they already exist" do
-    user.save!
-    expect(Services::UpdatesUser).to receive(:update).and_call_original
-    expect(Services::CreatesNewUser).to_not receive(:create)
-    described_class.execute attributes: attributes, course: course, send_welcome_email: false
-  end
-
-  it "fails if the attributes do not have an email address to check" do
-    attributes.delete(:email)
-    expect(Services::CreatesNewUser).to_not receive(:create)
-    expect(Services::UpdatesUser).to_not receive(:update)
-    described_class.execute attributes: attributes, course: course, send_welcome_email: false
-  end
-
   it "promises the created or updated user" do
-    result = described_class.execute attributes: attributes, course: course, send_welcome_email: false
+    result = described_class.execute attributes: attributes, send_welcome_email: false
     expect(result).to have_key :user
+  end
+
+  context "when the course is not provided in the context" do
+    it "creates the user if they do not exist" do
+      expect(Services::CreatesNewUser).to receive(:create).and_call_original
+      expect(Services::UpdatesUser).to_not receive(:update)
+      described_class.execute attributes: attributes, send_welcome_email: false
+    end
+
+    it "updates the user if they already exist" do
+      user.save!
+      expect(Services::UpdatesUser).to receive(:update).and_call_original
+      expect(Services::CreatesNewUser).to_not receive(:create)
+      described_class.execute attributes: attributes, send_welcome_email: false
+    end
+
+    it "fails if the attributes do not have an email address to check" do
+      attributes.delete(:email)
+      expect(Services::CreatesNewUser).to_not receive(:create)
+      expect(Services::UpdatesUser).to_not receive(:update)
+      described_class.execute attributes: attributes, send_welcome_email: false
+    end
+  end
+
+  context "when the course is provided in the context" do
+    let(:course) { create :course }
+
+    it "creates the user if they do not exist" do
+      expect(Services::CreatesNewUser).to receive(:create).and_call_original
+      expect(Services::UpdatesUserForCourse).to_not receive(:update)
+      described_class.execute attributes: attributes, course: course, send_welcome_email: false
+    end
+
+    it "updates the user if they already exist" do
+      user.save!
+      expect(Services::UpdatesUserForCourse).to receive(:update).and_call_original
+      expect(Services::CreatesNewUser).to_not receive(:create)
+      described_class.execute attributes: attributes, course: course, send_welcome_email: false
+    end
+
+    it "fails if the attributes do not have an email address to check" do
+      attributes.delete(:email)
+      expect(Services::CreatesNewUser).to_not receive(:create)
+      expect(Services::UpdatesUserForCourse).to_not receive(:update)
+      described_class.execute attributes: attributes, course: course, send_welcome_email: false
+    end
   end
 end
