27d627dba3a6b41e3a63db460f196c83ad2d59df
Jon Yu <jonathoy@umich.edu>
2017-01-30 11:16:34 -0500

    Fail context if auth hash is missing name info .. but has an email

--- app/services/creates_or_updates_user_from_lti/parse_user_attributes_from_auth_hash.rb
+++ app/services/creates_or_updates_user_from_lti/parse_user_attributes_from_auth_hash.rb
@@ -9,6 +9,7 @@ module Services
       executed do |context|
         raise ArgumentError, "Unexpected auth_hash format" unless context.auth_hash.is_a? OmniAuth::AuthHash
         context.auth_hash.extra.raw_info.tap do |raw_info|
+          ensure_valid_request context, raw_info
           email = raw_info.lis_person_contact_email_primary
           username = email.split("@")[0]
 
@@ -20,6 +21,16 @@ module Services
             username: username,
             kerberos_uid: username
           }
+        end
+      end
+
+      private
+
+      # Fail context early if first_name, last_name are missing
+      def self.ensure_valid_request(context, attributes)
+        if attributes.lis_person_name_given.blank? && attributes.lis_person_name_family.blank? &&
+          attributes.lis_person_contact_email_primary.present?
+          context.fail! "Please check your LTI configuration", 400
         end
       end
     end
