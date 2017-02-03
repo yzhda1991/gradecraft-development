99f9212b14760156edf291263fde4e7c5c8f9c39
Jon Yu <jonathoy@umich.edu>
2017-02-03 13:18:25 -0500

    Add not null database constraint to email
    .. on users table to be consistent with existing not null constraint on
    username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203175241) do
+ActiveRecord::Schema.define(version: 20170203181526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -745,7 +745,7 @@ ActiveRecord::Schema.define(version: 20170203175241) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",                                                               null: false
-    t.string   "email"
+    t.string   "email",                                                                  null: false
     t.string   "crypted_password"
     t.string   "salt"
     t.datetime "created_at"
