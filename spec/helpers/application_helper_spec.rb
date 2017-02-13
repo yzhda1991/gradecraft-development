e6a2cc9c071ab61cf4c368f06dad9f9ba8fb66b5
Jon Yu <jonathoy@umich.edu>
2017-02-13 11:10:15 -0500

    Minor suggestions for spec test

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,30 +1,42 @@
 require "rails_spec_helper"
 require "./app/helpers/application_helper"
-include ActionView
 
 describe ApplicationHelper do
   include RSpecHtmlMatchers
 
   describe "#icon_tooltip" do
-    let(:tooltip_id) {"tooltip-id"}
-    let(:icon) {:lock}
+    let(:tooltip_id) { "tooltip-id" }
+    let(:icon) { :lock }
 
-    it "it renders tooltip with icon when given" do
-      icon_tooltip = helper.icon_tooltip(tooltip_id, icon) {"blah blah"}
-      expect(icon_tooltip).to have_tag "span", with: { class: "has-tooltip", "aria-describedby": "tooltip-id", tabindex: "0" } do
-        with_tag "i", with: { class: "fa-lock" }
-        with_tag "span", with: { class: "display-on-hover hover-style hover-style-top", id: "tooltip-id", role: "tooltip"}
-      end
-    end
-    context "tooltip with placement specified" do
-      let(:placement) {"right"}
-
-      it "adds appropriate class based on placement" do
-        icon_tooltip = helper.icon_tooltip(tooltip_id, icon, placement) {"blah blah"}
+    context "when there is no placement specified" do
+      it "renders the expected elements if a block is given" do # this should describe the test, but I'm iffy about the wording here
+        icon_tooltip = helper.icon_tooltip(tooltip_id, icon) { "blah blah" }
         expect(icon_tooltip).to have_tag "span", with: { class: "has-tooltip", "aria-describedby": "tooltip-id", tabindex: "0" } do
           with_tag "i", with: { class: "fa-lock" }
-          with_tag "span", with: { class: "display-on-hover hover-style hover-style-right", id: "tooltip-id", role: "tooltip"}
+          with_tag "span", with: { class: "display-on-hover hover-style hover-style-top", id: "tooltip-id", role: "tooltip" }
         end
+      end
+
+      it "renders the expected elements if no block is given" do
+        pending("the implementation")
+        your_test
+      end
+    end
+
+    context "when there is placement specified" do
+      let(:placement) { "right" }
+
+      it "renders the expected elements if a block is given" do
+        icon_tooltip = helper.icon_tooltip(tooltip_id, icon, placement) { "blah blah" }
+        expect(icon_tooltip).to have_tag "span", with: { class: "has-tooltip", "aria-describedby": "tooltip-id", tabindex: "0" } do
+          with_tag "i", with: { class: "fa-lock" }
+          with_tag "span", with: { class: "display-on-hover hover-style hover-style-right", id: "tooltip-id", role: "tooltip" }
+        end
+      end
+
+      it "renders the expected elements if no block is given" do
+        pending("the implementation")
+        your_test
       end
     end
   end
