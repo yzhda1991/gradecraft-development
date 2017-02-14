1943e41920d345ee8fb5fae51abd58b4f585d420
Marie Hooper <marie.ashtari@gmail.com>
2017-02-14 11:31:40 -0500

    reduce spec quantity for tooltip helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,10 +7,10 @@ describe ApplicationHelper do
   describe "#tooltip" do
     let(:tooltip_id) { "tooltip-id" }
 
-    context "when type is icon and there is no placement specified" do
+    context "when type is icon" do
       let(:src) { :lock }
 
-      it "renders the expected tooltip html if a block is given" do
+      it "renders the expected tooltip if a block is given" do
         tooltip = helper.tooltip(tooltip_id, src) { "blah blah" }
         expect(tooltip).to have_tag "span", with: { class: "has-tooltip", "aria-describedby": "tooltip-id", tabindex: "0" } do
           with_tag "i", with: { class: "fa-lock" }
@@ -18,7 +18,7 @@ describe ApplicationHelper do
         end
       end
 
-      it "renders the expected tooltip html if no block is given" do
+      it "renders the expected tooltip if no block is given" do
         tooltip = helper.tooltip(tooltip_id, src)
         expect(tooltip).to have_tag "span", with: { class: "has-tooltip", "aria-describedby": "tooltip-id", tabindex: "0" } do
           with_tag "i", with: { class: "fa-lock" }
@@ -27,58 +27,28 @@ describe ApplicationHelper do
       end
     end
 
-    context "when type is icon and placement is specified" do
-      let(:placement) { "right" }
+    context "when tooltip placement is specified" do
       let(:src) { :lock }
+      let(:placement) {"right"}
 
-      it "renders the expected tooltip html if a block is given" do
+      it "renders tooltip at correct position" do
         tooltip = helper.tooltip(tooltip_id, src, placement: placement) { "blah blah" }
         expect(tooltip).to have_tag "span", with: { class: "has-tooltip", "aria-describedby": "tooltip-id", tabindex: "0" } do
           with_tag "i", with: { class: "fa-lock" }
           with_tag "span", with: { class: "display-on-hover hover-style hover-style-right", id: "tooltip-id", role: "tooltip" }
         end
       end
-
-      it "renders the expected tooltip html if no block is given" do
-        tooltip = helper.tooltip(tooltip_id, src, placement: placement)
-        expect(tooltip).to have_tag "span", with: { class: "has-tooltip", "aria-describedby": "tooltip-id", tabindex: "0" } do
-          with_tag "i", with: { class: "fa-lock" }
-          with_tag "span", with: { class: "display-on-hover hover-style hover-style-right", id: "tooltip-id", role: "tooltip" }
-        end
-      end
     end
 
-    context "when type is image without placement specified", focus: true do
+    context "when type is image" do
       let(:src) { "http://localhost:5000/images/badge.png" }
       let(:type) {"image"}
 
-      it "renders the expected tooltip html if a block is given" do
+      it "renders expected tooltip with image trigger" do
         tooltip = helper.tooltip(tooltip_id, src, type: type) { "blah blah" }
         expect(tooltip).to have_tag "span", with: { class: "has-tooltip", "aria-describedby": "tooltip-id", tabindex: "0" } do
           with_tag "img", with: { src: "http://localhost:5000/images/badge.png" }
           with_tag "span", with: { class: "display-on-hover hover-style hover-style-top", id: "tooltip-id", role: "tooltip" }
-        end
-      end
-
-      it "renders the expected tooltip html if no block is given" do
-        tooltip = helper.tooltip(tooltip_id, src, type: type)
-        expect(tooltip).to have_tag "span", with: { class: "has-tooltip", "aria-describedby": "tooltip-id", tabindex: "0" } do
-          with_tag "img", with: { src: "http://localhost:5000/images/badge.png" }
-          with_tag "span", with: { class: "display-on-hover hover-style hover-style-top", id: "tooltip-id", role: "tooltip" }
-        end
-      end
-    end
-
-    context "when there is placement specified" do
-      let(:src) { "http://localhost:5000/images/badge.png" }
-      let(:type) {"image"}
-      let(:placement) { "right" }
-
-      it "renders tooltip if a block is given" do
-        tooltip = helper.tooltip(tooltip_id, src, type: type, placement: placement) { "blah blah" }
-        expect(tooltip).to have_tag "span", with: { class: "has-tooltip", "aria-describedby": "tooltip-id", tabindex: "0" } do
-          with_tag "img", with: { src: "http://localhost:5000/images/badge.png" }
-          with_tag "span", with: { class: "display-on-hover hover-style hover-style-right", id: "tooltip-id", role: "tooltip" }
         end
       end
     end
