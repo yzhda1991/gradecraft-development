9cc101ff5e86889f461ddf2b8856b883d4f2f186
Jamie Wright <jamie@brilliantfantastic.com>
2017-02-22 22:22:36 -0500

    Add ExponentialBackoff plugin to the base resque job

--- app/jobs/resque_application_job.rb
+++ app/jobs/resque_application_job.rb
@@ -1,3 +1,8 @@
 class ResqueApplicationJob < ApplicationJob
   extend Resque::Plugins::Retry
+  extend Resque::Plugins::ExponentialBackoff
+
+  def self.backoff_strategy
+    @backoff_strategy ||= ResqueJob.configuration.backoff_strategy
+  end
 end
