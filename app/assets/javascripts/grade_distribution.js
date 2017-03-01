c9829e413f64f234780d214d565a81d9d5ef67a0
Marie Hooper <marie.ashtari@gmail.com>
2017-02-28 19:06:37 -0500

    remove hover data point labels, format userScore

--- app/assets/javascripts/grade_distribution.js
+++ app/assets/javascripts/grade_distribution.js
@@ -5,16 +5,15 @@ if ($gradeDistro.length) {
 var scores = dataSet.scores;
 var userScore = dataSet.user_score[0];
 
-var trace1 = {
+var data = [{
   x: scores,
   type: 'box'
-};
-
-var data = [trace1];
+}];
 
 var layout = {
   showlegend: false,
-  height: 160,
+  height: 120,
+  hovermode: !1,
   margin: {
     l: 8,
     r: 8,
@@ -22,7 +21,11 @@ var layout = {
     t: 8,
     pad: 8
   },
+  xaxis: {
+    fixedrange: true
+  },
   yaxis: {
+    fixedrange: true,
     showticklabels: false
   },
   marker: {
@@ -30,22 +33,25 @@ var layout = {
   }
 };
 
-if ($gradeDistro.hasClass('student-distro')) {
-    layout.annotations = [
-      {
-        x: userScore,
-        y: 0,
-        xref: 'x',
-        yref: 'y',
-        text: userScore,
-        showarrow: true,
-        arrowhead: 2,
-        arrowsize: 1,
-        arrowwidth: 2,
-        ax: 0.5,
-        ay: -50
-      }
-    ]
-  }
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
-  Plotly.newPlot('grade_distro', data, layout, {displayModeBar: false});
+if ($gradeDistro.hasClass('student-distro')) {
+  layout.annotations = [{
+    x: userScore,
+    y: 0,
+    xref: 'x',
+    yref: 'y',
+    text: numberWithCommas(userScore),
+    showarrow: true,
+    arrowhead: 2,
+    arrowsize: 1,
+    arrowwidth: 2,
+    ax: 0.5,
+    ay: -40
+  }]
+}
+
+  // eslint-disable-next-line no-undef
+  Plotly.newPlot('grade_distro', data, layout, {displayModeBar: false}); 
