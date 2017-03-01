fb5b0ad8f3abdf652d82ad28756c7a21e36d197a
Kyle Dove <dovek@umich.edu>
2017-03-01 13:33:12 -0500

    Upgraded google-api-client from 0.8 to 0.10

--- app/controllers/google_calendar_controller.rb
+++ app/controllers/google_calendar_controller.rb
@@ -2,56 +2,58 @@ class GoogleCalendarController < ApplicationController
   include OAuthProvider
 
   oauth_provider_param :google_oauth2
+  require 'google/apis/calendar_v3'
+  require 'google/api_client/client_secrets.rb'
+  require 'googleauth'
+
+  Calendar = Google::Apis::CalendarV3
 
   def index
-    binding.pry
-    puts "stopped"
     redirect_to "/events"
   end
 
-  def refresh_google_token
+  def add_event_to_google_calendar
     @google_authorization = current_user.authorizations.find_by(provider: "google_oauth2")
     if !@google_authorization.nil?
-        puts "google_oauth2 exists"
-        @google_authorization = current_user.authorizations.find_by(provider: "google_oauth2")
-        puts "expires_at:    " + @google_authorization.expires_at.to_s
-        if Time.now > @google_authorization.expires_at
-          puts "google_oauth2 exists but is expired. Refreshing token."
-          @google_authorization.refresh!({ client_id: ENV["GOOGLE_CLIENT_ID"], client_secret: ENV["GOOGLE_SECRET"] })
-          puts "token is now refreshed"
-        else
-          puts "Token is fine"
-        end
-        #get event info
-        puts "name: " + params[:id]
-        @event = current_course.events.find(params[:id])
-        puts "event: " + @event.open_at.to_s
+      @google_authorization = current_user.authorizations.find_by(provider: "google_oauth2")
+      if Time.now > @google_authorization.expires_at
+        @google_authorization.refresh!({ client_id: ENV["GOOGLE_CLIENT_ID"], client_secret: ENV["GOOGLE_SECRET"] })
+      end
+      @event = current_course.events.find(params[:id])
 
-        @client = Google::APIClient.new
-        @client.authorization.access_token = @google_authorization.access_token
-        @client.authorization.refresh_token = @google_authorization.refresh_token
-        @client.authorization.client_id = ENV["GOOGLE_CLIENT_ID"]
-        @client.authorization.client_secret = ENV["GOOGLE_SECRET"]
-        @client.authorization.refresh!
+      if !@event.open_at.nil? and !@event.due_at.nil?
+        event = Calendar::Event.new({
+          :summary => @event.name,
+          :start => {
+            :date_time => @event.open_at.to_datetime.rfc3339
+          },
+          :end => {
+            :date_time => @event.due_at.to_datetime.rfc3339
+          }
+        })
 
-        @service = @client.discovered_api('calendar', 'v3')
+        calendar = Calendar::CalendarService.new
+        client_id = Google::Auth::ClientId.new(ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'])
 
-        event = {
-          summary: "New Event!",
-          start: {dateTime: '2017-02-27T09:00:00+0000'},
-          end:   {dateTime: '2017-02-27T10:00:00+0000'}
-        }
+        secrets = Google::APIClient::ClientSecrets.new({"web" =>
+          {"access_token" => @google_authorization.access_token,
+            "refresh_token" => @google_authorization.refresh_token,
+            "client_id" => ENV['GOOGLE_CLIENT_ID'],
+            "client_secret" => ENV['GOOGLE_SECRET']}
+          })
+        calendar.authorization = secrets.to_authorization
+        calendar.authorization.refresh!
 
-        @client.execute(:api_method => @service.events.insert,:parameters =>
-        {'calendarId' => 'primary','sendNotifications' => false},
-        :body => JSON.dump(event),:headers => {'Content-Type' => 'application/json'})
+        event = calendar.insert_event('primary', event)
+      else
+        puts "event missing START or END time"
+      end
 
-        redirect_to "/events"
+      redirect_to "/events"
     else
       puts "google_oath2 does not exist. Redirecting to authentication page"
       redirect_to "/auth/google_oauth2"
     end
-
   end
 
 end
