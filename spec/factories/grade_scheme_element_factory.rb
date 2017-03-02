6abe527a02925a78d7b1771993b69a526f851c05
Jon Yu <jonathoy@umich.edu>
2017-03-02 14:01:23 -0500

    Remove obsolete highest_points from factories

--- spec/factories/grade_scheme_element_factory.rb
+++ spec/factories/grade_scheme_element_factory.rb
@@ -5,21 +5,18 @@ FactoryGirl.define do
 
     factory :grade_scheme_element_high do
       lowest_points 10000
-      highest_points 20000
       letter { "A" }
       level { "Amazing" }
     end
 
     factory :grade_scheme_element_low do
       lowest_points 0
-      highest_points 9999
       letter { "F" }
       level { "Awful" }
     end
 
     factory :grade_scheme_element_highest do
       lowest_points 20001
-      highest_points 30000
       letter { "A+" }
       level { "Ahhhmazing" }
     end
