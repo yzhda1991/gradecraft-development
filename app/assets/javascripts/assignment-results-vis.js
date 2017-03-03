0f38f1bc96a269c8b45bdc7cf35d8706ccb6c359
Marie Hooper <marie.ashtari@gmail.com>
2017-03-03 15:55:11 -0500

    add donut chart to show participation percentage

--- app/assets/javascripts/assignment-results-vis.js
+++ app/assets/javascripts/assignment-results-vis.js
@@ -53,5 +53,44 @@ if ($assignmentDistro.length) {
     Plotly.newPlot('grades_per_assign', data, layout, {displayModeBar: false});
 }
 
+var $numberComplete = $('#numberComplete');
+if ($numberComplete.length) {
+  var percentParticipated = JSON.parse($numberComplete.attr('data-percent'));
+  var percentNotParticipated = 100 - percentParticipated;
+  var participationData = [{
+    values: [percentParticipated, percentNotParticipated],
+    labels: ['Learners Participated'],
+    type: 'pie',
+    hole: .6,
+    marker: {
+      colors: ['rgba(31, 119, 180, 0.5)', 'rgba(222,227,229, 0.5)' ],
+      line: {
+        color: 'rgba(31, 119, 180, 1)',
+        width: 2
+      }
+    }
+  }];
 
-$('#numberComplete').circliful();
+  var pieLayout = {
+    showlegend: false,
+    height: 220,
+    margin: {
+      l: 8,
+      r: 8,
+      b: 40,
+      t: 4,
+      pad: 8
+    },
+    annotations: [{
+      font: {
+        size: 16
+      },
+      showarrow: false,
+      text: 'Learners<br> Participated',
+      x: 0.5,
+      y: 0.5
+    }]
+  };
+
+  Plotly.newPlot('numberComplete', participationData, pieLayout, {displayModeBar: false});
+}
