b562c6e1fb43a28725c907d87f883f7dd3982c6c
chcholman <caitlin.holman@gmail.com>
2017-03-05 15:00:04 -0500

    Remove world from students controller

--- spec/controllers/assignments/students_controller_spec.rb
+++ spec/controllers/assignments/students_controller_spec.rb
@@ -1,36 +1,39 @@
 require "spec_helper"
 
 describe Assignments::StudentsController do
-  let(:world) { World.create.with(:course, :assignment, :professor, :student) }
+  let(:course) { build(:course) }
+  let(:assignment) { create(:assignment, course: course) }
+  let(:professor) { create(:course_membership, :professor, course: course).user }
+  let(:student) { create(:course_membership, :student, course: course).user }
 
   describe "#grade" do
 
     context "as a professor" do
-      before(:each) { login_user(world.professor) }
+      before(:each) { login_user(professor) }
 
       it "creates the grade for the assignment and student if it doesn't exist" do
-        post :grade, params: { assignment_id: world.assignment.id,
-                               student_id: world.student.id }
+        post :grade, params: { assignment_id: assignment.id,
+                               student_id: student.id }
 
         grade = Grade.unscoped.last
-        expect(grade.assignment_id).to eq world.assignment.id
-        expect(grade.student_id).to eq world.student.id
+        expect(grade.assignment_id).to eq assignment.id
+        expect(grade.student_id).to eq student.id
       end
 
       it "redirects to the edit grade view" do
-        post :grade, params: { assignment_id: world.assignment.id,
-                               student_id: world.student.id }
+        post :grade, params: { assignment_id: assignment.id,
+                               student_id: student.id }
 
         expect(response).to redirect_to edit_grade_path(Grade.unscoped.last)
       end
     end
 
     context "as a student" do
-      before(:each) { login_user(world.student) }
+      before(:each) { login_user(student) }
 
       it "redirects to the root" do
-        post :grade, params: { assignment_id: world.assignment.id,
-                               student_id: world.student.id }
+        post :grade, params: { assignment_id: assignment.id,
+                               student_id: student.id }
 
         expect(response).to redirect_to root_path
       end
