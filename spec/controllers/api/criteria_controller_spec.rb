80ce4bc01fec13027887d85055e7604fbb41ac1e
chcholman <caitlin.holman@gmail.com>
2017-03-06 14:57:10 -0500

    World removal

--- spec/controllers/api/criteria_controller_spec.rb
+++ spec/controllers/api/criteria_controller_spec.rb
@@ -1,12 +1,12 @@
 require "spec_helper"
 
 describe API::CriteriaController do
-  let(:course) { build :course }
+  let(:course) { create :course }
   let(:student)  { create(:course_membership, :student, course: course).user }
   let(:professor) { create(:course_membership, :professor, course: course).user }
   let(:assignment) { create(:assignment, course: course) }
-  let(:rubric) { create(:rubric, assignment: assignment) }
-  let(:criterion) { create(:criterion, rubric: rubric) }
+  let!(:rubric) { create(:rubric, assignment: assignment) }
+  let!(:criterion) { create(:criterion, rubric: rubric) }
   let(:criterion_grade) { create(:criterion_grade, criterion: criterion) }
 
   context "as professor" do
@@ -15,7 +15,7 @@ describe API::CriteriaController do
     describe "GET criteria" do
       it "returns criteria for the current assignment" do
         get :index, params: { assignment_id: assignment.id }, format: :json
-        expect(assigns(:criteria)[0].id).to eq(criteria[0].id)
+        expect(assigns(:criteria)[0].id).to eq(rubric.criteria[0].id)
         expect(response).to render_template(:index)
       end
     end
