cf5b65cbee4362f0193c0666a2a14bc7ac84526b
Kyle Dove <dovek@umich.edu>
2017-03-14 13:02:29 -0400

    Made changes based on code review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   before_action :ensure_staff?,
     except: [:activate, :activated, :edit_profile, :update_profile]
   before_action :ensure_admin?, only: [:all]
+  before_action :save_referer, only: :manually_activate
   skip_before_action :require_login, only: [:activate, :activated]
   skip_before_action :require_course_membership, only: [:activate, :activated]
 
@@ -99,7 +100,7 @@ class UsersController < ApplicationController
     session[:return_to] ||= request.referer
     @user = User.find(params[:id])
     @user.activate!
-    redirect_to session.delete(:return_to), notice: "#{@user.first_name} #{@user.last_name} has been activated!" and return
+    redirect_to session[:return_to] || dashboard_path, notice: "#{@user.first_name} #{@user.last_name} has been activated!" and return
   end
 
   def activated
