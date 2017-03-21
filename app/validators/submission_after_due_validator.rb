ace5de53932c4abb2ce9f0b90407c21ef5e267f2
Jonathan Gabel <hello@jonathangabel.com>
2017-03-21 13:51:49 -0400

    update submissions accepted date validator

--- app/validators/submission_after_due_validator.rb
+++
@@ -1,5 +0,0 @@
-class SubmissionAfterDueValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    record.errors.add :base, "Submission accept date must be after due date." if (record.due_at? && record.accepts_submissions_until?) && (record.accepts_submissions_until < record.due_at)
-  end
-end
