eb9f47ef291a51872e256521d8caa373bdfb2300
Jonathan Gabel <hello@jonathangabel.com>
2017-03-21 14:41:03 -0400

    open before close validator

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -51,9 +51,8 @@ class Assignment < ActiveRecord::Base
   :show_name_when_locked, :show_points_when_locked, :show_description_when_locked,
   :show_purpose_when_locked, in: [true, false], message: "must be true or false"
 
-  #validates :accepts_submissions_until, submission_after_open: true
-  validates :open_at, open_before_close: true
   validates_numericality_of :max_group_size, :min_group_size, allow_nil: true, greater_than_or_equal_to: 1
+  validates_with OpenBeforeCloseValidator
   validates_with SubmissionsAcceptedAfterOpenValidator
   validates_with SubmissionsAcceptedAfterDueValidator
   validates_with PointsUnderCapValidator
--- app/validators/open_before_close_validator.rb
+++ app/validators/open_before_close_validator.rb
@@ -1,5 +1,8 @@
-class OpenBeforeCloseValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    record.errors.add :base, "Due date must be after open date." if (record.due_at? && record.open_at?) && (record.due_at < record.open_at)
+class OpenBeforeCloseValidator < ActiveModel::Validator
+  def validate(record)
+    record.errors.add :base,
+      "Due date must be after open date." \
+      if (record.due_at? && record.open_at?) &&
+         (record.due_at < record.open_at)
   end
 end
