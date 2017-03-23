45afa5b11c1f6356effff55a9e9b34bae09503bb
Kyle Dove <dovek@umich.edu>
2017-03-23 15:59:30 -0400

    Resolving merge conflicts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   respond_to :html, :json
 
   before_action :ensure_staff?,
-    except: [:activate, :activated, :edit_profile, :update_profile]
+    except: [:activate, :activated, :edit_profile, :update_profile] 
   before_action :save_referer, only: [:manually_activate, :resend_invite_email]
   skip_before_action :require_login, only: [:activate, :activated]
   skip_before_action :require_course_membership, only: [:activate, :activated]
