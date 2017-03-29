0eca63e61755fafb4aed18d2a528d47a492e0cda
chcholman <caitlin.holman@gmail.com>
2017-03-29 14:26:41 -0400

    renaming

--- spec/models/concerns/grade_status_spec.rb
+++ spec/models/concerns/grade_status_spec.rb
@@ -4,35 +4,35 @@ describe GradeStatus do
   let(:assignment) { create(:assignment, course: course) }
   let(:grade) { create(:grade, assignment: assignment, student: student) }
   
-  describe "graded_and_visible_by_student?" do
+  describe "student_visible?" do
     it "returns true if the grade is released" do
       grade.status = "Released"
       assignment.release_necessary = true
-      expect(grade.graded_and_visible_by_student?).to eq true
+      expect(grade.student_visible?).to eq true
     end
     
     it "returns true if the grade is graded and the assignment does not need release" do 
       grade.status = "Graded"
       assignment.release_necessary = false
-      expect(grade.graded_and_visible_by_student?).to eq true
+      expect(grade.student_visible?).to eq true
     end
     
     it "returns false if the grade is not released" do
       grade.status = "Graded"
       assignment.release_necessary = true
-      expect(grade.graded_and_visible_by_student?).to eq false
+      expect(grade.student_visible?).to eq false
     end
     
     it "returns false if the grade is not marked as graded" do
       grade.status = nil
       assignment.release_necessary = true
-      expect(grade.graded_and_visible_by_student?).to eq false
+      expect(grade.student_visible?).to eq false
     end
     
     it "returns false if the grade is marked as in progress" do
       grade.status = "In Progress"
       assignment.release_necessary = true
-      expect(grade.graded_and_visible_by_student?).to eq false
+      expect(grade.student_visible?).to eq false
     end
   end
 end
