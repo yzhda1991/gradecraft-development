f8beb4462bf21b1ca28b8f037af0ee806aac803b
chcholman <caitlin.holman@gmail.com>
2017-04-03 23:20:01 -0400

    Remove all references to new_password path

--- config/routes.rb
+++ config/routes.rb
@@ -286,7 +286,7 @@ Rails.application.routes.draw do
   get :logout, to: "user_sessions#destroy", as: :logout
   get :reset, to: "user_sessions#new"
   resources :user_sessions, only: [:new, :create, :destroy]
-  resources :passwords, except: [:destroy, :index, :show]
+  resources :passwords, except: [:new, :destroy, :index, :show]
 
   get "impersonate_student/:student_id", to: "user_sessions#impersonate_student", as: :student_preview
   get "exit_student_impersonation", to: "user_sessions#exit_student_impersonation"
