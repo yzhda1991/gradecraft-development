86734308c9e2849f6b732b1184cae1011d6ff554
Jon Yu <jonathoy@umich.edu>
2017-04-12 09:16:03 -0400

    Revise Canvas API to return a result hash
    Currently includes metadata describing whether there are additional
    pages to traverse

--- lib/canvas/api.rb
+++ lib/canvas/api.rb
@@ -16,6 +16,7 @@ module Canvas
     # Fetch data from Canvas
     # Optionally decide whether to automatically traverse additional pages
     def get_data(path="/", params={}, fetch_next=true)
+      result = {}
       params.merge! access_token: access_token
       next_url = "#{self.class.base_uri}#{path}"
       next_url += "?#{params.to_query}" unless params.empty?
@@ -25,8 +26,11 @@ module Canvas
         response = self.class.get(next_url, query: { access_token: access_token })
         raise ResponseError.new(response) unless response.success?
         yield response.parsed_response if block_given?
-        next_url = fetch_next ? get_next_url(response) : nil
+        next_url = get_next_url(response)
+        result[:has_next_page] = next_url.present?
+        break if !fetch_next
       end
+      result
     end
 
     def set_data(path="/", method=:post, params={})
