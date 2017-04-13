2997744b66fbd3658962bd868edd87dfb6c2cd85
chcholman <caitlin.holman@gmail.com>
2017-04-13 17:33:07 -0400

    Fix bug where resubmit button didn't show if the grade only needed to be graded

--- app/proctors/submission_proctor.rb
+++ app/proctors/submission_proctor.rb
@@ -16,7 +16,7 @@ class SubmissionProctor
   end
 
   def open_for_editing?(assignment)
-    return false if @submission.graded? && !@submission.submission_grade.is_released?
+    return false if @submission.graded? && !@submission.submission_grade.student_visible?
     if @submission.will_be_resubmitted?
       assignment.open? && assignment.resubmissions_allowed?
     else
