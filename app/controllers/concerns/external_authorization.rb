fd85eb10aebe8863338c1e9e58fff4c6b6cebf68
Jon Yu <jonathoy@umich.edu>
2017-04-18 14:05:04 -0400

    Return 401 vs redirecting if API request
    Per the Angular docs regarding $http usage:
    
    Note that if the response is a redirect, XMLHttpRequest will
    transparently follow it, meaning that the outcome (success or error)
    will be determined by the final response status code.
    
    If the user is unauthorized due to an OAuth issue, a 401 is returned for
    handling by the client.

--- app/controllers/concerns/external_authorization.rb
+++ app/controllers/concerns/external_authorization.rb
@@ -7,7 +7,13 @@ module ExternalAuthorization
     auth = authorization(provider)
 
     if auth.nil?
-      redirect_to "/auth/#{provider}"
+      respond_to do |format|
+        format.html { redirect_to "/auth/#{provider}" }
+        format.json do
+          render json: { errors: [{ detail: "Unauthorized" }], success: false },
+            status: 401
+        end
+      end
     elsif auth.expired?
       config = ActiveLMS.configuration.providers[provider.to_sym]
       auth.refresh_with_config! config
