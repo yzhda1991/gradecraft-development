4ecadd13f5d57ce1c25890f8bcdd624b5346bc14
Jonathan Gabel <hello@jonathangabel.com>
2017-04-21 10:00:43 -0400

    fixed broken spec

--- spec/controllers/api/students_controller_spec.rb
+++ spec/controllers/api/students_controller_spec.rb
@@ -21,6 +21,7 @@ describe API::StudentsController do
   context "as a student" do
     before do
       login_user(student)
+      allow(controller).to receive(:current_course).and_return(course)
     end
 
     describe "GET index" do
@@ -34,12 +35,12 @@ describe API::StudentsController do
 
       it "assigns information for charting course progress" do
         assignment_type = create(:assignment_type, course: course)
-        allow(student).to receive(:earned_badges).and_return double(points: [100, 200, 300])
+        allow(student).to receive(:earned_badges).and_return double(sum: 600)
         allow(course).to receive(:total_points).and_return 1000
         get :analytics, format: :json
 
         expect(assigns(:student)).to eq(student)
-        expect(assigns(:assignment_type)).to eq([assignment_type])
+        expect(assigns(:assignment_types)).to eq([assignment_type])
         expect(assigns(:earned_badge_points)).to eq(600)
         expect(assigns(:course_potential_points_for_student)).to eq(1600)
       end
