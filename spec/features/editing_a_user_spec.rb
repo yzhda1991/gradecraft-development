ff59606a1c1224ba2f360071cbd86cae3ced3ce3
chcholman <caitlin.holman@gmail.com>
2017-04-21 08:41:40 -0400

    Test for not updating a password

--- spec/features/editing_a_user_spec.rb
+++ spec/features/editing_a_user_spec.rb
@@ -9,17 +9,25 @@ feature "editing a user" do
       visit edit_user_path(student.id)
     end
 
-    context "updating a password for a GradeCraft student" do
-      scenario "successfully" do
-        expect(current_path).to eq edit_user_path(student)
+    context "updating a GradeCraft student" do
+      scenario "setting a new password successfully" do
         fill_in "Password", with: "crookshanks"
         fill_in "Password confirmation", with: "crookshanks"
 
-        #click_button "Update User"
         find('input[name="commit"]').click
 
         expect(current_path).to eq students_path
         expect(page).to have_notification_message "notice", "#{course.student_term} #{student.name} was successfully updated!"
+      end
+
+      scenario "updating a name but not a password" do
+        visit edit_user_path(student.id)
+        fill_in "First name", with: "The Amazing Hermione"
+
+        find('input[name="commit"]').click
+
+        expect(current_path).to eq students_path
+        expect(page).to have_notification_message "notice", "#{course.student_term} The Amazing Hermione Granger was successfully updated!"
       end
     end
   end
