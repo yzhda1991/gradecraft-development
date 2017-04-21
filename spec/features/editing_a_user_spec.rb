936ef00a7c52a6118158d1918ab731edec65dd7e
chcholman <caitlin.holman@gmail.com>
2017-04-21 08:29:02 -0400

    Added feature test for editing a user's password as an admin

--- spec/features/editing_a_user_spec.rb
+++ spec/features/editing_a_user_spec.rb
@@ -1,24 +1,25 @@
-feature "editing a user", focus: true do
+feature "editing a user" do
   context "as an administrator" do
     let(:course) { build :course }
-    let(:admin) { build :user, role: :admin, courses: [course] }
-    let(:student) { build :user, first_name: "Hermione", last_name: "Granger", courses: [course], role: :student }
+    let(:admin) { create :user, role: :admin, courses: [course] }
+    let(:student) { create :user, first_name: "Hermione", last_name: "Granger", courses: [course], kerberos_uid: nil, role: :student }
 
     before(:each) do
       login_as admin
-      visit edit_user_path(student)
+      visit edit_user_path(student.id)
     end
 
-    context "for an existing GradeCraft student" do
+    context "updating a password for a GradeCraft student" do
       scenario "successfully" do
-        # fill_in 'Password', with: "crookshanks"
-        # fill_in "Password Confirmation", with: "crookshanks"
-        fill_in "First Name", with: "H2"
+        expect(current_path).to eq edit_user_path(student)
+        fill_in "Password", with: "crookshanks"
+        fill_in "Password confirmation", with: "crookshanks"
 
-        click_button "Update User"
+        #click_button "Update User"
+        find('input[name="commit"]').click
 
         expect(current_path).to eq students_path
-        expect(page).to have_notification_message "notice", "#{course_membership.course.student_term} #{user.name} was successfully updated!"
+        expect(page).to have_notification_message "notice", "#{course.student_term} #{student.name} was successfully updated!"
       end
     end
   end
