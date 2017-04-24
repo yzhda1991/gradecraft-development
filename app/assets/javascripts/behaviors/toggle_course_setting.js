b8eb9e06fddf8ba411006b437c3bc85772cb9ac7
Marie Hooper <marie.ashtari@gmail.com>
2017-04-24 11:11:11 -0400

    updates to styling for rubric creation form, grade scheme builder, team filter and dynatable select

--- app/assets/javascripts/behaviors/toggle_course_setting.js
+++ app/assets/javascripts/behaviors/toggle_course_setting.js
@@ -2,17 +2,17 @@ $(function() {
   $(document).on("click", "[data-behavior~=toggle-course-setting]", function(e) {
     e.preventDefault();
     var target = $(e.target);
-    var section = target.parents("section");
-    var setting = section.find(".course-setting");
+    var course = target.parents(".course-selection");
+    var setting = course.find(".course-setting");
     setting.slideToggle(100, function() {
       var index = setting.find("input[name=child_index]").val();
       var destroy = setting.find("#user_course_memberships_attributes_" + index + "__destroy");
       destroy.val(setting.is(":hidden"));
-      section.find("h4 .button").hide();
+      course.find("h4 .button").hide();
       if (setting.is(":hidden")) {
-        section.find("h4 .button").not(".danger").show();
+        course.find("h4 .button").not(".danger").show();
       } else {
-        section.find("h4 .button.danger").show();
+        course.find("h4 .button.danger").show();
       }
     });
   });
