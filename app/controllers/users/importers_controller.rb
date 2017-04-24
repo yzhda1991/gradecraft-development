a3fe11d1038c78c24940b7cebcd375fe87c952dc
Jon Yu <jonathoy@umich.edu>
2017-04-24 13:46:29 -0400

    Add user import controller method + view

--- app/controllers/users/importers_controller.rb
+++ app/controllers/users/importers_controller.rb
@@ -17,6 +17,7 @@ class Users::ImportersController < ApplicationController
     @course_id = params[:id]
   end
 
+  # POST /users/importers/:importer_provider_id/course/:id/users/import
   def users_import
     @provider_name = params[:importer_provider_id]
 
@@ -27,7 +28,7 @@ class Users::ImportersController < ApplicationController
     if @result.success?
       render :user_import_results
     else
-      redirect_to users_importer_users(params[:importer_provider_id], params[:id]),
+      redirect_to users_importer_users_path(params[:importer_provider_id], params[:id]),
         alert: "Failed to import #{params[:importer_provider_id]} users"
     end
   end
---
+++ spec/controllers/users/importers_controller_spec.rb
@@ -0,0 +1,47 @@
+describe Users::ImportersController do
+  let(:course) { build_stubbed :course }
+
+  before(:each) do
+    login_user user
+    allow(controller).to receive(:current_course).and_return course
+  end
+
+  context "as a professor" do
+    let(:provider) { :canvas }
+    let(:access_token) { "BLAH" }
+    let(:user) { build :user, courses: [course], role: :professor }
+    let!(:user_authorization) do
+      create :user_authorization, :canvas, user: user, access_token: access_token,
+        expires_at: 2.days.from_now
+    end
+
+    describe "#users_import" do
+      let(:id) { 1 }
+      let(:user_ids) { [12, 23] }
+      let(:result) { double(:result, success?: true, message: "") }
+
+      before(:each) do
+        allow(Services::ImportsLMSUsers).to receive(:import).and_return result
+      end
+
+      it "imports the lms users" do
+        expect(Services::ImportsLMSUsers).to receive(:import).and_return result
+        post :users_import, params: { id: id, importer_provider_id: provider,
+          user_ids: user_ids }
+      end
+
+      it "renders the results if successful" do
+        post :users_import, params: { id: id, importer_provider_id: provider,
+          user_ids: user_ids }
+        expect(response).to render_template :user_import_results
+      end
+
+      it "redirects to the user import page if unsuccessful" do
+        allow(result).to receive(:success?).and_return false
+        post :users_import, params: { id: id, importer_provider_id: provider,
+          user_ids: user_ids }
+        expect(response).to redirect_to users_importer_users_path(provider, id)
+      end
+    end
+  end
+end
