c56f99f65092da1ee9cda92763032f17b541e478
Jon Yu <jonathoy@umich.edu>
2017-05-04 13:41:11 -0400

    Fix validation for uniqueness

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -2,7 +2,7 @@ class Institution < ActiveRecord::Base
   has_many :courses
   has_many :providers, dependent: :destroy, as: :providee
 
-  validates_presence_of :name, uniqueness: true
-  
+  validates :name, presence: true, uniqueness: true
+
   accepts_nested_attributes_for :providers
 end
