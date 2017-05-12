d5601958c6d45623ffb9a22cb2770d1144f43053
Jonathan Gabel <hello@jonathangabel.com>
2017-05-12 13:52:13 -0400

    updated specs

--- spec/controllers/api/criteria_controller_spec.rb
+++ spec/controllers/api/criteria_controller_spec.rb
@@ -8,11 +8,36 @@ describe API::CriteriaController do
   context "as professor" do
     before(:each) { login_user(professor) }
 
-    describe "GET criteria" do
+    describe "GET index" do
       it "returns criteria for the current assignment" do
         get :index, params: { assignment_id: assignment.id }, format: :json
         expect(assigns(:criteria)[0].id).to eq(rubric.criteria[0].id)
         expect(response).to render_template(:index)
+      end
+    end
+
+    describe "PUT set_expectations" do
+      let(:level) { criterion.levels.last }
+
+      it "sets epxectations for the level in params" do
+        put :set_expectations, criterion_id: criterion.id, level_id: level.id, format: :json
+        expect(level.reload.meets_expectations).to be_truthy
+        expect(criterion.reload.meets_expectations_level_id).to eq(level.id)
+        expect(criterion.reload.meets_expectations_points).to eq(level.points)
+      end
+
+      it "removes expectations for all other levels" do
+        criterion.levels.first.update_attributes(meets_expectations: true)
+        put :set_expectations, criterion_id: criterion.id, level_id: level.id, format: :json
+        expect(criterion.levels.first.reload.meets_expectations).to be_falsey
+      end
+    end
+
+    describe "PUT remove_expectations" do
+      it "removes expectations on criteria and all levels" do
+        criterion.levels.first.update_attributes(meets_expectations: true)
+        put :remove_expectations, criterion_id: criterion.id, format: :json
+        expect(criterion.levels.pluck(:meets_expectations).uniq).to eq([false])
       end
     end
   end
--- spec/controllers/api/levels_controller_spec.rb
+++ spec/controllers/api/levels_controller_spec.rb
@@ -9,6 +9,14 @@ describe API::LevelsController do
   context "as professor" do
     before(:each) { login_user(professor) }
 
+    describe "POST create" do
+      it "creates a new level" do
+        post :create, params: { level: {criterion_id: criterion.id, name: "New Level", points: 1000 }}, format: :json
+        expect(criterion.levels.count).to eq(3)
+        expect(assigns(:level).name).to eq("New Level")
+      end
+    end
+
     describe "PUT update" do
       let(:level) { criterion.levels.last }
       let(:params) do
@@ -17,30 +25,35 @@ describe API::LevelsController do
 
       it "updates the level attributes" do
         put :update, params: params, format: :json
-        expect(level.reload.meets_expectations).to be_truthy
-        expect(level.description).to eq("You have reached a new level")
+        expect(level.reload.description).to eq("You have reached a new level")
       end
 
-      it "insures only one level per criterion meets expectations" do
-        l_1 = criterion.levels.first
-        l_1.update_attributes(meets_expectations: true)
+      it "does not update meets expectations" do
         put :update, params: params, format: :json
-        expect(l_1.reload.meets_expectations).to be_falsey
+        expect(level.reload.meets_expectations).to be_falsey
       end
 
       it "renders success message when request format is JSON" do
-        put :update, params: params
+        put :update, params: params, format: :json
         expect(response.status).to eq(200)
-        expect(JSON.parse(response.body)).to eq("message" => "level successfully updated", "success" => true)
+        expect(assigns(:level)).to eq(criterion.levels.last)
       end
 
       describe "on error" do
         it "describes failure to update" do
           allow_any_instance_of(Level).to receive(:update_attributes) { false }
-          put :update, params: params
+          put :update, params: params, format: :json
           expect(JSON.parse(response.body)).to eq("errors"=>[{"detail"=>"failed to update level"}], "success"=>false)
           expect(response.status).to eq(500)
         end
+      end
+    end
+
+    describe "DELETE level" do
+      it "removes the level from the criterion" do
+        delete :destroy, params: { id: criterion.levels.last.id}, format: :json
+        expect(criterion.levels.count).to eq(1)
+        expect(JSON.parse(response.body)).to eq("message"=>"level successfully deleted", "success"=>true)
       end
     end
   end
