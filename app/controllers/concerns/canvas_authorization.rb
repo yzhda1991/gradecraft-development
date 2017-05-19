64b672155d63d16dd13b919fa9ba20b2ab82c042
Jon Yu <jonathoy@umich.edu>
2017-05-19 11:53:59 -0400

    Appease Rubocop

--- app/controllers/concerns/canvas_authorization.rb
+++ app/controllers/concerns/canvas_authorization.rb
@@ -7,17 +7,16 @@ module CanvasAuthorization
   # if they were stored in the Provider table
   def link_canvas_credentials
     linked_provider = Provider.for_course current_course
+    return if linked_provider.nil?
 
-    unless linked_provider.nil?
-      ActiveLMS.configuration.providers[:canvas].base_uri =
-        linked_provider.base_url
-      ActiveLMS.configuration.providers[:canvas].client_id =
-        linked_provider.consumer_key
-      ActiveLMS.configuration.providers[:canvas].client_secret =
-        linked_provider.consumer_secret
-      ActiveLMS.configuration.providers[:canvas].client_options = {
-        site: "#{linked_provider.base_url}/login/canvas"
-      }
-    end
+    ActiveLMS.configuration.providers[:canvas].base_uri =
+      linked_provider.base_url
+    ActiveLMS.configuration.providers[:canvas].client_id =
+      linked_provider.consumer_key
+    ActiveLMS.configuration.providers[:canvas].client_secret =
+      linked_provider.consumer_secret
+    ActiveLMS.configuration.providers[:canvas].client_options = {
+      site: "#{linked_provider.base_url}/login/canvas"
+    }
   end
 end
