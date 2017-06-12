103f7ba92c8f8fa81eabf45aacc0ccb1969c59a2
chcholman <caitlin.holman@gmail.com>
2017-06-11 21:45:01 -0400

    Specs

--- spec/support/pages/user_page.rb
+++ spec/support/pages/user_page.rb
@@ -13,15 +13,6 @@ class UserPage
     fill_in "First name", with: fields[:first_name] || user.first_name
     fill_in "Last name", with: fields[:last_name] || user.last_name
     fill_in "Email", with: fields[:email] || user.email
-
-    courses = fields[:courses]
-    courses.each_with_index do |course, index|
-      destroyed = find(:xpath, ".//input[@id='user_course_memberships_attributes_#{index}__destroy']", visible: false)
-      if destroyed.value == "true"
-        destroyed.set false
-      else
-        destroyed.set true
-      end
-    end if courses
+    select "Student", from: "user_course_memberships_attributes_0_role"
   end
 end
