b042efcede6a2f8fa7110a4c56c633899faba63d
Kyle Dove <dovek@umich.edu>
2017-06-14 15:05:15 -0400

    3166 Parse name_full from LTI

--- app/services/creates_or_updates_user_from_lti/parse_user_attributes_from_auth_hash.rb
+++ app/services/creates_or_updates_user_from_lti/parse_user_attributes_from_auth_hash.rb
@@ -1,3 +1,5 @@
+require "namae"
+
 module Services
   module Actions
     class ParseUserAttributesFromAuthHash
@@ -12,12 +14,13 @@ module Services
           ensure_valid_request context, raw_info
           email = raw_info.lis_person_contact_email_primary
           username = email.split("@")[0]
+          names_hash = get_names(raw_info)
 
           context[:user_attributes] = {
             email: email,
             lti_uid: raw_info.lis_person_sourcedid,
-            first_name: raw_info.lis_person_name_given,
-            last_name: raw_info.lis_person_name_family,
+            first_name: names_hash["first_name"],
+            last_name: names_hash["last_name"],
             username: username,
             kerberos_uid: username
           }
@@ -28,11 +31,22 @@ module Services
 
       # Fail context early if first_name, last_name are missing
       def self.ensure_valid_request(context, attributes)
-        if attributes.lis_person_name_given.blank? && attributes.lis_person_name_family.blank? &&
-          attributes.lis_person_contact_email_primary.present?
+        if attributes.lis_person_name_given.blank? && attributes.lis_person_name_family.blank? && attributes.lis_person_name_full.blank? &&
+          attributes.lis_person_name_full.blank? && attributes.lis_person_contact_email_primary.present?
           context.fail! "Please check your LTI configuration", 400
         end
       end
+
+      def self.get_names(raw_info)
+        if !raw_info.lis_person_name_given.blank? && !raw_info.lis_person_name_family.blank?
+          return names_hash = {"first_name" => raw_info.lis_person_name_given, "last_name" => raw_info.lis_person_name_family}
+        elsif raw_info.lis_person_name_given.blank? && raw_info.lis_person_name_family.blank? && !raw_info.lis_person_name_full.blank?
+          names = Namae.parse raw_info.lis_person_name_full
+          names[0].family = names[0].given if names[0].family == nil # If user has only one name, will set that name as first name and last name
+          return names_hash = {"first_name" => names[0].given, "last_name" => names[0].family}
+        end
+      end
+
     end
   end
 end
