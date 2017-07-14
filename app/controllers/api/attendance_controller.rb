66215b2fe3b53f4afae8fe49d05124694c70a841
Jon Yu <jonathoy@umich.edu>
2017-07-14 09:27:05 -0400

    Update spec and add additional validation

--- app/controllers/api/attendance_controller.rb
+++ app/controllers/api/attendance_controller.rb
@@ -1,13 +1,17 @@
+# rubocop:disable AndOr
 class API::AttendanceController < ApplicationController
   before_action :ensure_staff?
 
   # POST api/attendance
   # Creates/updates many assignments from the given nested attributes
   def create
+    render json: { message: "Bad request", success: false },
+      status: :bad_request and return if assignments_params.blank?
+
     if current_course.update assignments_params
       @assignments = current_course.assignments.with_attendance_type
     else
-      render json: { message: "Failed to update assignments", success: false },
+      render json: { message: "Failed to update attendance assignments", success: false },
         status: :internal_server_error
     end
   end
@@ -15,7 +19,7 @@ class API::AttendanceController < ApplicationController
   private
 
   def assignments_params
-    params.permit(assignments_attributes: [:id, :name, :description,
-      :open_at, :due_at, :assignment_type_id, :full_points, :pass_fail])
+    params.permit assignments_attributes: [:id, :name, :description,
+      :open_at, :due_at, :assignment_type_id, :full_points, :pass_fail]
   end
 end
--- spec/controllers/api/attendance_controller_spec.rb
+++ spec/controllers/api/attendance_controller_spec.rb
@@ -1,15 +1,50 @@
 describe API::AttendanceController do
-  context "as a professor", focus: true do
+  let(:course) { build :course }
+  let(:assignment_type) { build_stubbed :assignment_type, :attendance }
+
+  before(:each) do
+    login_user user
+    allow(controller).to receive(:current_course).and_return course
+  end
+
+  context "as a professor" do
+    let(:user) { build_stubbed :user, courses: [course], role: :professor }
+
     describe "#create" do
-      xit "creates the assignments"
+      let(:assignments_params) { [assignments_attributes_1, assignments_attributes_2] }
+      let(:assignments_attributes_1) { attributes_for(:assignment).merge(assignment_type_id: assignment_type.id) }
+      let(:assignments_attributes_2) { attributes_for(:assignment).merge(assignment_type_id: assignment_type.id) }
+
+      context "when successful" do
+        it "creates the assignments" do
+          expect{ post :create, params: { assignments_attributes: assignments_params }, format: :json }.to \
+            change(Assignment, :count).by 2
+        end
+
+        it "renders the json template" do
+          post :create, params: { assignments_attributes: assignments_params }, format: :json
+          expect(response).to render_template :create
+          expect(response).to have_http_status :ok
+        end
+      end
+
+      context "when unsuccessful" do
+        it "renders a 400 bad request" do
+          post :create, format: :json
+          expect(response).to have_http_status :bad_request
+          expect(response.body).to include "Bad request"
+        end
+      end
     end
   end
 
   context "as a student" do
+    let(:user) { build_stubbed :user, courses: [course], role: :student }
+
     describe "protected routes" do
       it "redirect with a status 302" do
         [
-          -> { post :create }
+          -> { post :create, format: :json }
         ].each do |protected_route|
           expect(protected_route.call).to have_http_status :redirect
         end
