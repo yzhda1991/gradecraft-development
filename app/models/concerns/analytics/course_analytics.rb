45c1ff00262a5471c002e651cbef0d21b9c4790d
chcholman <caitlin.holman@gmail.com>
2017-07-24 15:45:03 -0400

    specs

--- app/models/concerns/analytics/course_analytics.rb
+++ app/models/concerns/analytics/course_analytics.rb
@@ -4,11 +4,11 @@ module Analytics
     extend ActiveSupport::Concern
 
     def student_count
-      students_for_course.count
+      course_memberships.where(role: "student", active: true).count
     end
 
     def graded_student_count
-      students_being_graded.count
+      course_memberships.being_graded.count
     end
 
     def groups_to_review_count
