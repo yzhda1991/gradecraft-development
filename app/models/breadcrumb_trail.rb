2ddfdea0b2a0fff9a300b4c8892dc6babdcb39fa
chcholman <caitlin.holman@gmail.com>
2017-08-06 21:44:33 -0400

    Styling predictor section

--- app/models/breadcrumb_trail.rb
+++ app/models/breadcrumb_trail.rb
@@ -100,11 +100,6 @@ class BreadcrumbTrail < Croutons::BreadcrumbTrail
     breadcrumb('Quick Grading ' + objects[:assignment].name)
   end
 
-  def assignment_type_weights_index
-    assignments_index
-    breadcrumb('Edit My ' + objects[:course].weight_term + ' Choices')
-  end
-
   def assignment_types_index
     dashboard
     breadcrumb(objects[:course].assignment_term + ' Type Analytics')
