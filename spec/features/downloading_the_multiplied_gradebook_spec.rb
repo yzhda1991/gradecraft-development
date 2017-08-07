60db726c53afa034d8427b7c5581b19f4d7b62f6
chcholman <caitlin.holman@gmail.com>
2017-08-06 23:01:29 -0400

    specs

--- spec/features/downloading_the_multiplied_gradebook_spec.rb
+++ spec/features/downloading_the_multiplied_gradebook_spec.rb
@@ -1,6 +1,6 @@
 feature "downloading multiplied gradebook file" do
   context "as a professor" do
-    let(:course) { build :course, name: "Course Name", has_multipliers: true, total_weights: 1, max_weights_per_assignment_type: 2, max_assignment_types_weighted: 2 }
+    let(:course) { build :course, name: "Course Name", has_multipliers: true, total_weights: 1, max_weights_per_assignment_type: 2, max_assignment_types_weighted: 2, weight_term: "Multiplier" }
     let!(:course_membership) { create :course_membership, :professor, user: professor, course: course }
     let(:professor) { create :user }
 
@@ -11,7 +11,7 @@ feature "downloading multiplied gradebook file" do
 
     scenario "successfully" do
       within(".pageContent") do
-        click_link "Multiplied Gradebook"
+        click_link "Multiplier Gradebook"
       end
 
       expect(page).to have_notification_message("notice", "Your request to export the multiplied gradebook for \"Course Name\" is currently being processed. We will email you the data shortly.")
