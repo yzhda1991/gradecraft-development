528dca8e532dbe8e8b4342e7174856c97165362b
chcholman <caitlin.holman@gmail.com>
2017-08-25 22:11:49 -0400

    Rubrics temp fix, sessions redesign

--- app/controllers/api/rubrics_controller.rb
+++ app/controllers/api/rubrics_controller.rb
@@ -2,7 +2,7 @@ class API::RubricsController < ApplicationController
 
   # GET api/rubric/:id
   def show
-    @rubric = current_course.rubrics.find params[:id]
+    @rubric = Rubric.find params[:id]
     @criteria =
       @rubric.criteria.ordered.includes(:levels)
     @levels = Level.where(criterion_id: @criteria.pluck(:id)).order("criterion_id").order("points").order("sort_order")
