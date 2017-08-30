feb3d6fa02c890ad07bcd1ca9797dfb2308678bf
chcholman <caitlin.holman@gmail.com>
2017-08-30 08:18:14 -0400

    block non-admin users from accessing user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   respond_to :html, :json
 
+  before_action :ensure_admin?, only: [:index, :destroy]
   before_action :ensure_staff?,
     except: [:activate, :activated, :activated_external, :activate_set_password, :edit_profile, :update_profile, :new_external, :create_external]
   before_action :save_referer, only: [:manually_activate, :resend_invite_email]
