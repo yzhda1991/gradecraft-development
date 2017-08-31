4cd1a29a03060bf8ef21d464777a81eb0b9a5260
Kyle Dove <dovek@umich.edu>
2017-08-31 07:49:23 -0400

    Fixed some testing issues

--- spec/controllers/google_calendars/assignments_controller_spec.rb
+++ spec/controllers/google_calendars/assignments_controller_spec.rb
@@ -87,7 +87,7 @@ describe GoogleCalendars::AssignmentsController, type:[:disable_external_api, :c
 
   describe "POST add_assignments" do
     before(:each) do
-      stub_request(:post, "https://www.googleapis.com/calendar/v3/calendars/primary/events").
+      stub_request(:post, "https://www.googleapis.com/batch").
         to_return(:status => 200, :body => "", :headers => {})
     end
     context "with an existing authentication" do
