2ce4e1234a4c0b659e8e45d0e3f2559bc0d3c800
chcholman <caitlin.holman@gmail.com>
2017-09-16 20:46:23 -0400

    dashboard events

--- app/presenters/info/dashboard_course_events_presenter.rb
+++ app/presenters/info/dashboard_course_events_presenter.rb
@@ -15,15 +15,6 @@ class Info::DashboardCourseEventsPresenter < Showtime::Presenter
     properties[:assignments]
   end
 
-  # figure out what dates need to be shown
-  def dates_for(event, user)
-    if event.open_at && event.due_at?
-      "#{event.open_at.in_time_zone(user.time_zone)} - #{ event.due_at.in_time_zone(user.time_zone)}"
-    elsif event.due_at?
-      "Due: #{event.due_at.in_time_zone(user.time_zone)}"
-    end
-  end
-
   # collect the assignments that are on the same date as the event
   def assignments_due_on(event)
     assignments.where('DATE(due_at) = ?', event.due_at.to_date)
--- spec/presenters/info/dashboard_course_events_spec.rb
+++ spec/presenters/info/dashboard_course_events_spec.rb
@@ -7,15 +7,6 @@ describe Info::DashboardCourseEventsPresenter do
 
   subject { described_class.new course: course, student: student, assignments: course.assignments }
 
-  describe "#dates_for(event, user)" do
-    it "returns the due dates to be displayed for a particular event" do
-      expect(subject.dates_for(event, student)).to eq "Due: #{event.due_at.in_time_zone(student.time_zone)}"
-    end
-    it "returns both the open at and the due at dates to be displayed for a particular event" do
-      expect(subject.dates_for(event_with_open, student)).to eq "#{event_with_open.open_at.in_time_zone(student.time_zone)} - #{event_with_open.due_at.in_time_zone(student.time_zone)}"
-    end
-  end
-
   describe "#assignments_due_on(event)" do
     it "returns the assignments for the course that are also due on this day" do
       expect(subject.assignments_due_on(event)).to eq [assignment]
