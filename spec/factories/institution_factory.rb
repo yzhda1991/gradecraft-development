3447ad3919bf939b1077cb6d987ddd6e91be1b14
Jon Yu <jonathoy@umich.edu>
2017-10-24 10:34:54 -0400

    Add UserProctor, consolidate password edit logic

--- spec/factories/institution_factory.rb
+++ spec/factories/institution_factory.rb
@@ -6,5 +6,17 @@ FactoryGirl.define do
     trait :without_site_license do
       has_site_license false
     end
+
+    trait :k_12 do
+      institution_type "K-12"
+    end
+
+    trait :higher_ed do
+      institution_type "Higher Education"
+    end
+
+    trait :other do
+      institution_type "Other"
+    end
   end
 end
