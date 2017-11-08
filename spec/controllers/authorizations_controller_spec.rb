0cf7077de5969996cb0837bb126ecae1d0b838ec
Kyle Dove <dovek@umich.edu>
2017-11-08 15:35:26 -0500

    Added test for auth failure in create for google

--- spec/controllers/authorizations_controller_spec.rb
+++ spec/controllers/authorizations_controller_spec.rb
@@ -66,7 +66,6 @@ describe AuthorizationsController do
 
       before do
         request.env["omniauth.auth"] = google_oauth2_hash
-        # login_user(professor)
       end
 
       context "for a new authorization" do
@@ -84,35 +83,23 @@ describe AuthorizationsController do
 
           get :create_for_google, params: { provider: provider }
 
-          # expect(response).to redirect_to courses_path
           expect(session["user_id"]).to eq(professor.id.to_s)
+        end
+      end
+
+      before do
+        request.env["omniauth.auth"] = nil
+      end
+
+      context "for a new authorization" do
+        it "redirects to auth failure page" do
+          get :create_for_google, params: { provider: provider }
+
+          authorization = UserAuthorization.unscoped.last
+
+          expect(response).to redirect_to auth_failure_path
         end
       end
     end
   end
-
-  # describe "#log_me_in" do
-  #   context "as a professor" do
-  #     let(:google_oauth2_hash) do
-  #       {
-  #         provider: provider,
-  #         credentials: {
-  #           token: "BLAH",
-  #           refresh_token: "REFRESH",
-  #           expires_at: expires_at.to_i,
-  #           expires: true
-  #         }
-  #       }.deep_stringify_keys
-  #     end
-  #     let(:expires_at) { Time.now + (30 * 24 * 60 * 60) }
-  #     let(:professor) { professor_membership.user }
-  #     let(:professor_membership) { create :course_membership, :professor }
-  #     let(:provider) { :google_oauth2 }
-  #
-  #     before do
-  #       request.env["omniauth.auth"] = google_oauth2_hash
-  #     end
-  #
-  #   end
-  # end
 end
