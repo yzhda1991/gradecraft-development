02d11b1e6df5004938ad3cf25dedfec67884e2f7
Kyle Dove <dovek@umich.edu>
2018-03-26 10:45:15 -0400

    Rspec changes

--- spec/exporters/course_grade_exporter_spec.rb
+++ spec/exporters/course_grade_exporter_spec.rb
@@ -3,16 +3,20 @@ describe CourseGradeExporter do
   subject { CourseGradeExporter.new }
 
   describe "#final_grades_for_course(course)" do
+    let!(:team) { create :team, course: course }
 
     it "generates an empty CSV if there are no students" do
       csv = subject.final_grades_for_course(course)
-      expect(csv).to include "First Name,Last Name,Email,Username,Score,Grade,Level,Earned Badge #,GradeCraft ID,GradeCraft Account Created,Last Logged In At,Auditing"
+      expect(csv).to include "First Name,Last Name,Email,Username,Score,Grade,Level,Team,Earned Badge #,GradeCraft ID,GradeCraft Account Created,Last Logged In At,Auditing"
     end
 
     it "generates a CSV with student grades for the course" do
       @student = create(:user, first_name: "Pinky", last_name: "Alpha")
       @student_2 = create(:user, first_name: "The Brain", last_name: "Zed")
       create(:course_membership, :student, course: course, user: @student, score: 80000, auditing: true)
+      # let!(:team_membership) { create :team_membership, team: team, student: student }
+      create(:team_membership, team: team, student: @student)
+      create(:team_membership, team: team, student: @student_2)
       @students = course.students
       create(:course_membership, :student, course: course, user: @student_2, score: 120000, auditing: false)
       create(:grade_scheme_element, course: course, level: "Amazing", letter: "B-")
@@ -34,12 +38,14 @@ describe CourseGradeExporter do
       expect(csv[2][5]).to eq "B"
       expect(csv[1][6]).to eq "Amazing"
       expect(csv[2][6]).to eq "Phenomenal"
-      expect(csv[1][7]).to eq "0"
-      expect(csv[2][7]).to eq "0"
-      expect(csv[1][8]).to eq "#{@student.id}"
-      expect(csv[2][8]).to eq "#{@student_2.id}"
-      expect(csv[1][11]).to eq "Yes"
-      expect(csv[2][11]).to eq "No"
+      expect(csv[1][7]).to eq @student.team_for_course(course).name
+      expect(csv[2][7]).to eq @student_2.team_for_course(course).name
+      expect(csv[1][8]).to eq "0"
+      expect(csv[2][8]).to eq "0"
+      expect(csv[1][9]).to eq "#{@student.id}"
+      expect(csv[2][9]).to eq "#{@student_2.id}"
+      expect(csv[1][12]).to eq "Yes"
+      expect(csv[2][12]).to eq "No"
     end
   end
 end
