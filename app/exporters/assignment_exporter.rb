bf4802f9575b5550f73ca2003e688d1ab9e1585b
chcholman <caitlin.holman@gmail.com>
2018-04-04 13:04:01 -0400

    added fields to the assignment structure export

--- app/exporters/assignment_exporter.rb
+++ app/exporters/assignment_exporter.rb
@@ -3,9 +3,10 @@ class AssignmentExporter
     CSV.generate do |csv|
       csv << baseline_headers
       course.assignments.each do |assignment|
-        csv << [ assignment.id, assignment.name, assignment.full_points,
-          assignment.description, assignment.open_at, assignment.due_at,
-          assignment.accepts_submissions_until, assignment.submissions.submitted.count  ]
+        csv << [ assignment.id, assignment.name, assignment.assignment_type.name,
+          assignment.full_points, assignment.description, assignment.open_at,
+          assignment.due_at, assignment.accepts_submissions_until,
+          assignment.submissions.submitted.count, assignment.created_at ]
       end
     end
   end
@@ -13,7 +14,7 @@ class AssignmentExporter
   private
 
   def baseline_headers
-    ["Assignment ID", "Name", "Point Total", "Description", "Open At",
-      "Due At", "Accept Until", "Submissions Count" ]
+    ["Assignment ID", "Name", "Assignment Type", "Point Total", "Description", "Open At",
+      "Due At", "Accept Until", "Submissions Count", "Created At" ]
   end
 end
