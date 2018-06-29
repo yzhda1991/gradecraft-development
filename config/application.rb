3b517b11ee1f48e292cb7d0694ed50d83b07f253
Jon Yu <jonathoy@umich.edu>
2018-06-29 15:02:47 -0400

    Run app:update task

--- config/application.rb
+++ config/application.rb
@@ -4,13 +4,23 @@ require 'rails/all'
 require "csv"
 require "sanitize"
 
-# Uncomment to debug deprecations in gems
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+
+# Uncomment the following line to debug deprecations in gems
 # ActiveSupport::Deprecation.debug = true
 
 Bundler.require(*Rails.groups)
 
 module GradeCraft
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
     config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/models/abilities)
 
     config.assets.precompile += %w(.svg .eot .otf .woff .ttf)
