aa310264e59f7588b6236dd5774bdba0ade1a351
Jon Yu <jonathoy@umich.edu>
2018-07-06 11:01:14 -0400

    Run app:update, add bootsnap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,9 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
-  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
-  # `config/secrets.yml.key`.
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  config.require_master_key = false
   config.read_encrypted_secrets = false
 
   # Disable serving static files from the `/public` folder by default since
@@ -40,8 +40,11 @@ Rails.application.configure do
   config.action_controller.default_url_options = { :host => "umich.gradecraft.com" }
 
   # Specifies the header that your server uses for sending files.
-  config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
+  config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
   config.action_dispatch.default_headers = { "X-Frame-Options" => "ALLOWALL" }
+
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = nil
@@ -65,6 +68,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "grade_craft_#{Rails.env}"
+  
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
