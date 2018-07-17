162d0332a4b76de201f87413563061262b5ee784
Yuan <yuant@umich.edu>
2018-07-17 15:07:50 -0400

    update the indentation to 2

--- app/controllers/multipliers_controller.rb
+++ app/controllers/multipliers_controller.rb
@@ -1,21 +1,21 @@
 class MultipliersController < ApplicationController
-    before_action :ensure_staff?
-    before_action :find_course
+  before_action :ensure_staff?
+  before_action :find_course
 
-    def export
-        if !@course.total_weights
-            redirect_to downloads_path, flash: { error: "This course doesn't have multipliers"}
-            return
-        end
-
-        respond_to do |format|
-            format.csv { send_data MultipliersExporter.new(@course).export(), filename: "#{ @course.name } multipliers - #{ Date.today }.csv" }
-        end
+  def export
+    if !@course.total_weights
+      redirect_to downloads_path, flash: { error: "This course doesn't have multipliers"}
+      return
     end
 
-    private
-
-    def find_course
-        @course = current_user.courses.find(params[:course_id])
+    respond_to do |format|
+      format.csv { send_data MultipliersExporter.new(@course).export(), filename: "#{ @course.name } multipliers - #{ Date.today }.csv" }
     end
+  end
+
+  private
+
+  def find_course
+    @course = current_user.courses.find(params[:course_id])
+  end
 end
