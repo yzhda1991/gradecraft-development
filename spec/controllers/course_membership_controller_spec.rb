f3162120ebb9262092fa7c8b7ea144b88909c1a6
Yuan <yuant@umich.edu>
2018-07-26 16:10:53 -0400

    update pr

--- spec/controllers/course_membership_controller_spec.rb
+++ spec/controllers/course_membership_controller_spec.rb
@@ -1,9 +1,10 @@
-describe CourseMembershipsController do
+describe CourseMembershipsController, focus: true do
   let(:course) { create :course }
   let(:student) { create(:course_membership, :student, course: course).user }
   let(:professor) { create(:course_membership, :professor, course: course).user }
   let(:admin) { create(:course_membership, :admin, course: course).user }
 
+  before(:each) { Course.destroy_all }
   before(:each) { CourseMembership.destroy_all }
 
   context "as professor" do
