409d365fc812bf99395b9acdd4bf07438974f0fd
Jon Yu <jonathoy@umich.edu>
2018-07-27 16:04:58 -0400

    Tweak failing spec

--- spec/features/downloading_assignment_structure_spec.rb
+++ spec/features/downloading_assignment_structure_spec.rb
@@ -14,7 +14,7 @@ feature "downloading assignment structure file" do
       end
 
       expect(page.response_headers["Content-Type"]).to eq("text/csv")
-      expect(page).to have_content "Name,Assignment Type,Point Total,Description,Purpose,Open At,Due At,Accepts Submissions,Accept Until,Required,Assignment Id,Created At,Submissions Count,Grades Count,Learning Objectives\n"
+      expect(page).to have_content "Name,Assignment Type,Point Total,Description,Purpose,Open At,Due At,Accepts Submissions,Accept Until,Required,Assignment Id,Created At,Submissions Count,Grades Count,Learning Objectives"
     end
   end
 end
