75df6f165b16066bad16ae6163593fc155882a43
Jon Yu <jonathoy@umich.edu>
2018-07-31 12:52:11 -0400

    Revise errant headers, import relative dates

--- app/exporters/assignment_exporter.rb
+++ app/exporters/assignment_exporter.rb
@@ -1,4 +1,10 @@
 class AssignmentExporter
+  attr_accessor :user, :course
+
+  def initialize(current_user, course)
+    @course = course
+    @user = current_user
+  end
 
   # This format is shared with Assignment import views and functions
   FORMAT = [
@@ -12,18 +18,35 @@ class AssignmentExporter
     "Assignment Id", "Created At", "Submissions Count", "Grades Count", "Learning Objectives"
   ]
 
-  def export(course)
+  def export
     CSV.generate do |csv|
       csv << FORMAT + ADDITIONAL_HEADERS
-      course.assignments.each do |a|
+      @course.assignments.each do |a|
         csv << [
-          a.name, a.assignment_type.name, a.full_points, a.description,
-          a.purpose, a.open_at, a.due_at,
-          a.accepts_submissions, a.accepts_submissions_until, a.required,
-          a.id, a.created_at, a.submissions.submitted.count, a.grades.student_visible.count,
+          a.name,
+          a.assignment_type.name,
+          a.full_points,
+          a.description,
+          a.purpose,
+          formatted_date(a.open_at),
+          formatted_date(a.due_at),
+          a.accepts_submissions,
+          formatted_date(a.accepts_submissions_until),
+          a.required,
+          a.id,
+          formatted_date(a.created_at),
+          a.submissions.submitted.count,
+          a.grades.student_visible.count,
           a.learning_objectives.pluck(:name).join(',')
         ]
       end
     end
+  end
+
+  private
+
+  def formatted_date(date)
+    return nil if date.nil?
+    date.in_time_zone(@user.time_zone).strftime("%d/%m/%Y")
   end
 end
