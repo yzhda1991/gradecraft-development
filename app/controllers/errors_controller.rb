cb98f0e3fc069a3ce313013b6528b894edabc9a3
Jon Yu <jonathoy@umich.edu>
2018-08-06 14:03:29 -0400

    Set up test layout

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,8 +2,10 @@ class ErrorsController < ApplicationController
   skip_before_action :require_login
   skip_before_action :require_course_membership
 
+  layout "sophia"
+
   def show
     render status: params[:status_code].blank? ? 500 : params[:status_code],
-      layout: "error", locals: { error_type: params[:error_type] }
+      locals: { error_type: params[:error_type] }
   end
 end
