e2df5c0192c9b0da9d5e3e185fd3a1836d42dfca
Jon Yu <jonathoy@umich.edu>
2018-08-27 13:40:56 -0400

    Fix specs

--- spec/exporters/learning_objectives_outcomes_exporter_spec.rb
+++ spec/exporters/learning_objectives_outcomes_exporter_spec.rb
@@ -6,13 +6,13 @@ describe LearningObjectivesOutcomesExporter do
     let(:learning_objectives) { create_list :learning_objective, 3, course: course, count_to_achieve: 3 }
 
     it "generates an empty CSV if there are no students or learning objectives" do
-      csv = subject.learning_objectives_outcomes
+      csv = subject.learning_objective_outcomes
       expect(csv).to eq "First Name,Last Name,Email,Username,Team\n"
     end
 
     it "generates an empty CSV if there are no students" do
       learning_objectives
-      csv = subject.learning_objectives_outcomes
+      csv = subject.learning_objective_outcomes
       expect(csv).to eq "First Name,Last Name,Email,Username,Team,#{learning_objectives.first.name},#{learning_objectives.second.name},#{learning_objectives.third.name}\n"
     end
 
@@ -27,7 +27,7 @@ describe LearningObjectivesOutcomesExporter do
       failed_cumulative_outcome = create :learning_objective_cumulative_outcome, learning_objective: learning_objectives.first, user: another_student
       failed_observed_outcome = create :learning_objective_observed_outcome, objective_level_id: failed_learning_objective_level.id, learning_objective_cumulative_outcomes_id: failed_cumulative_outcome.id
 
-      csv = CSV.new(subject.learning_objectives_outcomes).read
+      csv = CSV.new(subject.learning_objective_outcomes).read
 
       expect(csv.length).to eq 3
       expect(csv[1]).to eq [student.first_name, student.last_name, student.email,
