1ab8b9a91cfaf1e795aa64c53e3fbfe4611bcbd6
Jon Yu <jonathoy@umich.edu>
2018-10-29 15:06:38 -0400

    Revert migration version to 5.0

--- db/migrate/20181024184812_add_auto_award_after_unlock_to_badges.rb
+++ db/migrate/20181024184812_add_auto_award_after_unlock_to_badges.rb
@@ -1,4 +1,4 @@
-class AddAutoAwardAfterUnlockToBadges < ActiveRecord::Migration[5.2]
+class AddAutoAwardAfterUnlockToBadges < ActiveRecord::Migration[5.0]
   def change
     add_column :badges, :auto_award_after_unlock, :boolean
   end
