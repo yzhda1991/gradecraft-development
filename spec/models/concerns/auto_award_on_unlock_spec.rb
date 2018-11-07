ee512992cb531299e8ee1399bea5c9e88dafb7de
Jon Yu <jonathoy@umich.edu>
2018-11-07 11:00:15 -0500

    [WIP] Add test

---
+++ spec/models/concerns/auto_award_on_unlock_spec.rb
@@ -0,0 +1,39 @@
+describe AutoAwardOnUnlock do
+  subject { build :grade }
+
+  let(:unlock_state) { build :unlock_state }
+  let(:creates_earned_badge_service) { class_double "Services::CreatesEarnedBadge", call: true }
+
+  before(:each) do
+    allow(subject).to receive("Services::CreatesEarnedBadge").and_return \
+      creates_earned_badge_service
+  end
+
+  describe "#award_badge", focus: true do
+    let(:course) { create :course }
+    let(:student) { create :user, role: :student, courses: [course] }
+    let(:earned_badge_attr) do
+      {
+        course_id: course.id,
+        student_id: student.id
+      }
+    end
+
+    it "does not award the badge if the unlock state is not unlocked" do
+      unlock_state.unlocked = false
+      expect(subject.award_badge(unlock_state, earned_badge_attr)).to be_nil
+    end
+
+    it "does not award the badge if the unlockable type is not a badge" do
+
+    end
+
+    it "does not award the badge if the badge does not auto award" do
+
+    end
+
+    it "creates an earned badge with auto-awarded feedback" do
+
+    end
+  end
+end
