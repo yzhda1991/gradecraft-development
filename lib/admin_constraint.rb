47e59da9671ac4c023ec61d5157ae37599db445a
Jon Yu <jonathoy@umich.edu>
2018-11-12 11:04:00 -0500

    Fix admin constraint, if not logged in

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -1,5 +1,6 @@
 class AdminConstraint
   def matches?(request)
+    return false if request.session[:user_id].blank?
     user = User.find request.session[:user_id]
     user.present? && user.admin?
   end
